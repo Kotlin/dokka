deae3bfbe0a5bda997f3033693879042a058de3a
Ilya Ryzhenkov <orangy@jetbrains.com>
2014-07-11 17:16:09 +0400

    Bootstrapped TDD, first test red then green.

--- src/DocumentationModel.kt
+++ src/DocumentationModel.kt
@@ -3,10 +3,17 @@ package com.jetbrains.dokka
 import com.intellij.psi.PsiFile
 import org.jetbrains.jet.lang.resolve.BindingContext
 
+public class DocumentationNode {
+    val name = "fn"
+    val doc = "doc"
+}
+
 public class DocumentationModel {
     fun merge(other: DocumentationModel): DocumentationModel {
         return DocumentationModel()
     }
+
+    val items : List<DocumentationNode> = listOf(DocumentationNode())
 }
 
 fun BindingContext.createDocumentation(file: PsiFile): DocumentationModel {
--- test/src/DiscoveryTest.kt
+++ test/src/DiscoveryTest.kt
@@ -1,12 +1,15 @@
 package com.jetbrains.dokka.tests
 
 import org.junit.Test
+import kotlin.test.*
 
 
 public class FunctionTest {
     Test fun function() {
-        verifyFiles("test/data/function.kt") {
-
+        verifyFiles("test/data/function.kt") { model ->
+            val item = model.items.single()
+            assertEquals("fn", item.name)
+            assertEquals("doc", item.doc)
         }
     }
 }
