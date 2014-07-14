073d8613fdc105baacaee4d138755f02f658acbe
Ilya Ryzhenkov <orangy@jetbrains.com>
2014-07-14 18:38:02 +0400

    Cleanup

--- src/Generation/FileGenerator.kt
+++ src/Generation/FileGenerator.kt
@@ -5,13 +5,14 @@ import java.io.File
 public class FileGenerator(val signatureGenerator: SignatureGenerator,
                            val locationService: LocationService,
                            val formatService: FormatService) {
+
     public fun generate(node: DocumentationNode) {
         val location = locationService.location(node)
         val file = location.file.appendExtension(formatService.extension)
         file.getParentFile()?.mkdirs()
         file.writeText(formatService.format(node), defaultCharset)
-        val items = node.members.sortBy { it.name }
-        for (child in items)
-            generate(child)
+        val members = node.members.sortBy { it.name }
+        for (member in members)
+            generate(member)
     }
 }
