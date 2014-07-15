62177099390dccf4fba3facb668415b8633e6e24
Ilya Ryzhenkov <orangy@jetbrains.com>
2014-07-16 00:10:39 +0400

    Add ignored test for mixed Java + Kotlin

---
+++ test/data/java/member.java
@@ -0,0 +1,12 @@
+package test
+
+class Test {
+    /**
+     * Summary for Function
+     * @param name is String parameter
+     * @param value is int parameter
+     */
+    public void Function(String name, int value) {
+
+    }
+}
---
+++ test/src/model/JavaTest.kt
@@ -0,0 +1,23 @@
+package org.jetbrains.dokka.tests
+
+import kotlin.test.*
+import org.jetbrains.dokka.*
+import org.junit.*
+
+public class JavaTest {
+    Ignore Test fun function() {
+        verifyModel("test/data/java/") { model ->
+            val pkg = model.members.single()
+            with(pkg.members.single()) {
+                assertEquals("fn", name)
+                assertEquals(DocumentationNode.Kind.Function, kind)
+                assertEquals("Function fn", doc.summary)
+                assertEquals("Unit", detail(DocumentationNode.Kind.Type).name)
+                assertTrue(members.none())
+                assertTrue(links.none())
+            }
+        }
+    }
+
+}
+
