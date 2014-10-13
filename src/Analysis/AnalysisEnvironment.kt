22ba0ae92ae3142d4731747def27faf2a37797e6
Ilya Ryzhenkov <orangy@jetbrains.com>
2014-10-13 19:30:42 +0400

    Migrate to lazy resolve session

--- src/Analysis/AnalysisEnvironment.kt
+++ src/Analysis/AnalysisEnvironment.kt
@@ -32,8 +32,9 @@ public class AnalysisEnvironment(val messageCollector: MessageCollector, body: A
      */
     public fun withContext<T>(processor: (JetCoreEnvironment, ModuleDescriptor, BindingContext) -> T): T {
         val environment = JetCoreEnvironment.createForProduction(this, configuration)
-        val exhaust = environment.analyze()
-        return processor(environment, exhaust.getModuleDescriptor(), exhaust.getBindingContext())
+        val resolveSession = environment.analyze()
+        resolveSession.forceResolveAll()
+        return processor(environment, resolveSession.getModuleDescriptor(), resolveSession.getBindingContext())
     }
 
     /**
