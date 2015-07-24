891cdbbe19580ee2379e3b7cfe269109bf897d84
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2015-07-24 15:35:22 +0300

    Put source position information to node's details section

--- src/Model/SourceLinks.kt
+++ src/Model/SourceLinks.kt
@@ -4,16 +4,15 @@ import com.intellij.psi.PsiElement
 import java.io.File
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiNameIdentifierOwner
+import org.jetbrains.kotlin.psi.psiUtil.startOffset
 
 class SourceLinkDefinition(val path: String, val url: String, val lineSuffix: String?)
 
 fun DocumentationNode.appendSourceLink(psi: PsiElement?, sourceLinks: List<SourceLinkDefinition>) {
-    val path = psi?.getContainingFile()?.getVirtualFile()?.getPath()
-    if (path == null) {
-        return
-    }
-    val target = if (psi is PsiNameIdentifierOwner) psi.getNameIdentifier() else psi
-    val absPath = File(path).getAbsolutePath()
+    val path = psi?.containingFile?.virtualFile?.path ?: return
+
+    val target = if (psi is PsiNameIdentifierOwner) psi.nameIdentifier else psi
+    val absPath = File(path).absolutePath
     val linkDef = sourceLinks.firstOrNull { absPath.startsWith(it.path) }
     if (linkDef != null) {
         var url = linkDef.url + path.substring(linkDef.path.length())
@@ -26,10 +25,19 @@ fun DocumentationNode.appendSourceLink(psi: PsiElement?, sourceLinks: List<Sourc
         append(DocumentationNode(url, Content.Empty, DocumentationNode.Kind.SourceUrl),
                 DocumentationReference.Kind.Detail);
     }
+
+    append(DocumentationNode("$path:${target?.lineNumber()}:${target?.columnNumber()}", Content.Empty, DocumentationNode.Kind.SourcePosition), DocumentationReference.Kind.Detail)
 }
 
 fun PsiElement.lineNumber(): Int? {
-    val doc = PsiDocumentManager.getInstance(getProject()).getDocument(getContainingFile())
+    val doc = PsiDocumentManager.getInstance(project).getDocument(containingFile)
     // IJ uses 0-based line-numbers; external source browsers use 1-based
-    return doc?.getLineNumber(getTextRange().getStartOffset())?.plus(1)
+    return doc?.getLineNumber(textRange.startOffset)?.plus(1)
+}
+
+fun PsiElement.columnNumber(): Int? {
+    val doc = PsiDocumentManager.getInstance(project).getDocument(containingFile)
+    // IJ uses 0-based line-numbers; external source browsers use 1-based
+    val lineNumber = doc?.getLineNumber(textRange.startOffset)?.plus(1) ?: return null
+    return startOffset - doc!!.getLineStartOffset(lineNumber)
 }
