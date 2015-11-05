c83e3d00b38f74e203c6e88a5f7f5a2ca2d71191
Dmitry Jemerov <yole@jetbrains.com>
2015-11-05 13:30:05 +0100

    get rid of duplicate implementations of calculating DocumentationNode qualified name

--- javadoc/src/main/kotlin/utils.kt
+++
@@ -1,9 +0,0 @@
-package org.jetbrains.dokka.javadoc
-
-import org.jetbrains.dokka.DocumentationNode
-import org.jetbrains.dokka.path
-
-val DocumentationNode.qualifiedName: String
-    get() = this.path.filter { it.kind == DocumentationNode.Kind.Package || it.kind in DocumentationNode.Kind.classLike }
-                .map { it.name }
-                .joinToString(".")
