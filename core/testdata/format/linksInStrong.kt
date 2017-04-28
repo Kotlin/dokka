9b891a6ae82c712482a1f8137e781d4cbc4af605
Alex Waters <awaters@nextfaze.com>
2017-04-28 14:56:44 +0930

    Fix missing whitespace around links and tokens in Markdown formatted text

---
+++ core/testdata/format/linksInStrong.kt
@@ -0,0 +1,13 @@
+/**
+ * A strong class.
+ *
+ * __This class [Bar] is awesome.__
+ *
+ * __Even more awesomer is the function [Bar.foo]__
+ *
+ * __[Bar.hello] is also OK__
+ */
+class Bar {
+    fun foo() {}
+    fun hello() {}
+}
