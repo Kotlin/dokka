ce115e851df5813719ae4b700f706109f07c4616
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2017-06-16 22:38:01 +0300

    Configure dokka based on Kotlin compile tasks

---
+++ integration/src/main/kotlin/org/jetbrains/dokka/ReflectDsl.kt
@@ -0,0 +1,70 @@
+package org.jetbrains.dokka
+
+import kotlin.reflect.*
+import kotlin.reflect.jvm.isAccessible
+
+object ReflectDsl {
+
+    class CallOrPropAccess(private val receiver: Any?,
+                           private val clz: KClass<*>,
+                           private val selector: String) {
+
+        @Suppress("UNCHECKED_CAST")
+        operator fun <T : Any?> invoke(vararg a: Any?): T {
+            return func!!.call(receiver, *a) as T
+        }
+
+        operator fun get(s: String): CallOrPropAccess {
+            return v<Any?>()!![s]
+        }
+
+        val func: KFunction<*>? by lazy { clz.memberFunctions.find { it.name == selector } }
+        val prop: KProperty<*>? by lazy { clz.memberProperties.find { it.name == selector } }
+
+        fun takeIfIsFunc(): CallOrPropAccess? = if (func != null) this else null
+
+        fun takeIfIsProp(): CallOrPropAccess? = if (prop != null) this else null
+
+        @Suppress("UNCHECKED_CAST")
+        fun <T : Any?> v(): T {
+            val prop = prop!!
+            return try {
+                prop.getter.apply { isAccessible = true }.call(receiver) as T
+            } catch (e: KotlinNullPointerException) {
+                // Hack around kotlin-reflect bug KT-18480
+                val jclass = clz.java
+                val customGetterName = prop.getter.name
+                val getterName = if (customGetterName.startsWith("<")) "get" + prop.name.capitalize() else customGetterName
+                val getter = jclass.getDeclaredMethod(getterName)
+                getter.isAccessible = true
+
+                getter.invoke(receiver) as T
+
+            }
+        }
+
+        @Suppress("UNCHECKED_CAST")
+        fun v(x: Any?) {
+            (prop as KMutableProperty).setter.apply { isAccessible = true }.call(receiver, x)
+        }
+
+
+    }
+
+    operator fun Any.get(s: String): CallOrPropAccess {
+        val clz = this.javaClass.kotlin
+        return CallOrPropAccess(this, clz, s)
+    }
+
+    operator fun Any.get(s: String, clz: Class<*>): CallOrPropAccess {
+        val kclz = clz.kotlin
+        return CallOrPropAccess(this, kclz, s)
+    }
+
+    operator fun Any.get(s: String, clz: KClass<*>): CallOrPropAccess {
+        return CallOrPropAccess(this, clz, s)
+    }
+
+    inline infix fun Any.isInstance(clz: Class<*>?): Boolean = clz != null && clz.isAssignableFrom(this.javaClass)
+    inline infix fun Any.isNotInstance(clz: Class<*>?): Boolean = !(this isInstance clz)
+}
--- runners/android-gradle-plugin/src/main/kotlin/mainAndroid.kt
+++ runners/android-gradle-plugin/src/main/kotlin/mainAndroid.kt
@@ -1,13 +1,8 @@
 package org.jetbrains.dokka.gradle
 
-import com.android.build.gradle.*
-import com.android.build.gradle.internal.VariantManager
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.tasks.SourceSet
 import java.io.File
-import kotlin.reflect.jvm.isAccessible
-import kotlin.reflect.memberProperties
 
 open class DokkaAndroidPlugin : Plugin<Project> {
     override fun apply(project: Project) {
@@ -19,50 +14,4 @@ open class DokkaAndroidPlugin : Plugin<Project> {
     }
 }
 
-open class DokkaAndroidTask : DokkaTask() {
-    override val sdkProvider: SdkProvider? = AndroidSdkProvider(project)
-}
-
-private class AndroidSdkProvider(private val project: Project) : SdkProvider {
-    private val ext: BaseExtension? by lazy {
-        project.extensions.findByType(LibraryExtension::class.java)
-                ?: project.extensions.findByType(AppExtension::class.java)
-                ?: project.extensions.findByType(TestExtension::class.java)
-    }
-
-    private val isAndroidProject: Boolean get() = ext != null
-
-    private val variantManager: VariantManager? by lazy {
-        val plugin = (project.plugins.findPlugin("android")
-                ?: project.plugins.findPlugin("android-library")
-                ?: project.plugins.findPlugin("com.android.test")
-                ?: throw Exception("Android plugin not found, please use dokka-android with android or android-library plugin.")) as BasePlugin
-        plugin.javaClass.kotlin.memberProperties
-                .find { it.name == "variantManager" }
-                ?.apply { isAccessible = true }
-                ?.let { it.get(plugin) as VariantManager }
-                ?: plugin.variantManager
-    }
-
-    private val allVariantsClassPath by lazy {
-        try {
-            variantManager?.variantDataList?.flatMap { it.variantConfiguration.compileClasspath }!!
-        } catch(e: Exception) {
-            throw Exception("Unsupported version of android build tools, could not access variant manager.", e)
-        }
-    }
-
-    override val name: String = "android"
-
-    override val isValid: Boolean
-        get() = isAndroidProject
-
-    override val classpath: List<File>
-        get() = ext?.bootClasspath.orEmpty() + allVariantsClassPath
-
-    override val sourceDirs: Set<File>?
-        get() {
-            val sourceSet = ext?.sourceSets?.findByName(SourceSet.MAIN_SOURCE_SET_NAME)
-            return sourceSet?.java?.srcDirs
-        }
-}
+open class DokkaAndroidTask : DokkaTask()
