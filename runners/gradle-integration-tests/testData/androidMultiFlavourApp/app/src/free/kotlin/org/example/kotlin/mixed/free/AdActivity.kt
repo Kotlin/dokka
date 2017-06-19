00883dff89c95661d2a3653b076d836f804eee38
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2017-06-19 18:52:49 +0300

    Add android integration test, multi-flavour app test

---
+++ runners/gradle-integration-tests/testData/androidMultiFlavourApp/app/src/free/kotlin/org/example/kotlin/mixed/free/AdActivity.kt
@@ -0,0 +1,14 @@
+package org.example.kotlin.mixed.free
+
+import android.os.Bundle
+import android.app.Activity
+import org.example.kotlin.mixed.R
+
+class AdActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ad)
+    }
+
+}
