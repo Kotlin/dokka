6805bae192908c287ffd0fcad9ece50541db0e46
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2017-06-26 15:26:14 +0300

    Workaround top-level java class name not match filename

--- core/testdata/format/JavaSupertype.java
+++ core/testdata/format/JavaSupertype.java
@@ -1,7 +1,8 @@
-class C {
+public class JavaSupertype {
   public static class Foo {
   }
 
   public static class Bar extends Foo {
+    public Foo returnFoo(Foo foo) { return foo; }
   }
 }
--- core/testdata/java/InnerClass.java
+++ core/testdata/java/InnerClass.java
@@ -1,4 +1,4 @@
-class Test {
+class InnerClass {
     public class D {
     }
 }
