5e9dd421cd323c138486cf8b38b6ce7158bcca8a
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2017-12-02 05:18:41 +0300

    Bind NodeLocationAwareGenerator to Generator

--- core/src/main/kotlin/Formats/FormatDescriptor.kt
+++ core/src/main/kotlin/Formats/FormatDescriptor.kt
@@ -2,8 +2,6 @@ package org.jetbrains.dokka.Formats
 
 import com.google.inject.Binder
 import org.jetbrains.dokka.*
-import org.jetbrains.dokka.Model.DescriptorSignatureProvider
-import org.jetbrains.dokka.Samples.SampleProcessingService
 import org.jetbrains.dokka.Utilities.bind
 import org.jetbrains.dokka.Utilities.toOptional
 import org.jetbrains.dokka.Utilities.toType
@@ -18,16 +16,17 @@ interface FormatDescriptorOutputComponent {
     fun configureOutput(binder: Binder)
 }
 
-interface FormatDescriptor: FormatDescriptorAnalysisComponent, FormatDescriptorOutputComponent
+interface FormatDescriptor : FormatDescriptorAnalysisComponent, FormatDescriptorOutputComponent
 
 
 abstract class FileGeneratorBasedFormatDescriptor : FormatDescriptor {
 
     override fun configureOutput(binder: Binder): Unit = with(binder) {
+        bind<Generator>() toType NodeLocationAwareGenerator::class
+
         bind<OutlineFormatService>() toOptional (outlineServiceClass)
         bind<FormatService>() toOptional formatServiceClass
-        bind<FileGenerator>() toType generatorServiceClass
-        //bind<Generator>() toType  generatorServiceClass
+        bind<NodeLocationAwareGenerator>() toType generatorServiceClass
         bind<PackageListService>() toOptional packageListServiceClass
     }
 
