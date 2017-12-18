cc716bead20ad9ff116702c1c3297c33e5f3c3f4
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2017-12-18 21:47:29 +0300

    Fix adding file name to relative URI, if it's not different

--- core/src/main/kotlin/Formats/JavaLayoutHtmlFormat.kt
+++ core/src/main/kotlin/Formats/JavaLayoutHtmlFormat.kt
@@ -556,21 +556,26 @@ fun URI.relativeTo(uri: URI): URI {
     val base = uri.normalize()
     val child = this.normalize()
 
-    // Split paths into segments
-    var bParts = base.path.split('/').dropLastWhile { it.isEmpty() }
-    val cParts = child.path.split('/').dropLastWhile { it.isEmpty() }
+    fun StringBuilder.appendRelativePath() {
+        // Split paths into segments
+        var bParts = base.path.split('/').dropLastWhile { it.isEmpty() }
+        val cParts = child.path.split('/').dropLastWhile { it.isEmpty() }
 
-    // Discard trailing segment of base path
-    if (bParts.isNotEmpty() && !base.path.endsWith("/")) {
-        bParts = bParts.dropLast(1)
-    }
+        // Discard trailing segment of base path
+        if (bParts.isNotEmpty() && !base.path.endsWith("/")) {
+            bParts = bParts.dropLast(1)
+        }
 
-    // Compute common prefix
-    val commonPartsSize = bParts.zip(cParts).count { (basePart, childPart) -> basePart == childPart }
-
-    return URI.create(buildString {
+        // Compute common prefix
+        val commonPartsSize = bParts.zip(cParts).count { (basePart, childPart) -> basePart == childPart }
         bParts.drop(commonPartsSize).joinTo(this, separator = "") { "../" }
         cParts.drop(commonPartsSize).joinTo(this, separator = "/")
+    }
+
+    return URI.create(buildString {
+        if (base.path != child.path) {
+            appendRelativePath()
+        }
         child.rawQuery?.let {
             append("?")
             append(it)
