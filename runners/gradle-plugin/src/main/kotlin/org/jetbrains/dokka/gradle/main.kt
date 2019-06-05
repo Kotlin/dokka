50f85dce8229e4ce9b5966c2f08d7179dec4bd02
Kamil Doległo <kamilok1965@interia.pl>
2019-06-05 15:07:22 +0200

    Implement configuration avoidance

--- runners/gradle-plugin/src/main/kotlin/org/jetbrains/dokka/gradle/main.kt
+++ runners/gradle-plugin/src/main/kotlin/org/jetbrains/dokka/gradle/main.kt
@@ -2,7 +2,7 @@ package org.jetbrains.dokka.gradle
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.jetbrains.kotlin.gradle.plugin.KotlinGradleSubplugin
+import org.gradle.util.GradleVersion
 import java.io.File
 import java.io.InputStream
 import java.util.*
@@ -15,16 +15,22 @@ open class DokkaPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         DokkaVersion.loadFrom(javaClass.getResourceAsStream("/META-INF/gradle-plugins/org.jetbrains.dokka.properties"))
 
-        // TODO: Register instead of create for Gradle >= 4.10
-        project.tasks.create("dokka", DokkaTask::class.java).apply {
-            moduleName = project.name
-            outputDirectory = File(project.buildDir, "dokka").absolutePath
-        }
-
         val dokkaRuntimeConfiguration = project.configurations.create("dokkaRuntime")
         val defaultDokkaRuntimeConfiguration = project.configurations.create("defaultDokkaRuntime")
 
         defaultDokkaRuntimeConfiguration.defaultDependencies{ dependencies -> dependencies.add(project.dependencies.create("org.jetbrains.dokka:dokka-fatjar:${DokkaVersion.version}")) }
+
+        if(GradleVersion.current() >= GradleVersion.version("4.10")) {
+            project.tasks.register("dokka", DokkaTask::class.java).configure {
+                it.moduleName = project.name
+                it.outputDirectory = File(project.buildDir, "dokka").absolutePath
+            }
+        } else {
+            project.tasks.create("dokka", DokkaTask::class.java).apply {
+                moduleName = project.name
+                outputDirectory = File(project.buildDir, "dokka").absolutePath
+            }
+        }
 
         project.tasks.withType(DokkaTask::class.java) { task ->
             val passConfiguration = project.container(GradlePassConfigurationImpl::class.java)
