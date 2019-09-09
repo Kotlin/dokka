f43ce671f53621456abf86457e9cae86c648a6be
Kamil Doległo <kamilok1965@interia.pl>
2019-09-09 16:30:41 +0200

    Add a test for multiplatform and kotlinTasks

---
+++ runners/gradle-integration-tests/testData/multiplatformProject/src/jsMain/kotlin/org/kotlintestmpp/main.kt
@@ -0,0 +1,14 @@
+package org.kotlintestmpp
+
+fun main(args : Array<String>) {
+    console.log("Hello, world!")
+}
+
+fun js(){}
+fun shared(){}
+
+fun getCurrentDate(): String {
+    return "test"
+}
+
+fun String.myExtension() = println("test")
