25f545cfb7fb6c32a35124f69ec6466ac64a3ae5
Szymon Świstun <sswistun@virtuslab.com>
2020-02-11 16:54:50 +0100

    removed useless cast

--- core/src/main/kotlin/pages/PageNodes.kt
+++ core/src/main/kotlin/pages/PageNodes.kt
@@ -138,7 +138,7 @@ class MemberPageNode(
     override val embeddedResources: List<String> = listOf()
 ) : ContentPage {
     override fun modified(name: String, children: List<PageNode>): MemberPageNode =
-        modified(name = name, content = this.content, children = children) as MemberPageNode
+        modified(name = name, content = this.content, children = children)
 
     override fun modified(
         name: String,
