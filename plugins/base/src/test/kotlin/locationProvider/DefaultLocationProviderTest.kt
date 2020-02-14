a09cdf52571c9c93ca3244d2fc92577dee3fae24
Kamil Doległo <kamilok1965@interia.pl>
2020-02-14 01:16:54 +0100

    Add test for path clash without `package` directive

---
+++ plugins/base/src/test/kotlin/locationProvider/DefaultLocationProviderTest.kt
@@ -0,0 +1,41 @@
+package locationProvider
+
+import org.jetbrains.dokka.plugability.DokkaContext
+import org.jetbrains.dokka.base.resolvers.local.DefaultLocationProvider
+import org.jetbrains.dokka.testApi.testRunner.AbstractCoreTest
+import org.junit.Assert.assertNotEquals
+import org.junit.Test
+
+class DefaultLocationProviderTest: AbstractCoreTest() {
+    @Test
+    fun `#644 same directory for module and package`() {
+        val configuration = dokkaConfiguration {
+            passes {
+                pass {
+                    sourceRoots = listOf("src/")
+                }
+            }
+        }
+
+        testInline(
+            """
+            |/src/main/kotlin/basic/Test.kt
+            |
+            |class Test {
+            |   val x = 1
+            |}
+        """.trimMargin(),
+            configuration
+        ) {
+            var context: DokkaContext? = null
+            pluginsSetupStage = {
+                context = it
+            }
+
+            pagesGenerationStage = { module ->
+                val lp = DefaultLocationProvider(module, context!!)
+                assertNotEquals(lp.resolve(module.children.single()).removePrefix("/"), lp.resolve(module))
+            }
+        }
+    }
+}
