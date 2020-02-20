638804f7ff636139f4979cde64a1852bb527f3a9
Paweł Marks <pmarks@virtuslab.com>
2020-02-20 16:43:59 +0100

    Sketch of property container

---
+++ core/src/main/kotlin/model/properties/PropertyContainer.kt
@@ -0,0 +1,17 @@
+package org.jetbrains.dokka.model.properties
+
+class PropertyContainer<C : Any> private constructor(
+    @PublishedApi internal val map: Map<Property.Key<C, *>, Property<C>>
+) {
+    operator fun <D : C> plus(prop: Property<D>): PropertyContainer<D> =
+        PropertyContainer(map + (prop.key to prop))
+
+    inline operator fun <reified T: Any> get(key: Property.Key<C, T>): T? = when (val prop = map[key]) {
+        is T? -> prop
+        else -> throw ClassCastException("Property for $key stored under not matching key type.")
+    }
+
+    companion object {
+        val empty: PropertyContainer<Any> = PropertyContainer(emptyMap())
+    }
+}
