43dcb4c708eca9b29bbd98e9e6cdf1e2fa862257
Paweł Marks <pmarks@virtuslab.com>
2020-02-24 12:09:17 +0100

    Adds calculated properties

--- core/src/main/kotlin/model/properties/PropertyContainer.kt
+++ core/src/main/kotlin/model/properties/PropertyContainer.kt
@@ -6,6 +6,7 @@ class PropertyContainer<C : Any> private constructor(
     operator fun <D : C> plus(prop: Property<D>): PropertyContainer<D> =
         PropertyContainer(map + (prop.key to prop))
 
+    // TODO: Add logic for caching calculated properties
     inline operator fun <reified T: Any> get(key: Property.Key<C, T>): T? = when (val prop = map[key]) {
         is T? -> prop
         else -> throw ClassCastException("Property for $key stored under not matching key type.")
