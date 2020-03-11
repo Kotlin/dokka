89744d71a5e1bd849d0cb700c3c751dbf43ad642
Szymon Świstun <sswistun@virtuslab.com>
2020-03-11 15:07:56 +0100

    Add annotations to extra properties

--- core/src/main/kotlin/model/aditionalExtras.kt
+++ core/src/main/kotlin/model/aditionalExtras.kt
@@ -1,5 +1,6 @@
 package org.jetbrains.dokka.model
 
+import org.jetbrains.dokka.links.DRI
 import org.jetbrains.dokka.model.properties.ExtraProperty
 import org.jetbrains.dokka.model.properties.MergeStrategy
 
@@ -14,4 +15,12 @@ class AdditionalModifiers(val content: Set<ExtraModifiers>) : ExtraProperty<Docu
     override fun equals(other: Any?): Boolean = if (other is AdditionalModifiers) other.content == content else false
     override fun hashCode() = content.hashCode()
     override val key: ExtraProperty.Key<Documentable, *> = AdditionalKey
+}
+
+class Annotations(val content: List<Annotation>) : ExtraProperty<Documentable> {
+    companion object : ExtraProperty.Key<Documentable, Annotations>
+
+    override val key: ExtraProperty.Key<Documentable, *> = Annotations
+
+    data class Annotation(val dri: DRI, val params: Map<String, String>)
 }
