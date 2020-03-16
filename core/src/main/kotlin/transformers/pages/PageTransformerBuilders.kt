3de1182033ca5a5db374e16a6ab3555627faa79b
Szymon Świstun <sswistun@virtuslab.com>
2020-03-16 13:08:29 +0100

    Add default values for parameters

--- core/src/main/kotlin/transformers/pages/PageTransformerBuilders.kt
+++ core/src/main/kotlin/transformers/pages/PageTransformerBuilders.kt
@@ -3,15 +3,15 @@ package org.jetbrains.dokka.transformers.pages
 import org.jetbrains.dokka.pages.PageNode
 import org.jetbrains.dokka.pages.RootPageNode
 
-fun pageScanner(block: PageNode.() -> Unit) = object : PageNodeTransformer {
+fun pageScanner(block: PageNode.() -> Unit) = object : PageTransformer {
     override fun invoke(input: RootPageNode): RootPageNode = input.invokeOnAll(block) as RootPageNode
 }
 
-fun pageMapper(block: PageNode.() -> PageNode) = object : PageNodeTransformer {
+fun pageMapper(block: PageNode.() -> PageNode) = object : PageTransformer {
     override fun invoke(input: RootPageNode): RootPageNode = input.alterChildren(block) as RootPageNode
 }
 
-fun pageStructureTransformer(block: RootPageNode.() -> RootPageNode) = object : PageNodeTransformer {
+fun pageStructureTransformer(block: RootPageNode.() -> RootPageNode) = object : PageTransformer {
     override fun invoke(input: RootPageNode): RootPageNode = block(input)
 }
 
