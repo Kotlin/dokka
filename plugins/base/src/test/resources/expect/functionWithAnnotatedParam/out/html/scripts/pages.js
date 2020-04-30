960406d9c116cbd0ee00e2d9c2a06119e8016e99
Paweł Marks <pmarks@virtuslab.com>
2020-04-30 13:24:32 +0200

    Regenerate expects

--- plugins/base/src/test/resources/expect/functionWithAnnotatedParam/out/html/scripts/pages.js
+++ plugins/base/src/test/resources/expect/functionWithAnnotatedParam/out/html/scripts/pages.js
@@ -1,10 +1,10 @@
 var pages = [
 { "name": "root",  "location": "root/index.html" },
 { "name": "[jvm root]",  "location": "root/[jvm root]/index.html" },
+{ "name": "function",  "location": "root/[jvm root]/function.html" },
 { "name": "Fancy", "class": "Fancy", "location": "root/[jvm root]/-fancy/index.html" },
-{ "name": "<init>",  "location": "root/[jvm root]/-fancy/-init-.html" },
 { "name": "equals",  "location": "root/[jvm root]/-fancy/equals.html" },
-{ "name": "hashCode",  "location": "root/[jvm root]/-fancy/hash-code.html" },
+{ "name": "<init>",  "location": "root/[jvm root]/-fancy/-init-.html" },
 { "name": "toString",  "location": "root/[jvm root]/-fancy/to-string.html" },
-{ "name": "function",  "location": "root/[jvm root]/function.html" }
+{ "name": "hashCode",  "location": "root/[jvm root]/-fancy/hash-code.html" }
 ]
