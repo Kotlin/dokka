498a98e29ab2dc1706e36c078c9752caa187e2b0
Marcin Aman <maman@virtuslab.com>
2020-05-11 13:51:26 +0200

    Regenerate expects

--- plugins/base/src/test/resources/expect/functionWithAnnotatedParam/out/html/scripts/pages.js
+++ plugins/base/src/test/resources/expect/functionWithAnnotatedParam/out/html/scripts/pages.js
@@ -3,8 +3,8 @@ var pages = [
 { "name": "[jvm root]",  "location": "root/[jvm root]/index.html" },
 { "name": "function",  "location": "root/[jvm root]/function.html" },
 { "name": "Fancy", "class": "Fancy", "location": "root/[jvm root]/-fancy/index.html" },
-{ "name": "toString",  "location": "root/[jvm root]/-fancy/to-string.html" },
 { "name": "<init>",  "location": "root/[jvm root]/-fancy/-init-.html" },
 { "name": "hashCode",  "location": "root/[jvm root]/-fancy/hash-code.html" },
+{ "name": "toString",  "location": "root/[jvm root]/-fancy/to-string.html" },
 { "name": "equals",  "location": "root/[jvm root]/-fancy/equals.html" }
 ]
