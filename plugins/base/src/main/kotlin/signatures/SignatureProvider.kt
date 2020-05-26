e1ea1614e2fc28e8acf401d4de5dbae7a654ff2e
Andrzej Ratajczak <andrzej.ratajczak98@gmail.com>
2020-05-26 11:05:08 +0200

    Remove unnecessary imports

--- plugins/base/src/main/kotlin/signatures/SignatureProvider.kt
+++ plugins/base/src/main/kotlin/signatures/SignatureProvider.kt
@@ -1,6 +1,6 @@
 package org.jetbrains.dokka.base.signatures
 
-import org.jetbrains.dokka.model.*
+import org.jetbrains.dokka.model.Documentable
 import org.jetbrains.dokka.pages.ContentNode
 
 interface SignatureProvider {
