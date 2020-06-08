99275bdf22b4a290da9c4ba347f049bb460ce129
Andrzej Ratajczak <andrzej.ratajczak98@gmail.com>
2020-06-08 11:34:15 +0200

    Fix after rebase

--- core/src/main/kotlin/pages/PageNodes.kt
+++ core/src/main/kotlin/pages/PageNodes.kt
@@ -180,6 +180,8 @@ class MultimoduleRootPageNode(
 
     override val children: List<PageNode> = emptyList()
 
+    override val enforceDirectory: Boolean = false
+
     override val documentable: Documentable? = null
 
     override fun modified(name: String, children: List<PageNode>): RootPageNode =
