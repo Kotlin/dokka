7add3d2d1780c3ca77e81a9a39a6083420eb9a13
Filip Zybała <fzybala@virtuslab.com>
2020-06-23 12:46:19 +0200

    Repaired frontend gradle module

--- plugins/base/frontend/build.gradle.kts
+++ plugins/base/frontend/build.gradle.kts
@@ -2,16 +2,16 @@ plugins {
     id("com.moowork.node") version "1.3.1"
 }
 
+task("generateFrontendFiles") {
+    dependsOn("npmInstall", "npm_run_build")
+}
+
 tasks {
     val npmRunBuild by registering {
         inputs.dir("$projectDir/src/main/")
         inputs.files("$projectDir/package.json", "$projectDir/webpack.config.js")
         outputs.dir("$projectDir/dist/")
         outputs.cacheIf { true }
-    }
-
-    register("generateFrontendFiles") {
-        dependsOn(npmInstall, npmRunBuild)
     }
 
     clean {
