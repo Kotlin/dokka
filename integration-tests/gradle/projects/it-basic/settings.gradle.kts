b1f0bdc606b9524a9284e6a965562461bec4c7ab
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2020-07-02 09:26:30 +0200

    Introduce special gradle plugin publication for integration tests

--- integration-tests/gradle/projects/it-basic/settings.gradle.kts
+++ integration-tests/gradle/projects/it-basic/settings.gradle.kts
@@ -2,13 +2,18 @@
 
 pluginManagement {
     val dokka_it_kotlin_version: String by settings
-    val dokka_version = "0.11.0-SNAPSHOT"
 
     plugins {
         id("org.jetbrains.kotlin.jvm") version dokka_it_kotlin_version
-        id("org.jetbrains.dokka") version dokka_version
     }
 
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "org.jetbrains.dokka") {
+                useModule("org.jetbrains.dokka:dokka-gradle-plugin:for-integration-tests-SNAPSHOT")
+            }
+        }
+    }
     repositories {
         maven("https://dl.bintray.com/kotlin/kotlin-dev/")
         mavenLocal()
