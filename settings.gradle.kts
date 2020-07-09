ef9191faac392d48fddb86f1ec2702e0e35c814d
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2020-07-09 07:56:44 +0200

    Update gradle-publish to 0.12.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -28,7 +28,7 @@ pluginManagement {
         id("org.jetbrains.kotlin.jvm") version kotlin_version
         id("com.github.johnrengelman.shadow") version "5.2.0"
         id("com.jfrog.bintray") version "1.8.5"
-        id("com.gradle.plugin-publish") version "0.10.1"
+        id("com.gradle.plugin-publish") version "0.12.0"
     }
 
     repositories {
