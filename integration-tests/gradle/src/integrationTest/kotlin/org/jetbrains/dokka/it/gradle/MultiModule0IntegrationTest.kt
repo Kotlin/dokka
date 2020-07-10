bb20739e1e80f0e395dec0189cb9db1438fa4155
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2020-07-10 11:53:53 +0200

    Implement basic MultiModule0IntegrationTest.kt

---
+++ integration-tests/gradle/src/integrationTest/kotlin/org/jetbrains/dokka/it/gradle/MultiModule0IntegrationTest.kt
@@ -0,0 +1,41 @@
+package org.jetbrains.dokka.it.gradle
+
+import org.gradle.testkit.runner.TaskOutcome
+import org.junit.runners.Parameterized
+import java.io.File
+import kotlin.test.*
+
+class MultiModule0IntegrationTest(override val versions: BuildVersions) : AbstractGradleIntegrationTest() {
+    companion object {
+        @get:JvmStatic
+        @get:Parameterized.Parameters(name = "{0}")
+        val versions = BuildVersions.permutations(
+            gradleVersions = listOf("6.5.1", "6.1.1"),
+            kotlinVersions = listOf("1.4-M2-eap-70")
+        )
+    }
+
+    @BeforeTest
+    fun prepareProjectFiles() {
+        val templateProjectDir = File("projects", "it-multimodule-0")
+        templateProjectDir.listFiles().orEmpty()
+            .filter { it.isFile }
+            .forEach { topLevelFile -> topLevelFile.copyTo(File(projectDir, topLevelFile.name)) }
+        File(templateProjectDir, "moduleA").copyRecursively(File(projectDir, "moduleA"))
+    }
+
+    @Test
+    fun execute() {
+        val result = createGradleRunner("dokkaKdoc", ":moduleA:dokkaKdocMultimodule", "--stacktrace").build()
+
+        assertEquals(TaskOutcome.SUCCESS, assertNotNull(result.task(":moduleA:dokkaKdocMultimodule")).outcome)
+
+        val dokkaOutputDir = File(projectDir, "moduleA/build/dokka/kdocMultimodule")
+        assertTrue(dokkaOutputDir.isDirectory, "Missing dokka output directory")
+
+        dokkaOutputDir.allHtmlFiles().forEach { file ->
+            assertContainsNoErrorClass(file)
+            assertNoUnresolvedLInks(file)
+        }
+    }
+}
