f438f18b463108ec895dc64143916b108ce1a631
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2020-07-14 18:40:00 +0200

    Decalre documentation group for all dokka tasks

--- runners/gradle-plugin/src/test/kotlin/org/jetbrains/dokka/gradle/DokkaTasksTest.kt
+++ runners/gradle-plugin/src/test/kotlin/org/jetbrains/dokka/gradle/DokkaTasksTest.kt
@@ -1,8 +1,10 @@
 package org.jetbrains.dokka.gradle
 
+import org.gradle.api.plugins.JavaBasePlugin
 import org.gradle.kotlin.dsl.withType
 import org.gradle.testfixtures.ProjectBuilder
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertSame
 import kotlin.test.assertTrue
 
@@ -49,4 +51,15 @@ class DokkaTasksTest {
         }
     }
 
+    @Test
+    fun `all dokka tasks are part of the documentation group`() {
+        val project = ProjectBuilder.builder().build()
+        project.plugins.apply("org.jetbrains.dokka")
+        project.tasks.filter { "dokka" in it.name.toLowerCase() }.forEach { dokkaTask ->
+            assertEquals(
+                JavaBasePlugin.DOCUMENTATION_GROUP, dokkaTask.group,
+                "Expected task: ${dokkaTask.path} group to be ${JavaBasePlugin.DOCUMENTATION_GROUP}"
+            )
+        }
+    }
 }
