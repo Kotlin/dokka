18c2b9daefc0667f430c63a4f990338ea7099ce9
Andrzej Ratajczak <andrzej.ratajczak98@gmail.com>
2020-07-14 11:36:47 +0200

    Refactor and add integration tests

--- integration-tests/maven/src/integrationTest/kotlin/org/jetbrains/dokka/it/maven/MavenIntegrationTest.kt
+++ integration-tests/maven/src/integrationTest/kotlin/org/jetbrains/dokka/it/maven/MavenIntegrationTest.kt
@@ -2,6 +2,7 @@ package org.jetbrains.dokka.it.maven
 
 import org.jetbrains.dokka.it.AbstractIntegrationTest
 import org.jetbrains.dokka.it.awaitProcessResult
+import org.jetbrains.dokka.it.ProcessResult
 import java.io.File
 import kotlin.test.BeforeTest
 import kotlin.test.Test
@@ -26,10 +27,84 @@ class MavenIntegrationTest : AbstractIntegrationTest() {
     }
 
     @Test
-    fun run() {
+    fun `dokka dokka`() {
         val result = ProcessBuilder().directory(projectDir)
             .command(mavenBinaryFile.absolutePath, "dokka:dokka", "-U", "-e").start().awaitProcessResult()
 
+        diagnosticAsserts(result)
+
+        val dokkaOutputDir = File(projectDir, "output")
+        assertTrue(dokkaOutputDir.isDirectory, "Missing dokka output directory")
+
+        val imagesDir = File(dokkaOutputDir, "images")
+        assertTrue(imagesDir.isDirectory, "Missing images directory")
+
+        val scriptsDir = File(dokkaOutputDir, "scripts")
+        assertTrue(scriptsDir.isDirectory, "Missing scripts directory")
+
+        val stylesDir = File(dokkaOutputDir, "styles")
+        assertTrue(stylesDir.isDirectory, "Missing styles directory")
+
+        val navigationHtml = File(dokkaOutputDir, "navigation.html")
+        assertTrue(navigationHtml.isFile, "Missing navigation.html")
+
+        projectDir.allHtmlFiles().forEach { file ->
+            assertContainsNoErrorClass(file)
+            assertNoUnresolvedLInks(file)
+        }
+    }
+
+    @Test
+    fun `dokka javadoc`() {
+        val result = ProcessBuilder().directory(projectDir)
+            .command(mavenBinaryFile.absolutePath, "dokka:javadoc", "-U", "-e").start().awaitProcessResult()
+
+        diagnosticAsserts(result)
+
+        val dokkaOutputDir = File(projectDir, "output")
+        assertTrue(dokkaOutputDir.isDirectory, "Missing dokka output directory")
+
+        val scriptsDir = File(dokkaOutputDir, "jquery")
+        assertTrue(scriptsDir.isDirectory, "Missing jquery directory")
+
+        val stylesDir = File(dokkaOutputDir, "resources")
+        assertTrue(stylesDir.isDirectory, "Missing resources directory")
+
+        projectDir.allHtmlFiles().forEach { file ->
+            assertContainsNoErrorClass(file)
+            assertNoUnresolvedLInks(file)
+        }
+    }
+
+    @Test
+    fun `dokka javadocJar`() {
+        val result = ProcessBuilder().directory(projectDir)
+            .command(mavenBinaryFile.absolutePath, "dokka:javadocJar", "-U", "-e").start().awaitProcessResult()
+
+        diagnosticAsserts(result)
+
+        val dokkaOutputDir = File(projectDir, "output")
+        assertTrue(dokkaOutputDir.isDirectory, "Missing dokka output directory")
+
+        val scriptsDir = File(dokkaOutputDir, "jquery")
+        assertTrue(scriptsDir.isDirectory, "Missing jquery directory")
+
+        val stylesDir = File(dokkaOutputDir, "resources")
+        assertTrue(stylesDir.isDirectory, "Missing resources directory")
+
+        val dokkaTargetDir = File(projectDir, "target")
+        assertTrue(dokkaOutputDir.isDirectory, "Missing dokka target directory")
+
+        val jarFile = File(dokkaTargetDir, "it-maven-1.0-SNAPSHOT-javadoc.jar")
+        assertTrue(jarFile.isFile, "Missing dokka jar file")
+
+        projectDir.allHtmlFiles().forEach { file ->
+            assertContainsNoErrorClass(file)
+            assertNoUnresolvedLInks(file)
+        }
+    }
+
+    private fun diagnosticAsserts(result: ProcessResult) {
         assertEquals(0, result.exitCode, "Expected exitCode 0 (Success)")
 
         val extensionLoadedRegex = Regex("""Extension: org\.jetbrains\.dokka\.base\.DokkaBase""")
@@ -53,26 +128,5 @@ class MavenIntegrationTest : AbstractIntegrationTest() {
             amountOfUndocumentedJavaReports > 0,
             "Expected at least one report of undocumented java code (found $amountOfUndocumentedJavaReports)"
         )
-
-        val dokkaOutputDir = File(projectDir, "output")
-        assertTrue(dokkaOutputDir.isDirectory, "Missing dokka output directory")
-
-        val imagesDir = File(dokkaOutputDir, "images")
-        assertTrue(imagesDir.isDirectory, "Missing images directory")
-
-        val scriptsDir = File(dokkaOutputDir, "scripts")
-        assertTrue(scriptsDir.isDirectory, "Missing scripts directory")
-
-        val stylesDir = File(dokkaOutputDir, "styles")
-        assertTrue(stylesDir.isDirectory, "Missing styles directory")
-
-        val navigationHtml = File(dokkaOutputDir, "navigation.html")
-        assertTrue(navigationHtml.isFile, "Missing navigation.html")
-
-        projectDir.allHtmlFiles().forEach { file ->
-            assertContainsNoErrorClass(file)
-            assertNoUnresolvedLInks(file)
-        }
-
     }
 }
