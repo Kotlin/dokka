a45d7c3e02aaf50775cfd49b7fc6946c144fe80b
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2020-07-17 22:36:53 +0200

    Upgrade kotlinx-html to 0.7.1-1.4-M3

--- plugins/javadoc/build.gradle.kts
+++ plugins/javadoc/build.gradle.kts
@@ -2,10 +2,12 @@ import org.jetbrains.registerDokkaArtifactPublication
 
 dependencies {
     implementation("com.soywiz.korlibs.korte:korte-jvm:1.10.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.6.10")
     implementation(project(":plugins:base"))
     implementation(project(":plugins:kotlin-as-java"))
     testImplementation(project(":plugins:base:test-utils"))
+
+    val kotlinx_html_version: String by project
+    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:$kotlinx_html_version")
 
     val coroutines_version: String by project
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutines_version")
