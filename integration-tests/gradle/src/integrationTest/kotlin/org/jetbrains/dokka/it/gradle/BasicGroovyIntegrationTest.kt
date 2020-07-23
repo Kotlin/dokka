10fbd60612b2668afa2e430851773741e6debcd6
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2020-07-23 15:11:15 +0200

    Update to Kotlin version 1.4.0-rc

--- integration-tests/gradle/src/integrationTest/kotlin/org/jetbrains/dokka/it/gradle/BasicGroovyIntegrationTest.kt
+++ integration-tests/gradle/src/integrationTest/kotlin/org/jetbrains/dokka/it/gradle/BasicGroovyIntegrationTest.kt
@@ -16,7 +16,7 @@ class BasicGroovyIntegrationTest(override val versions: BuildVersions) : Abstrac
         @get:Parameterized.Parameters(name = "{0}")
         val versions = BuildVersions.permutations(
             gradleVersions = listOf("6.5.1", "5.6.4"),
-            kotlinVersions = listOf("1.4-M3")
+            kotlinVersions = listOf("1.4.0-rc")
         )
     }
 
