59d9d25b3949a542a8ded20406f2fbf6cac59590
Marcin Aman <maman@virtuslab.com>
2020-07-23 16:14:46 +0200

    Fix truncated text on sidebar with long packages

--- plugins/base/src/main/kotlin/renderers/html/NavigationPage.kt
+++ plugins/base/src/main/kotlin/renderers/html/NavigationPage.kt
@@ -25,9 +25,9 @@ class NavigationPage(val root: NavigationNode) : RendererSpecificPage {
                 id = navId
                 attributes["pageId"] = node.dri.toString()
                 div("overview") {
-                    buildLink(node.dri, node.sourceSets.toList()) { +node.name }
+                    buildLink(node.dri, node.sourceSets.toList()) { buildBreakableDotSeparatedHtml(node.name) }
                     if (node.children.isNotEmpty()) {
-                        span("navButton") {
+                        span("navButton pull-right") {
                             onClick = """document.getElementById("$navId").classList.toggle("hidden");"""
                             span("navButtonContent")
                         }
