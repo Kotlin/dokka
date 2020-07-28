d0f707af0199a99085dc40693628acbf7e543abe
Marcin Aman <maman@virtuslab.com>
2020-07-28 16:57:07 +0200

    Highlight searched phrase

--- plugins/base/frontend/src/main/types/@jetbrains/index.d.ts
+++ plugins/base/frontend/src/main/types/@jetbrains/index.d.ts
@@ -1,3 +1,10 @@
 declare module '@jetbrains/ring-ui' {
     export const Select: any;
+    export const List: any;
+}
+
+declare module '@jetbrains/ring-ui/components/global/fuzzy-highlight.js' {
+    import {Option} from "../../components/search/types";
+
+    export const fuzzyHighlight: (string, string, boolean) => Option[]
 }
