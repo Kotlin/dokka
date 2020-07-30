16fc12808d2f00932822880cdea5ed006a100c2a
Marcin Aman <maman@virtuslab.com>
2020-07-30 13:37:53 +0200

    Remove unsafe html usage

--- plugins/base/frontend/src/main/types/@jetbrains/index.d.ts
+++ plugins/base/frontend/src/main/types/@jetbrains/index.d.ts
@@ -2,3 +2,7 @@ declare module '@jetbrains/ring-ui' {
     export const Select: any;
     export const List: any;
 }
+
+declare module '@jetbrains/ring-ui/components/markdown/markdown' {
+    export const Markdown: any
+}
