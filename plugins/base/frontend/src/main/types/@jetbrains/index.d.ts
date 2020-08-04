08845a66bb86e824f14695a96aa5062ed64edb20
Marcin Aman <maman@virtuslab.com>
2020-08-04 11:02:51 +0200

    Fix JS markdown component generating lots of files with highlighting

--- plugins/base/frontend/src/main/types/@jetbrains/index.d.ts
+++ plugins/base/frontend/src/main/types/@jetbrains/index.d.ts
@@ -2,7 +2,3 @@ declare module '@jetbrains/ring-ui' {
     export const Select: any;
     export const List: any;
 }
-
-declare module '@jetbrains/ring-ui/components/markdown/markdown' {
-    export const Markdown: any
-}
