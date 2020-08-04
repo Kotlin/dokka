201df32db44beaaecdf88ed62fb6b4591ca30391
Marcin Aman <maman@virtuslab.com>
2020-08-04 11:02:51 +0200

    Fix JS markdown component generating lots of files with highlighting

--- plugins/base/frontend/src/main/components/search/searchResultRow.tsx
+++ plugins/base/frontend/src/main/components/search/searchResultRow.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import {OptionWithSearchResult, SearchProps, SearchRank} from "./types";
-import Markdown from '@jetbrains/ring-ui/components/markdown/markdown';
+import _ from "lodash";
+
+type HighlighterProps = {
+    label: string
+}
+
+const Highlighter: React.FC<HighlighterProps> = ({label}: HighlighterProps) => {
+    return <strong>{label}</strong>
+}
 
 export const SearchResultRow: React.FC<SearchProps> = ({searchResult}: SearchProps) => {
     const signatureFromSearchResult = (searchResult: OptionWithSearchResult): string => {
@@ -10,9 +18,17 @@ export const SearchResultRow: React.FC<SearchProps> = ({searchResult}: SearchPro
         return searchResult.highlight
     }
 
+    /*
+    This is a work-around for an issue: https://youtrack.jetbrains.com/issue/RG-2108
+    */
+    const out = _.chunk(signatureFromSearchResult(searchResult).split('**'), 2).flatMap(([txt, label]) => [
+        txt,
+        label ? <Highlighter label={label}></Highlighter> : null,
+      ]);
+
     return (
         <div className="template-wrapper">
-            <span><Markdown source={signatureFromSearchResult(searchResult)}/></span>
+            <span>{out}</span>
             <span className="template-description">{searchResult.description}</span>
         </div>
     )
