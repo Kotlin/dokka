fb48028000cb760837ba25cf148a287c893fee37
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2020-08-13 18:18:20 +0200

    Implement shouldRenderSourceSetBubbles

---
+++ plugins/base/src/main/kotlin/renderers/html/shouldRenderSourceSetBubbles.kt
@@ -0,0 +1,16 @@
+package org.jetbrains.dokka.base.renderers.html
+
+import org.jetbrains.dokka.model.withDescendants
+import org.jetbrains.dokka.pages.ContentPage
+import org.jetbrains.dokka.pages.RootPageNode
+
+internal fun shouldRenderSourceSetBubbles(page: RootPageNode): Boolean {
+    return page.withDescendants()
+        .flatMap { pageNode ->
+            if (pageNode is ContentPage) pageNode.content.withDescendants()
+            else emptySequence()
+        }
+        .flatMap { contentNode -> contentNode.sourceSets }
+        .distinct()
+        .count() > 1
+}
