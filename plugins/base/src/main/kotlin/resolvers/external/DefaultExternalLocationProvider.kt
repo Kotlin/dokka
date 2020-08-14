116fc3e188c89cc0386a97cd149da67fd695e37b
Kamil Doległo <kamilok1965@interia.pl>
2020-08-14 13:42:12 +0200

    Rename `ExternalDocumentationInfo` to `ExternalDocumentation`

--- plugins/base/src/main/kotlin/resolvers/external/DefaultExternalLocationProvider.kt
+++ plugins/base/src/main/kotlin/resolvers/external/DefaultExternalLocationProvider.kt
@@ -1,17 +1,17 @@
 package org.jetbrains.dokka.base.resolvers.external
 
 import org.jetbrains.dokka.base.resolvers.local.DokkaLocationProvider.Companion.identifierToFilename
-import org.jetbrains.dokka.base.resolvers.shared.ExternalDocumentationInfo
+import org.jetbrains.dokka.base.resolvers.shared.ExternalDocumentation
 import org.jetbrains.dokka.links.DRI
 import org.jetbrains.dokka.plugability.DokkaContext
 
 open class DefaultExternalLocationProvider(
-    val externalDocumentationInfo: ExternalDocumentationInfo,
+    val externalDocumentation: ExternalDocumentation,
     val extension: String,
     val dokkaContext: DokkaContext
 ) : ExternalLocationProvider {
     override fun resolve(dri: DRI): String? { // TODO: classes without packages?
-        val docURL = externalDocumentationInfo.documentationURL.toString().removeSuffix("/") + "/"
+        val docURL = externalDocumentation.documentationURL.toString().removeSuffix("/") + "/"
         val classNamesChecked = dri.classNames ?: return "$docURL${dri.packageName ?: ""}/index$extension"
         val classLink = (listOfNotNull(dri.packageName) + classNamesChecked.split('.'))
             .joinToString("/", transform = ::identifierToFilename)
