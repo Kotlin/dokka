5720940210678a247c5e432f0e2645cf112f9072
Kamil Doległo <kamilok1965@interia.pl>
2020-08-14 13:54:55 +0200

    Rename `doOpenConnectionToReadContent` to `readContent` and make it return `InputStream`

--- plugins/base/src/main/kotlin/resolvers/shared/utils.kt
+++ plugins/base/src/main/kotlin/resolvers/shared/utils.kt
@@ -1,36 +1,37 @@
 package org.jetbrains.dokka.base.resolvers.shared
 
+import java.io.InputStream
 import java.net.HttpURLConnection
 import java.net.URL
 import java.net.URLConnection
 
-internal fun URL.doOpenConnectionToReadContent(timeout: Int = 10000, redirectsAllowed: Int = 16): URLConnection {
-    val connection = this.openConnection().apply {
-        connectTimeout = timeout
-        readTimeout = timeout
-    }
+internal fun URL.readContent(timeout: Int = 10000, redirectsAllowed: Int = 16): InputStream {
+    fun URL.doOpenConnection(timeout: Int, redirectsAllowed: Int): URLConnection {
+        val connection = this.openConnection().apply {
+            connectTimeout = timeout
+            readTimeout = timeout
+        }
 
-    when (connection) {
-        is HttpURLConnection -> {
-            return when (connection.responseCode) {
-                in 200..299 -> {
-                    connection
-                }
+        when (connection) {
+            is HttpURLConnection -> return when (connection.responseCode) {
+                in 200..299 -> connection
+
                 HttpURLConnection.HTTP_MOVED_PERM,
                 HttpURLConnection.HTTP_MOVED_TEMP,
                 HttpURLConnection.HTTP_SEE_OTHER -> {
                     if (redirectsAllowed > 0) {
                         val newUrl = connection.getHeaderField("Location")
-                        URL(newUrl).doOpenConnectionToReadContent(timeout, redirectsAllowed - 1)
+                        URL(newUrl).doOpenConnection(timeout, redirectsAllowed - 1)
                     } else {
                         throw RuntimeException("Too many redirects")
                     }
                 }
-                else -> {
-                    throw RuntimeException("Unhandled http code: ${connection.responseCode}")
-                }
+
+                else -> throw RuntimeException("Unhandled HTTP code: ${connection.responseCode}")
             }
+
+            else -> return connection
         }
-        else -> return connection
     }
+    return doOpenConnection(timeout, redirectsAllowed).getInputStream()
 }
