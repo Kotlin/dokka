88e9bdc88f804f29855b7991a3cf107d40378812
Kamil Doległo <kamilok1965@interia.pl>
2020-08-14 14:05:27 +0200

    Split `PackageList.load()` into several methods

--- plugins/base/src/main/kotlin/resolvers/shared/PackageList.kt
+++ plugins/base/src/main/kotlin/resolvers/shared/PackageList.kt
@@ -16,27 +16,34 @@ data class PackageList(
                 return null
 
             val packageListStream = url.readContent()
-            val (params, packages) =
-                packageListStream
-                    .bufferedReader()
-                    .useLines { lines -> lines.partition { it.startsWith(PackageListService.DOKKA_PARAM_PREFIX) } }
 
-            val paramsMap = params.asSequence()
-                .map { it.removePrefix("${PackageListService.DOKKA_PARAM_PREFIX}.").split(":", limit = 2) }
-                .groupBy({ (key, _) -> key }, { (_, value) -> value })
 
-            val format = paramsMap["format"]?.singleOrNull()?.let { RecognizedLinkFormat.fromString(it) } ?: when {
-                jdkVersion < 8 -> RecognizedLinkFormat.Javadoc1 // Covers JDK 1 - 7
-                jdkVersion < 10 -> RecognizedLinkFormat.Javadoc8 // Covers JDK 8 - 9
-                else -> RecognizedLinkFormat.Javadoc10 // Covers JDK 10+
-            }
+            val (params, packages) = packageListStream
+                .bufferedReader()
+                .useLines { lines -> lines.partition { it.startsWith(PackageListService.DOKKA_PARAM_PREFIX) } }
 
-            val locations = paramsMap["location"].orEmpty()
-                .map { it.split("\u001f", limit = 2) }
-                .map { (key, value) -> key to value }
-                .toMap()
+            val paramsMap = splitParams(params)
+            val format = linkFormat(paramsMap["format"]?.singleOrNull(), jdkVersion)
+            val locations = splitLocations(paramsMap["location"].orEmpty())
 
             return PackageList(format, packages.toSet(), locations, url)
         }
+
+
+        private fun splitParams(params: List<String>) = params.asSequence()
+            .map { it.removePrefix("${PackageListService.DOKKA_PARAM_PREFIX}.").split(":", limit = 2) }
+            .groupBy({ (key, _) -> key }, { (_, value) -> value })
+
+        private fun splitLocations(locations: List<String>) = locations.map { it.split("\u001f", limit = 2) }
+            .map { (key, value) -> key to value }
+            .toMap()
+
+        private fun linkFormat(formatName: String?, jdkVersion: Int) =
+            formatName?.let { RecognizedLinkFormat.fromString(it) }
+                ?: when {
+                    jdkVersion < 8 -> RecognizedLinkFormat.Javadoc1 // Covers JDK 1 - 7
+                    jdkVersion < 10 -> RecognizedLinkFormat.Javadoc8 // Covers JDK 8 - 9
+                    else -> RecognizedLinkFormat.Javadoc10 // Covers JDK 10+
+                }
     }
 }
