1115a144d749d07acd3392be89b6c89f47d51c21
Marcin Aman <maman@virtuslab.com>
2020-08-20 16:19:42 +0200

    Include interfaces to abstract model

---
+++ core/src/main/kotlin/pages/Pages.kt
@@ -0,0 +1,11 @@
+package org.jetbrains.dokka.pages
+
+interface MultimoduleRootPage : ContentPage
+
+interface ModulePage : ContentPage
+
+interface PackagePage : ContentPage
+
+interface ClasslikePage : ContentPage
+
+interface MemberPage : ContentPage
