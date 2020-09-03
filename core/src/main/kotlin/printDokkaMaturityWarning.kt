d6f8bcf84752c0e47e599aab4fc6c7c69a52086d
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2020-09-03 09:35:49 +0200

    Update Dokka maturity warning

--- core/src/main/kotlin/printDokkaMaturityWarning.kt
+++ core/src/main/kotlin/printDokkaMaturityWarning.kt
@@ -3,5 +3,5 @@ package org.jetbrains.dokka
 import org.jetbrains.dokka.utilities.DokkaLogger
 
 internal fun printDokkaMaturityWarning(logger: DokkaLogger) {
-    logger.warn("dokka 1.4.* is an alpha project")
+    logger.warn("Dokka 1.4.* is an alpha project")
 }
