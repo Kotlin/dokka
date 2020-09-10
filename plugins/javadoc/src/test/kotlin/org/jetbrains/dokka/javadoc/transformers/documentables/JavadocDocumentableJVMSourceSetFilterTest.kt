30d6e6c3d82993e76c3942fc9faf102f36736673
Błażej Kardyś <bkardys@virtuslab.com>
2020-09-10 16:54:16 +0200

    Adding javadoc source set based filtering

---
+++ plugins/javadoc/src/test/kotlin/org/jetbrains/dokka/javadoc/transformers/documentables/JavadocDocumentableJVMSourceSetFilterTest.kt
@@ -0,0 +1,90 @@
+package org.jetbrains.dokka.javadoc.transformers.documentables
+
+import org.jetbrains.dokka.DokkaSourceSetID
+import org.jetbrains.dokka.testApi.testRunner.AbstractCoreTest
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class JavadocDocumentableJVMSourceSetFilterTest: AbstractCoreTest() {
+
+    private val config = dokkaConfiguration {
+        format = "javadoc"
+        sourceSets {
+            sourceSet {
+                sourceRoots = listOf("jvmSrc/")
+                analysisPlatform = "jvm"
+                name = "jvm"
+                dependentSourceSets = setOf(DokkaSourceSetID("root", "common"))
+            }
+            sourceSet {
+                sourceRoots = listOf("jsSrc/")
+                analysisPlatform = "js"
+                name = "js"
+            }
+            sourceSet {
+                sourceRoots = listOf("commonSrc/")
+                analysisPlatform = "common"
+                name = "common"
+            }
+            sourceSet {
+                sourceRoots = listOf("otherCommonSrc/")
+                analysisPlatform = "common"
+                name = "otherCommon"
+            }
+        }
+    }
+    private val query = """
+            /jvmSrc/source0.kt
+            package package0
+            /** 
+            * Documentation for ClassA 
+            */
+            class ClassA {
+                fun a() {}
+                fun b() {}
+                fun c() {}
+            }
+            
+            /jsSrc/source1.kt
+            package package1
+            /**
+            * Documentation for ClassB
+            */
+            class ClassB {
+                fun d() {}
+                fun e() {}
+                fun f() {}
+            }
+            
+            /commonSrc/source2.kt
+            package package1
+            /**
+            * Documentation for ClassC
+            */
+            class ClassC {
+                fun g() {}
+                fun h() {}
+                fun j() {}
+            }
+            
+            /otherCommonSrc/source3.kt
+            package package1
+            /**
+            * Documentation for ClassD
+            */
+            class ClassD {
+                fun l() {}
+                fun m() {}
+                fun n() {}
+            }
+        """.trimIndent()
+
+    @Test
+    fun `non-jvm and not dependent common source sets are ommited`() {
+        testInline(query, config) {
+            documentablesFirstTransformationStep = { modules ->
+                assertEquals(4, modules.size)
+            }
+        }
+    }
+}
