3d07ebabdf0050171e12f0314fa80fc8391b8be8
Kamil Doległo <kamilok1965@interia.pl>
2020-09-16 09:29:25 +0200

    Add integration tests for Kotlin projects

---
+++ integration-tests/gradle/projects/stdlib/template.settings.gradle.kts
@@ -0,0 +1,1 @@
+../template.settings.gradle.kts
---
+++ integration-tests/gradle/src/integrationTest/kotlin/org/jetbrains/dokka/it/gradle/kotlin/StdlibGradleIntegrationTest.kt
@@ -0,0 +1,50 @@
+package org.jetbrains.dokka.it.gradle.kotlin
+
+import org.gradle.testkit.runner.TaskOutcome
+import org.jetbrains.dokka.it.S3Project
+import org.jetbrains.dokka.it.copyAndApplyGitDiff
+import org.jetbrains.dokka.it.gradle.*
+import org.junit.runners.Parameterized
+import java.io.File
+import kotlin.test.*
+
+class StdlibGradleIntegrationTest(override val versions: BuildVersions) : AbstractGradleIntegrationTest(),
+    S3Project {
+
+    companion object {
+        @get:JvmStatic
+        @get:Parameterized.Parameters(name = "{0}")
+        val versions = BuildVersions.permutations(
+            gradleVersions = listOf("5.6"),
+            kotlinVersions = listOf("1.4.10")
+        )
+    }
+
+    override val projectOutputLocation: File by lazy { File(projectDir, "build/dokka/kotlin-stdlib") }
+
+    @BeforeTest
+    fun prepareProjectFiles() {
+        val templateProjectDir = File("projects", "stdlib/kotlin-dokka-stdlib")
+        templateProjectDir.listFiles().orEmpty()
+            .forEach { topLevelFile -> topLevelFile.copyRecursively(File(projectDir, topLevelFile.name)) }
+
+        copyAndApplyGitDiff(File("projects", "stdlib/stdlib.diff"))
+    }
+
+    @Test
+    fun execute() {
+        val result = createGradleRunner("callDokka", "-i", "-s").buildRelaxed()
+
+        assertEquals(TaskOutcome.SUCCESS, assertNotNull(result.task(":callDokka")).outcome)
+
+        assertTrue(projectOutputLocation.isDirectory, "Missing dokka output directory")
+
+        projectOutputLocation.allHtmlFiles().forEach { file ->
+            assertContainsNoErrorClass(file)
+//            assertNoUnresolvedLinks(file)
+//            assertNoHrefToMissingLocalFileOrDirectory(file)
+            assertNoEmptyLinks(file)
+            assertNoEmptySpans(file)
+        }
+    }
+}
