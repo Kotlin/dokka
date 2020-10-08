643d955a6ba27db4ffb952ec72b9e995c23e9a64
Błażej Kardyś <bkardys@virtuslab.com>
2020-10-08 16:35:04 +0200

    Adding properties pages for enum entries

--- plugins/base/src/main/kotlin/translators/documentables/DefaultPageCreator.kt
+++ plugins/base/src/main/kotlin/translators/documentables/DefaultPageCreator.kt
@@ -45,7 +45,8 @@ open class DefaultPageCreator(
         ClasslikePageNode(
             e.name, contentForEnumEntry(e), setOf(e.dri), e,
             e.classlikes.renameClashingDocumentable().map(::pageForClasslike) +
-                    e.filteredFunctions.renameClashingDocumentable().map(::pageForFunction)
+                    e.filteredFunctions.renameClashingDocumentable().map(::pageForFunction) +
+                    e.properties.renameClashingDocumentable().mapNotNull(::pageForProperty)
         )
 
     open fun pageForClasslike(c: DClasslike): ClasslikePageNode {
