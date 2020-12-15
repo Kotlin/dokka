a11745b96c66512e0d05318cfabc8ac4471fd1cf
Marcin Aman <marcin.aman@gmail.com>
2020-12-15 16:08:13 +0100

    Remove warning that caused dokka to fail with failOnWarning flag (#1669)

--- core/src/main/kotlin/printDokkaMaturityWarning.kt
+++
@@ -1,7 +0,0 @@
-package org.jetbrains.dokka
-
-import org.jetbrains.dokka.utilities.DokkaLogger
-
-internal fun printDokkaMaturityWarning(logger: DokkaLogger) {
-    logger.warn("Dokka 1.4.* is an alpha project")
-}
