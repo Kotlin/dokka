f353eafe299f3f374b271488a3de25b6ea539fe1
Marcin Aman <marcin.aman@gmail.com>
2021-01-04 10:27:10 +0100

    JvmField annotation (#1678)

---
+++ plugins/kotlin-as-java/src/test/kotlin/JvmFieldTest.kt
@@ -0,0 +1,81 @@
+package kotlinAsJavaPlugin
+
+import org.jetbrains.dokka.base.testApi.testRunner.BaseAbstractTest
+import org.jetbrains.dokka.model.JavaVisibility
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+
+class JvmFieldTest : BaseAbstractTest() {
+    val configuration = dokkaConfiguration {
+        sourceSets {
+            sourceSet {
+                sourceRoots = listOf("src/")
+                classpath += jvmStdlibPath!!
+            }
+        }
+    }
+
+    @Test
+    fun `should keep properties annotated with JvmField as properties`() {
+        testInline(
+            """
+            |/src/main/kotlin/kotlinAsJavaPlugin/sample.kt
+            |package kotlinAsJavaPlugin
+            |class SampleClass(@JvmField val property: String, val otherProperty: String)
+        """.trimMargin(),
+            configuration,
+        ) {
+            documentablesTransformationStage = { module ->
+                val classLike = module.packages.flatMap { it.classlikes }.first()
+                assertNotNull(classLike.properties.firstOrNull { it.name == "property" })
+                assertEquals(
+                    listOf("getOtherProperty", "equals", "hashCode", "toString"),
+                    classLike.functions.map { it.name })
+            }
+        }
+    }
+
+    @Test
+    fun `should work for top-level property`(){
+        testInline(
+            """
+            |/src/main/kotlin/kotlinAsJavaPlugin/sample.kt
+            |package kotlinAsJavaPlugin
+            |@JvmField
+            |val property: String = TODO()
+        """.trimMargin(),
+            configuration,
+        ) {
+            documentablesTransformationStage = { module ->
+                val classLike = module.packages.flatMap { it.classlikes }.first()
+                assertNotNull(classLike.properties.firstOrNull { it.name == "property" })
+                assertEquals(
+                    emptyList(),
+                    classLike.functions.map { it.name })
+            }
+        }
+    }
+
+    @Test
+    fun `properties without JvmName should be kept private`() {
+        testInline(
+            """
+            |/src/main/kotlin/kotlinAsJavaPlugin/sample.kt
+            |package kotlinAsJavaPlugin
+            |class SampleClass(val property: String)
+        """.trimMargin(),
+            configuration,
+        ) {
+            documentablesTransformationStage = { module ->
+                val classLike = module.packages.flatMap { it.classlikes }.first()
+                assertEquals(JavaVisibility.Private, classLike.properties.firstOrNull()?.visibility?.values?.first())
+                assertNotNull(classLike.functions.firstOrNull { it.name.startsWith("get") })
+                assertEquals(
+                    JavaVisibility.Public,
+                    classLike.functions.first { it.name.startsWith("get") }.visibility.values.first()
+                )
+            }
+        }
+    }
+}
