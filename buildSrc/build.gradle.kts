eca846bc4031f4d9f541cdf29856317dc136c4fa
Kamil Doległo <kamilok1965@interia.pl>
2021-02-23 16:39:09 +0100

    Use Nexus publish plugin for publishing to Sonatype

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,9 +5,11 @@ plugins {
 repositories {
     mavenCentral()
     jcenter()
+    maven("https://plugins.gradle.org/m2/")
 }
 
 dependencies {
     implementation("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
     implementation("com.github.jengelman.gradle.plugins:shadow:2.0.4")
+    implementation("io.github.gradle-nexus:publish-plugin:1.0.0")
 }
