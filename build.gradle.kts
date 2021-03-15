f312a892e0ff328adb550b77d55e9c4dbd48bded
Kamil Doległo <kamilok1965@interia.pl>
2021-03-15 22:11:01 +0100

    Bump internal Dokka version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") apply false
     id("java")
-    id("org.jetbrains.dokka") version "1.4.20"
+    id("org.jetbrains.dokka") version "1.4.30"
     id("io.github.gradle-nexus.publish-plugin")
 }
 
