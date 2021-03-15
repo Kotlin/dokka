b84d5e1d0dc48cacb3ebf3956c1c75f5f162167d
Kamil Doległo <kamilok1965@interia.pl>
2021-03-15 18:55:21 +0100

    Fix a bug with closing Sonatype repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -89,5 +89,7 @@ nexusPublishing {
 }
 
 tasks.maybeCreate("dokkaPublish").run {
-    finalizedBy(tasks.named("closeAndReleaseSonatypeStagingRepository"))
+    if (publicationChannels.any { it.isMavenRepository }) {
+        finalizedBy(tasks.named("closeAndReleaseSonatypeStagingRepository"))
+    }
 }
