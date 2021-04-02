54b53bd06d83fb8f0def3c09daeae6fb1835e66c
Marcin Aman <marcin.aman@gmail.com>
2021-04-02 14:04:09 +0200

    1.4.31 test

--- kotlin-analysis/intellij-dependency/build.gradle.kts
+++ kotlin-analysis/intellij-dependency/build.gradle.kts
@@ -1,5 +1,6 @@
 import org.jetbrains.DokkaPublicationBuilder.Component.Shadow
 import org.jetbrains.registerDokkaArtifactPublication
+import java.net.URL
 
 plugins {
     id("com.github.johnrengelman.shadow")
@@ -14,6 +15,8 @@ repositories {
     maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-ide")
     maven("https://kotlin.bintray.com/kotlin-ide-plugin-dependencies")
     maven("https://jetbrains.bintray.com/intellij-third-party-dependencies")
+    maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-ide-plugin-dependencies")
+    maven("https://cache-redirector.jetbrains.com/intellij-dependencies")
 }
 
 val intellijCore: Configuration by configurations.creating
@@ -37,6 +40,37 @@ dependencies {
 }
 
 tasks {
+    val getDependenciesUrls by creating {
+        project.repositories.forEach { repository ->
+            val dependencyGroup = "org.jetbrains.intellij.deps"
+            val dependencyName = "log4j"
+            val dependencyVersion = "1.2.17" //1.2.17.1
+            val url = (repository as MavenArtifactRepository).url.let {
+                if (!it.toString().endsWith("/")) {
+                    "$it/"
+                } else {
+                    it
+                }
+            }
+            val jarUrl = String.format(
+                "%s%s/%s/%s/%s-%s.jar", url,
+                dependencyGroup.replace('.', '/'), dependencyName, dependencyVersion,
+                dependencyName, dependencyVersion
+            )
+
+            kotlin.runCatching {
+                val jarFile = URL(jarUrl)
+                val inStream = jarFile.openStream();
+                if (inStream != null) {
+                    println(
+                        String.format("%s:%s:%s", dependencyGroup, dependencyName, dependencyVersion)
+                                + " -> " + jarUrl
+                    )
+                }
+            }
+        }
+    }
+
     shadowJar {
         val dokka_version: String by project
         archiveFileName.set("dokka-kotlin-analysis-intellij-$dokka_version.jar")
