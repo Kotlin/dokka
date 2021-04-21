a466ca12cf0753e05750257896db025de381f007
Marcin Aman <marcin.aman@gmail.com>
2021-04-21 13:10:03 +0200

    Make anchors stable (#1851)
    * Make anchors stable
    
    * Remove backing field from merged and all in CompositeSourceSetID

--- plugins/base/src/main/kotlin/renderers/pageId.kt
+++ plugins/base/src/main/kotlin/renderers/pageId.kt
@@ -11,8 +11,17 @@ internal val ContentPage.pageId: String
 internal val NavigationNode.pageId: String
     get() = pageId(dri, sourceSets)
 
+@JvmName("shortenSourceSetsToUrl")
+internal fun Set<DisplaySourceSet>.shortenToUrl() =
+    sortedBy { it.sourceSetIDs.merged.let { it.scopeId + it.sourceSetName } }.joinToString().hashCode()
+
+internal fun DRI.shortenToUrl() = toString()
+
+@JvmName("shortenDrisToUrl")
+internal fun Set<DRI>.shortenToUrl() = sortedBy { it.toString() }.joinToString().hashCode()
+
 /**
  * Page Id is required to have a sourceSet in order to distinguish between different pages that has same DRI but different sourceSet
  * like main functions that are not expect/actual
  */
-private fun pageId(dri: DRI, sourceSets: Set<DisplaySourceSet>): String = "$dri/${sourceSets.hashCode()}"
+private fun pageId(dri: DRI, sourceSets: Set<DisplaySourceSet>): String = "${dri.shortenToUrl()}/${sourceSets.shortenToUrl()}"
