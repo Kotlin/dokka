34be1adfc7a5a40cf649b0243eefae8b03af49e1
Marcin Aman <marcin.aman@gmail.com>
2021-04-29 17:05:03 +0200

    Update tests with kotlin 1.5-RC and gradle 7.0 (#1857)
    * Update tests with kotlin 1.5-RC and gradle 7.0
    
    * Update tests with kotlin 1.5-RC and gradle 7.0

--- integration-tests/gradle/src/integrationTest/kotlin/org/jetbrains/dokka/it/gradle/BasicGroovyIntegrationTest.kt
+++ integration-tests/gradle/src/integrationTest/kotlin/org/jetbrains/dokka/it/gradle/BasicGroovyIntegrationTest.kt
@@ -15,8 +15,11 @@ class BasicGroovyIntegrationTest(override val versions: BuildVersions) : Abstrac
         @get:JvmStatic
         @get:Parameterized.Parameters(name = "{0}")
         val versions = BuildVersions.permutations(
-            gradleVersions = listOf("6.6", "5.6.4"),
-            kotlinVersions = listOf("1.4.0")
+            gradleVersions = listOf("7.0", "6.6"),
+            kotlinVersions = listOf("1.3.72", "1.4.0", "1.5.0-RC")
+        ) + BuildVersions.permutations(
+            gradleVersions = listOf("5.6.4"),
+            kotlinVersions = listOf("1.4.0", "1.4.32")
         )
     }
 
