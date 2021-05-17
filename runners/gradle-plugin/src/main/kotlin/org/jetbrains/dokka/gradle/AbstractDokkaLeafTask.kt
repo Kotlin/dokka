1c6e3f6978c02a147d3832692ab38a0cf850d69d
Kamil Doległo <9080183+kamildoleglo@users.noreply.github.com>
2021-05-17 18:05:05 +0200

    Extract properties to AbstractDokkaLeafTask (#1922)

--- runners/gradle-plugin/src/main/kotlin/org/jetbrains/dokka/gradle/AbstractDokkaLeafTask.kt
+++ runners/gradle-plugin/src/main/kotlin/org/jetbrains/dokka/gradle/AbstractDokkaLeafTask.kt
@@ -3,8 +3,9 @@ package org.jetbrains.dokka.gradle;
 import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.internal.plugins.DslObject
 import org.gradle.api.tasks.Internal
+import org.gradle.api.tasks.Nested
 
-abstract class AbstractDokkaLeafTask() : AbstractDokkaTask() {
+abstract class AbstractDokkaLeafTask : AbstractDokkaTask() {
 
     @get:Internal
     val dokkaSourceSets: NamedDomainObjectContainer<GradleDokkaSourceSetBuilder> =
@@ -17,4 +18,16 @@ abstract class AbstractDokkaLeafTask() : AbstractDokkaTask() {
                     }
                 }
             }
+
+    /**
+     * Only contains source sets that are marked with `isDocumented`.
+     * Non documented source sets are not relevant for Gradle's UP-TO-DATE mechanism, as well
+     * as task dependency graph.
+     */
+    @get:Nested
+    protected val unsuppressedSourceSets: List<GradleDokkaSourceSetBuilder>
+        get() = dokkaSourceSets
+                .toList()
+                .also(::checkSourceSetDependencies)
+                .filterNot { it.suppress.getSafe() }
 }
--- runners/gradle-plugin/src/main/kotlin/org/jetbrains/dokka/gradle/DokkaTaskPartial.kt
+++ runners/gradle-plugin/src/main/kotlin/org/jetbrains/dokka/gradle/DokkaTaskPartial.kt
@@ -1,25 +1,9 @@
 package org.jetbrains.dokka.gradle
 
-import org.gradle.api.NamedDomainObjectContainer
-import org.gradle.api.internal.plugins.DslObject
-import org.gradle.api.tasks.Internal
-import org.gradle.api.tasks.Nested
 import org.jetbrains.dokka.DokkaConfigurationImpl
 import org.jetbrains.dokka.build
 
 abstract class DokkaTaskPartial : AbstractDokkaLeafTask() {
-
-    /**
-     * Only contains source sets that are marked with `isDocumented`.
-     * Non documented source sets are not relevant for Gradle's UP-TO-DATE mechanism, as well
-     * as task dependency graph.
-     */
-    @get:Nested
-    protected val unsuppressedSourceSets: List<GradleDokkaSourceSetBuilder>
-        get() = dokkaSourceSets
-            .toList()
-            .also(::checkSourceSetDependencies)
-            .filterNot { it.suppress.getSafe() }
 
     override fun buildDokkaConfiguration(): DokkaConfigurationImpl {
         return DokkaConfigurationImpl(
