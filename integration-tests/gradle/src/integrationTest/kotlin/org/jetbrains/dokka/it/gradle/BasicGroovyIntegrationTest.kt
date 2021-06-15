f24060408be04bce08f523579c0f6ad358a58a34
Marcin Aman <marcin.aman@gmail.com>
2021-06-15 14:23:42 +0200

    Remove JDK 8 on windows due to OOM and bump kotlin version in test (#1971)

--- integration-tests/gradle/src/integrationTest/kotlin/org/jetbrains/dokka/it/gradle/BasicGroovyIntegrationTest.kt
+++ integration-tests/gradle/src/integrationTest/kotlin/org/jetbrains/dokka/it/gradle/BasicGroovyIntegrationTest.kt
@@ -16,7 +16,7 @@ class BasicGroovyIntegrationTest(override val versions: BuildVersions) : Abstrac
         @get:Parameterized.Parameters(name = "{0}")
         val versions = BuildVersions.permutations(
             gradleVersions = listOf("7.0", "6.6"),
-            kotlinVersions = listOf("1.3.72", "1.4.0", "1.5.0-RC")
+            kotlinVersions = listOf("1.3.72", "1.4.0", "1.5.10")
         ) + BuildVersions.permutations(
             gradleVersions = listOf("5.6.4"),
             kotlinVersions = listOf("1.4.0", "1.4.32")
