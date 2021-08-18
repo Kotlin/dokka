272892aa019583001c90c983191932592b4b0a2a
vmishenev <vad-mishenev@yandex.ru>
2021-08-18 23:20:08 +0300

    Add copy-button for code block

--- plugins/base/src/main/kotlin/renderers/html/HtmlRenderer.kt
+++ plugins/base/src/main/kotlin/renderers/html/HtmlRenderer.kt
@@ -685,6 +685,7 @@ open class HtmlRenderer(
                     code.children.forEach { buildContentNode(it, pageContext) }
                 }
             }
+            copyButton()
         }
     }
 
