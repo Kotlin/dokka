1555e561e50eae1d09236b205f5c5113b3a1fdbc
Marcin Aman <marcin.aman@gmail.com>
2021-09-07 13:02:05 +0200

    Fix footer on firefox

--- plugins/base/src/main/kotlin/renderers/html/HtmlRenderer.kt
+++ plugins/base/src/main/kotlin/renderers/html/HtmlRenderer.kt
@@ -848,7 +848,9 @@ open class HtmlRenderer(
                         content()
                         div(classes = "footer") {
                             span("go-to-top-icon") {
-                                a(href = "#content")
+                                a(href = "#content"){
+                                    id = "go-to-top-link"
+                                }
                             }
                             span {
                                 configuration?.footerMessage?.takeIf { it.isNotEmpty() }
