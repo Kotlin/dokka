0aae5e16323dd6a53ba4f62d4310d8404f0c93e1
vmishenev <vad-mishenev@yandex.ru>
2021-09-29 00:50:36 +0300

    Add sample task to generate previously docs

--- examples/gradle/dokka-versioning-multimodule-example/parentProject/build.gradle.kts
+++ examples/gradle/dokka-versioning-multimodule-example/parentProject/build.gradle.kts
@@ -1,9 +1,25 @@
+import org.jetbrains.dokka.gradle.DokkaMultiModuleTask
+
 dependencies {
     implementation(kotlin("stdlib"))
 }
 
+val olderVersionsFolder = "olderVersions"
+
+// The previously documentations should be generated with the versioning plugin
+val generatePreviouslyDocTask by tasks.register<DokkaMultiModuleTask>("dokkaPreviouslyDocumentation") {
+    dependencies {
+        dokkaPlugin("org.jetbrains.dokka:all-modules-page-plugin:1.6.0")
+        dokkaPlugin("org.jetbrains.dokka:versioning-plugin:1.6.0")
+    }
+    val configuredVersion = "0.9"
+    outputDirectory.set(file(projectDir.toPath().resolve(olderVersionsFolder).resolve(configuredVersion)))
+    pluginsMapConfiguration.set(mapOf("org.jetbrains.dokka.versioning.VersioningPlugin" to """{ "version": "$configuredVersion" }"""))
+    addChildTasks(listOf(project("childProjectA"), project("childProjectB")), "dokkaHtmlPartial")
+}
+
 tasks.dokkaHtmlMultiModule {
+    dependsOn(generatePreviouslyDocTask)
     val configuredVersion = "1.0"
-    // The previously documentations should be generated with the versioning plugin
-    pluginsMapConfiguration.set(mapOf("org.jetbrains.dokka.versioning.VersioningPlugin" to """{ "version": "$configuredVersion", "olderVersionsDir": "$projectDir/olderVersions" }"""))
+    pluginsMapConfiguration.set(mapOf("org.jetbrains.dokka.versioning.VersioningPlugin" to """{ "version": "$configuredVersion", "olderVersionsDir": "$projectDir/$olderVersionsFolder" }"""))
 }
