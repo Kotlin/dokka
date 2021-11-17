72927277fc4f980ecf6c6ab833b84987c443e20f
vmishenev <vad-mishenev@yandex.ru>
2021-11-17 17:07:18 +0300

    Update version of `SerializationGradleIntegrationTest` to 1.3.1

--- integration-tests/gradle/src/integrationTest/kotlin/org/jetbrains/dokka/it/gradle/kotlin/SerializationGradleIntegrationTest.kt
+++ integration-tests/gradle/src/integrationTest/kotlin/org/jetbrains/dokka/it/gradle/kotlin/SerializationGradleIntegrationTest.kt
@@ -16,7 +16,7 @@ class SerializationGradleIntegrationTest(override val versions: BuildVersions) :
         @get:Parameterized.Parameters(name = "{0}")
         val versions = BuildVersions.permutations(
             gradleVersions = listOf("6.8"),
-            kotlinVersions = listOf("1.4.30")
+            kotlinVersions = listOf("1.5.31")
         )
     }
 
