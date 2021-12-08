4dc0654ee951722d61087f1cf2e8c3cc9a341780
rajdeep <iamrajdeep1008@gmail.com>
2021-12-08 15:11:17 +0530

    Fix for repositories not defined issue in versioning multimodule example (#2263)

--- examples/gradle/dokka-versioning-multimodule-example/build.gradle.kts
+++ examples/gradle/dokka-versioning-multimodule-example/build.gradle.kts
@@ -5,12 +5,15 @@ plugins {
 
 // The versioning plugin should be applied in all submodules
 subprojects {
+    repositories {
+        mavenCentral()
+    }
     apply {
         plugin("org.jetbrains.kotlin.jvm")
         plugin("org.jetbrains.dokka")
     }
     val dokkaPlugin by configurations
     dependencies {
-       dokkaPlugin("org.jetbrains.dokka:versioning-plugin:1.6.0")
+        dokkaPlugin("org.jetbrains.dokka:versioning-plugin:1.6.0")
     }
 }
