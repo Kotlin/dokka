fa391ffd8da0c785e0450b8510f7d39ab2c71618
Ignat Beresnev <ignat@beresnev.me>
2021-12-23 12:02:59 +0300

    Add horizontal margin to breadcrumbs delimiter (KT-50296) (#2285)

--- plugins/base/src/main/kotlin/renderers/html/HtmlRenderer.kt
+++ plugins/base/src/main/kotlin/renderers/html/HtmlRenderer.kt
@@ -607,7 +607,9 @@ open class HtmlRenderer(
             if (path.size > 1) {
                 buildNavigationElement(path.first(), page)
                 path.drop(1).forEach { node ->
-                    text("/")
+                    span(classes = "delimiter") {
+                        text("/")
+                    }
                     buildNavigationElement(node, page)
                 }
             }
