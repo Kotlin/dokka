a9283f3652785640ba482b158ac785fbfbaf1f05
Ignat Beresnev <ignat@beresnev.me>
2021-12-27 18:55:35 +0300

    Update Dokka examples and readme to 1.6.10 (#2293)

--- examples/gradle/dokka-customFormat-example/build.gradle.kts
+++ examples/gradle/dokka-customFormat-example/build.gradle.kts
@@ -1,13 +1,13 @@
 import org.jetbrains.dokka.gradle.DokkaTask
 
 plugins {
-    kotlin("jvm") version "1.6.0"
-    id("org.jetbrains.dokka") version ("1.6.0")
+    kotlin("jvm") version "1.6.10"
+    id("org.jetbrains.dokka") version ("1.6.10")
 }
 
 buildscript {
     dependencies {
-        classpath("org.jetbrains.dokka:dokka-base:1.6.0")
+        classpath("org.jetbrains.dokka:dokka-base:1.6.10")
     }
 }
 
