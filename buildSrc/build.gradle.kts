8ba43b1aed302c55d0a7b5bbef05e8468f279642
Goooler <wangzongler@gmail.com>
2022-02-16 04:00:42 +0800

    Bump binary-compatibility-validator to 0.8.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,6 +8,6 @@ repositories {
 
 dependencies {
     implementation("com.github.jengelman.gradle.plugins:shadow:2.0.4")
-    implementation("org.jetbrains.kotlinx:binary-compatibility-validator:0.6.0")
+    implementation("org.jetbrains.kotlinx:binary-compatibility-validator:0.8.0")
     implementation("io.github.gradle-nexus:publish-plugin:1.0.0")
 }
