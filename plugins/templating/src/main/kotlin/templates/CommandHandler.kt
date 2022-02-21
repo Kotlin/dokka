f77ec86c336c3a466e9c2ef889d8e64998edac33
vmishenev <vad-mishenev@yandex.ru>
2022-02-22 00:23:55 +0300

    Call deprecated `handleCommand` from `handleCommandAsTag`

--- plugins/templating/src/main/kotlin/templates/CommandHandler.kt
+++ plugins/templating/src/main/kotlin/templates/CommandHandler.kt
@@ -8,9 +8,11 @@ import java.io.File
 
 interface CommandHandler  {
     @Deprecated("This was renamed to handleCommandAsTag", ReplaceWith("handleCommandAsTag(command, element, input, output)"))
-    fun handleCommand(element: Element, command: Command, input: File, output: File) =
-        handleCommandAsTag(command, element, input, output)
-    fun handleCommandAsTag(command: Command, body: Element, input: File, output: File)
+    fun handleCommand(element: Element, command: Command, input: File, output: File) { }
+
+    @Suppress("DEPRECATION")
+    fun handleCommandAsTag(command: Command, body: Element, input: File, output: File) =
+        handleCommand(body, command, input, output)
     fun handleCommandAsComment(command: Command, body: List<Node>, input: File, output: File) { }
     fun canHandle(command: Command): Boolean
     fun finish(output: File) {}
