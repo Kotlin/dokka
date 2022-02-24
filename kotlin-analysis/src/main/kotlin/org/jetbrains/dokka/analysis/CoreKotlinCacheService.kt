04dd34000750538cf4865ba7a9f11f4744691475
Ignat Beresnev <ignat.beresnev@jetbrains.com>
2022-02-24 23:30:22 +0300

    Update Kotlin to 1.6.20-M1 (#2375)

--- kotlin-analysis/src/main/kotlin/org/jetbrains/dokka/analysis/CoreKotlinCacheService.kt
+++ kotlin-analysis/src/main/kotlin/org/jetbrains/dokka/analysis/CoreKotlinCacheService.kt
@@ -5,19 +5,13 @@ import org.jetbrains.kotlin.analyzer.ModuleInfo
 import org.jetbrains.kotlin.caches.resolve.KotlinCacheService
 import org.jetbrains.kotlin.caches.resolve.PlatformAnalysisSettings
 import org.jetbrains.kotlin.idea.resolve.ResolutionFacade
+import org.jetbrains.kotlin.platform.TargetPlatform
 import org.jetbrains.kotlin.psi.KtElement
 import org.jetbrains.kotlin.resolve.diagnostics.KotlinSuppressCache
 
 
 class CoreKotlinCacheService(private val resolutionFacade: DokkaResolutionFacade) : KotlinCacheService {
     override fun getResolutionFacade(elements: List<KtElement>): ResolutionFacade {
-        return resolutionFacade
-    }
-
-    override fun getResolutionFacade(
-        elements: List<KtElement>,
-        platform: org.jetbrains.kotlin.platform.TargetPlatform
-    ): ResolutionFacade {
         return resolutionFacade
     }
 
@@ -38,6 +32,13 @@ class CoreKotlinCacheService(private val resolutionFacade: DokkaResolutionFacade
     override fun getResolutionFacadeByModuleInfo(
         moduleInfo: ModuleInfo,
         platform: org.jetbrains.kotlin.platform.TargetPlatform
+    ): ResolutionFacade {
+        return resolutionFacade
+    }
+
+    override fun getResolutionFacadeWithForcedPlatform(
+        elements: List<KtElement>,
+        platform: TargetPlatform
     ): ResolutionFacade {
         return resolutionFacade
     }
