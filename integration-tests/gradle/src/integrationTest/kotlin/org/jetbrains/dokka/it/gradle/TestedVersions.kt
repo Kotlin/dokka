9e419bf2b0ea3a34e89b6f944ae03434a209a642
Ignat Beresnev <ignat.beresnev@jetbrains.com>
2022-04-14 22:08:22 +0300

    Update gradle and AGP versions to the latest

--- integration-tests/gradle/src/integrationTest/kotlin/org/jetbrains/dokka/it/gradle/TestedVersions.kt
+++ integration-tests/gradle/src/integrationTest/kotlin/org/jetbrains/dokka/it/gradle/TestedVersions.kt
@@ -4,7 +4,7 @@ internal object TestedVersions {
 
     val BASE =
         BuildVersions.permutations(
-            gradleVersions = listOf("7.3", "6.9"),
+            gradleVersions = listOf("7.4.2", "6.9"),
             kotlinVersions = listOf("1.6.20", "1.5.31", "1.4.32"),
         ) + BuildVersions.permutations(
             gradleVersions = listOf(*ifExhaustive("7.0", "6.1.1")),
@@ -17,9 +17,9 @@ internal object TestedVersions {
      */
     val ANDROID =
         BuildVersions.permutations(
-            gradleVersions = listOf("7.3", *ifExhaustive("7.0")),
+            gradleVersions = listOf("7.4.2", *ifExhaustive("7.0")),
             kotlinVersions = listOf("1.6.20", "1.5.31", "1.4.32"),
-            androidGradlePluginVersions = listOf("7.0.0")
+            androidGradlePluginVersions = listOf("7.1.2")
         ) + BuildVersions.permutations(
             gradleVersions = listOf("6.9", *ifExhaustive("6.1.1", "5.6.4")),
             kotlinVersions = listOf("1.6.20", "1.5.31", "1.4.32"),
