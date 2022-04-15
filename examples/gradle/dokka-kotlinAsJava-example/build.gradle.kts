bf0476ec3e79cf9215980a2f03cb8e035973c40b
Ignat Beresnev <ignat.beresnev@jetbrains.com>
2022-04-15 12:45:15 +0300

    Update documentation, examples and test references to 1.6.20 (#2440)
    * Update documentation/example/test references to 1.6.20
    
    * Mute Multiplatform0GradleIntegrationTest due to missing KLIB support
    
    * Update gradle and AGP versions to the latest
    
    (cherry picked from commit 48b5918c2d465b482de4f070879b5ad93cc248b3)

--- examples/gradle/dokka-kotlinAsJava-example/build.gradle.kts
+++ examples/gradle/dokka-kotlinAsJava-example/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.6.10"
-    id("org.jetbrains.dokka") version ("1.6.10")
+    kotlin("jvm") version "1.6.20"
+    id("org.jetbrains.dokka") version ("1.6.20")
 }
 
 repositories {
@@ -12,11 +12,11 @@ dependencies {
     testImplementation(kotlin("test-junit"))
 
     // Will apply the plugin to all dokka tasks
-    dokkaPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.6.10")
+    dokkaPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.6.20")
 
     // Will apply the plugin only to the `:dokkaHtml` task
-    //dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.6.10")
+    //dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.6.20")
 
     // Will apply the plugin only to the `:dokkaGfm` task
-    //dokkaGfmPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.6.10")
+    //dokkaGfmPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.6.20")
 }
