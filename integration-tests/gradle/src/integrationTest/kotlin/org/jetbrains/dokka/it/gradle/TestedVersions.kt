a8c1a6428520ddc49e3658d17db98ff41f3d6643
Ignat Beresnev <ignat.beresnev@jetbrains.com>
2022-04-21 17:12:27 +0300

    Update Kotlin to 1.6.21 (#2454)
    * Update Kotlin to 1.6.21
    
    * Use 1.6.20 react wrapper for 1.6.21 kotlin
    
    (cherry picked from commit 14710abcbae17f835c98e42615c8fe5e946d3b4a)

--- integration-tests/gradle/src/integrationTest/kotlin/org/jetbrains/dokka/it/gradle/TestedVersions.kt
+++ integration-tests/gradle/src/integrationTest/kotlin/org/jetbrains/dokka/it/gradle/TestedVersions.kt
@@ -4,8 +4,8 @@ internal object TestedVersions {
 
     val BASE =
         BuildVersions.permutations(
-            gradleVersions = listOf("7.3", "6.9"),
-            kotlinVersions = listOf("1.6.10", "1.5.31", "1.4.32"),
+            gradleVersions = listOf("7.4.2", "6.9"),
+            kotlinVersions = listOf("1.6.21", "1.5.31", "1.4.32"),
         ) + BuildVersions.permutations(
             gradleVersions = listOf(*ifExhaustive("7.0", "6.1.1")),
             kotlinVersions = listOf(*ifExhaustive("1.6.0", "1.5.0", "1.4.0"))
@@ -17,12 +17,12 @@ internal object TestedVersions {
      */
     val ANDROID =
         BuildVersions.permutations(
-            gradleVersions = listOf("7.3", *ifExhaustive("7.0")),
-            kotlinVersions = listOf("1.6.10", "1.5.31", "1.4.32"),
-            androidGradlePluginVersions = listOf("7.0.0")
+            gradleVersions = listOf("7.4.2", *ifExhaustive("7.0")),
+            kotlinVersions = listOf("1.6.21", "1.5.31", "1.4.32"),
+            androidGradlePluginVersions = listOf("7.1.2")
         ) + BuildVersions.permutations(
             gradleVersions = listOf("6.9", *ifExhaustive("6.1.1", "5.6.4")),
-            kotlinVersions = listOf("1.6.10", "1.5.31", "1.4.32"),
+            kotlinVersions = listOf("1.6.21", "1.5.31", "1.4.32"),
             androidGradlePluginVersions = listOf("4.0.0", *ifExhaustive("3.6.3"))
         )
 
@@ -31,6 +31,6 @@ internal object TestedVersions {
         "1.5.0" to "-Preact_version=17.0.2-pre.204-kotlin-1.5.0",
         "1.6.0" to "-Preact_version=17.0.2-pre.280-kotlin-1.6.0",
         "1.5.31" to "-Preact_version=17.0.2-pre.265-kotlin-1.5.31",
-        "1.6.10" to "-Preact_version=18.0.0-pre.325-kotlin-1.6.10",
+        "1.6.21" to "-Preact_version=18.0.0-pre.329-kotlin-1.6.20" // TODO update to 1.6.21 when released
     )
 }
