91b5de427480971e0310e00f5761a690352a6270
Goooler <wangzongler@gmail.com>
2022-04-22 10:25:44 +0800

    Bump publish-plugin to 1.1.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,5 +9,5 @@ repositories {
 dependencies {
     implementation("gradle.plugin.com.github.johnrengelman:shadow:7.1.2")
     implementation("org.jetbrains.kotlinx:binary-compatibility-validator:0.9.0")
-    implementation("io.github.gradle-nexus:publish-plugin:1.0.0")
+    implementation("io.github.gradle-nexus:publish-plugin:1.1.0")
 }
