52d066c10a50b0e34f1c390c965a89ae403e07a9
Ignat Beresnev <ignat.beresnev@jetbrains.com>
2022-04-25 22:27:12 +0300

    Update documentation and examples to Dokka 1.6.21 (#2465)

--- examples/gradle/dokka-multimodule-example/settings.gradle.kts
+++ examples/gradle/dokka-multimodule-example/settings.gradle.kts
@@ -1,7 +1,7 @@
 pluginManagement {
     plugins {
-        kotlin("jvm") version "1.6.20"
-        id("org.jetbrains.dokka") version ("1.6.20")
+        kotlin("jvm") version "1.6.21"
+        id("org.jetbrains.dokka") version ("1.6.21")
     }
 }
 
