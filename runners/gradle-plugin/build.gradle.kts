8ae9204dd51f5b8a36f270ce1b9ec4df98a8791a
Martin Bonnin <martin@mbonnin.net>
2022-06-21 14:53:37 +0200

    also exclude kotlin-stdlib-common

--- runners/gradle-plugin/build.gradle.kts
+++ runners/gradle-plugin/build.gradle.kts
@@ -36,6 +36,7 @@ configurations.api {
     exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib")
     exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk7")
     exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk8")
+    exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-common")
 }
 
 
