55e56d29fa05874965372eb6cdaa70a49ae19309
Goooler <wangzongler@gmail.com>
2022-08-24 21:07:37 +0800

    Enable Gradle Enterprise

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -46,3 +46,15 @@ pluginManagement {
         id("com.gradle.plugin-publish") version "0.20.0"
     }
 }
+
+plugins {
+    `gradle-enterprise`
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishAlways()
+    }
+}
