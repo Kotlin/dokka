dd6e958dab2327c99a0b1cd572ce887bac38efad
vmishenev <vad-mishenev@yandex.ru>
2022-09-21 22:20:13 +0300

    Enhance mpp integration test

--- integration-tests/gradle/projects/it-multiplatform-0/build.gradle.kts
+++ integration-tests/gradle/projects/it-multiplatform-0/build.gradle.kts
@@ -14,6 +14,14 @@ kotlin {
     macosX64("macos")
     js()
     sourceSets {
+        val commonMain by sourceSets.getting
+        val linuxMain by sourceSets.getting
+        val macosMain by sourceSets.getting
+        val desktopMain by sourceSets.creating {
+            dependsOn(commonMain)
+            linuxMain.dependsOn(this)
+            macosMain.dependsOn(this)
+        }
         named("commonMain") {
             dependencies {
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
