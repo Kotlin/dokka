acb342fea1ac918f542f025ba4c71acd79052652
vmishenev <vad-mishenev@yandex.ru>
2022-10-12 20:26:25 +0300

    Display 'SinceKotlin' everywhere

--- plugins/base/src/main/kotlin/transformers/pages/annotations/SinceKotlinTransformer.kt
+++ plugins/base/src/main/kotlin/transformers/pages/annotations/SinceKotlinTransformer.kt
@@ -1,10 +1,12 @@
 package org.jetbrains.dokka.base.transformers.pages.annotations
 
 import org.intellij.markdown.MarkdownElementTypes
+import org.jetbrains.dokka.Platform
 import org.jetbrains.dokka.links.DRI
 import org.jetbrains.dokka.model.*
 import org.jetbrains.dokka.model.doc.CustomDocTag
 import org.jetbrains.dokka.model.doc.CustomTagWrapper
+import org.jetbrains.dokka.model.doc.DocumentationNode
 import org.jetbrains.dokka.model.doc.Text
 import org.jetbrains.dokka.model.properties.WithExtraProperties
 import org.jetbrains.dokka.plugability.DokkaContext
@@ -12,6 +14,30 @@ import org.jetbrains.dokka.transformers.documentation.DocumentableTransformer
 import org.jetbrains.kotlin.utils.addToStdlib.safeAs
 
 class SinceKotlinTransformer(val context: DokkaContext) : DocumentableTransformer {
+    class Version constructor(str: String) : Comparable<Version> {
+        private val parts: List<Int> = str.split(".").map { it.toInt() }
+
+        override fun compareTo(other: Version): Int {
+            val i1 = parts.listIterator()
+            val i2 = other.parts.listIterator()
+
+            while (i1.hasNext() || i2.hasNext()) {
+                val diff = (if (i1.hasNext()) i1.next() else 0) - (if (i2.hasNext()) i2.next() else 0)
+                if (diff != 0) return diff
+            }
+
+            return 0
+        }
+
+        override fun toString(): String = parts.joinToString(".")
+    }
+
+    private val minSinceKotlin = mapOf(
+        Platform.common to Version("1.2"),
+        Platform.jvm to Version("1.0"),
+        Platform.js to Version("1.1"),
+        Platform.native to Version("1.3")
+    )
 
     override fun invoke(original: DModule, context: DokkaContext) = original.transform() as DModule
 
@@ -20,74 +46,96 @@ class SinceKotlinTransformer(val context: DokkaContext) : DocumentableTransforme
             is DModule -> copy(
                 packages = packages.map { it.transform() as DPackage }
             )
+
             is DPackage -> copy(
                 classlikes = classlikes.map { it.transform() as DClasslike },
                 functions = functions.map { it.transform() as DFunction },
                 properties = properties.map { it.transform() as DProperty }
             )
+
             is DClass -> copy(
                 documentation = appendSinceKotlin(),
                 classlikes = classlikes.map { it.transform() as DClasslike },
                 functions = functions.map { it.transform() as DFunction },
                 properties = properties.map { it.transform() as DProperty }
             )
+
             is DEnum -> copy(
                 documentation = appendSinceKotlin(),
                 classlikes = classlikes.map { it.transform() as DClasslike },
                 functions = functions.map { it.transform() as DFunction },
                 properties = properties.map { it.transform() as DProperty }
             )
+
             is DInterface -> copy(
                 documentation = appendSinceKotlin(),
                 classlikes = classlikes.map { it.transform() as DClasslike },
                 functions = functions.map { it.transform() as DFunction },
                 properties = properties.map { it.transform() as DProperty }
             )
+
             is DObject -> copy(
                 documentation = appendSinceKotlin(),
                 classlikes = classlikes.map { it.transform() as DClasslike },
                 functions = functions.map { it.transform() as DFunction },
                 properties = properties.map { it.transform() as DProperty }
             )
+
             is DAnnotation -> copy(
                 documentation = appendSinceKotlin(),
                 classlikes = classlikes.map { it.transform() as DClasslike },
                 functions = functions.map { it.transform() as DFunction },
                 properties = properties.map { it.transform() as DProperty }
             )
+
             is DFunction -> copy(
                 documentation = appendSinceKotlin()
             )
+
             is DProperty -> copy(
                 documentation = appendSinceKotlin()
             )
+
             is DParameter -> copy(
                 documentation = appendSinceKotlin()
             )
+
             else -> this.also { context.logger.warn("Unrecognized documentable $this while SinceKotlin transformation") }
         }
 
     private fun Documentable.appendSinceKotlin() =
         sourceSets.fold(documentation) { acc, sourceSet ->
-            safeAs<WithExtraProperties<Documentable>>()?.extra?.get(Annotations)?.directAnnotations?.get(sourceSet)?.find {
-                it.dri == DRI("kotlin", "SinceKotlin")
-            }?.params?.get("version").safeAs<StringValue>()?.value?.let { version ->
+            val annotatedVersion =
+                safeAs<WithExtraProperties<Documentable>>()?.extra?.get(Annotations)?.directAnnotations?.get(sourceSet)
+                    ?.find {
+                        it.dri == DRI("kotlin", "SinceKotlin")
+                    }?.params?.get("version").safeAs<StringValue>()?.value
+
+            val version = annotatedVersion?.let {
+                Version(annotatedVersion.dropWhile { it == '"' }.dropLastWhile { it == '"' })
+            }?.takeIf { version -> minSinceKotlin[sourceSet.analysisPlatform]?.let { version >= it } ?: true }
+                ?: minSinceKotlin[sourceSet.analysisPlatform]
+
+            val customTag = CustomTagWrapper(
+                CustomDocTag(
+                    listOf(
+                        Text(
+                            version.toString()
+                        )
+                    ),
+                    name = MarkdownElementTypes.MARKDOWN_FILE.name
+                ),
+                "Since Kotlin"
+            )
+            if (acc[sourceSet] == null)
+                acc + (sourceSet to DocumentationNode(listOf(customTag)))
+            else
                 acc.mapValues {
                     if (it.key == sourceSet) it.value.copy(
                         it.value.children + listOf(
-                            CustomTagWrapper(
-                                CustomDocTag(
-                                    listOf(
-                                        Text(version.dropWhile { it == '"' }.dropLastWhile { it == '"' }
-                                        )
-                                    ),
-                                    name = MarkdownElementTypes.MARKDOWN_FILE.name
-                                ),
-                                "Since Kotlin"
-                            )
+                            customTag
                         )
                     ) else it.value
                 }
-            } ?: acc
         }
 }
--- plugins/base/src/test/kotlin/content/annotations/SinceKotlinTest.kt
+++ plugins/base/src/test/kotlin/content/annotations/SinceKotlinTest.kt
@@ -1,19 +1,152 @@
 package content.annotations
 
 import matchers.content.*
+import org.jetbrains.dokka.Platform
 import org.jetbrains.dokka.base.testApi.testRunner.BaseAbstractTest
+import org.jetbrains.dokka.base.transformers.pages.annotations.SinceKotlinTransformer.Version
+import org.jetbrains.dokka.model.DFunction
+import org.jetbrains.dokka.model.dfs
+import org.jetbrains.dokka.model.doc.CustomTagWrapper
+import org.jetbrains.dokka.model.doc.Text
 import org.jetbrains.dokka.pages.ContentPage
 import org.junit.jupiter.api.Test
 import utils.ParamAttributes
+import utils.assertNotNull
 import utils.bareSignature
+import kotlin.test.assertEquals
+
 
 class SinceKotlinTest : BaseAbstractTest() {
 
-    private val testConfiguration = dokkaConfiguration {
+    val testConfiguration = dokkaConfiguration {
         sourceSets {
             sourceSet {
                 sourceRoots = listOf("src/")
                 analysisPlatform = "jvm"
+            }
+        }
+    }
+
+    @Test
+    fun versionsComparing() {
+        assert(Version("1.0").compareTo(Version("1.0")) == 0)
+        assert(Version("1.0.0").compareTo(Version("1")) == 0)
+        assert(Version("1.0") >= Version("1.0"))
+        assert(Version("1.1") > Version("1"))
+        assert(Version("1.0") < Version("2.0"))
+        assert(Version("1.0") < Version("2.2"))
+    }
+
+    @Test
+    fun `mpp fun without SinceKotlin annotation`() {
+        val configuration =   dokkaConfiguration {
+            extraOptions = listOf("-XXSinceKotlin")
+            sourceSets {
+                sourceSet {
+                    sourceRoots = listOf("src/")
+                    analysisPlatform = "jvm"
+                }
+                sourceSet {
+                    sourceRoots = listOf("src/")
+                    analysisPlatform = "native"
+                }
+                sourceSet {
+                    sourceRoots = listOf("src/")
+                    analysisPlatform = "common"
+                }
+                sourceSet {
+                    sourceRoots = listOf("src/")
+                    analysisPlatform = "js"
+                }
+            }
+        }
+        testInline(
+            """
+            |/src/main/kotlin/test/source.kt
+            |package test
+            |
+            |fun ring(abc: String): String {
+            |    return "My precious " + abc
+            |}
+        """.trimIndent(), configuration
+        ) {
+            documentablesTransformationStage = { module ->
+                @Suppress("UNCHECKED_CAST") val funcs = module.children.single { it.name == "test" }
+                    .children.filter { it.name == "ring" && it is DFunction } as List<DFunction>
+                with(funcs) {
+                    val sinceKotlin = mapOf(
+                        Platform.common to Version("1.2"),
+                        Platform.jvm to Version("1.0"),
+                        Platform.js to Version("1.1"),
+                        Platform.native to Version("1.3")
+                    )
+
+                    for(i in sinceKotlin) {
+                        val tag =
+                            find { it.sourceSets.first().analysisPlatform == i.key }?.documentation?.values?.first()
+                                ?.dfs { it is CustomTagWrapper && it.name == "Since Kotlin" }
+                                .assertNotNull("SinceKotlin[${i.key}]")
+                        assertEquals((tag.children.first() as Text).body, i.value.toString())
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `mpp fun with SinceKotlin annotation`() {
+        val configuration =   dokkaConfiguration {
+            extraOptions = listOf("-XXSinceKotlin")
+            sourceSets {
+                sourceSet {
+                    sourceRoots = listOf("src/")
+                    analysisPlatform = "jvm"
+                }
+                sourceSet {
+                    sourceRoots = listOf("src/")
+                    analysisPlatform = "native"
+                }
+                sourceSet {
+                    sourceRoots = listOf("src/")
+                    analysisPlatform = "common"
+                }
+                sourceSet {
+                    sourceRoots = listOf("src/")
+                    analysisPlatform = "js"
+                }
+            }
+        }
+        testInline(
+            """
+            |/src/main/kotlin/test/source.kt
+            |package test
+            |
+            |/** dssdd */
+            |@SinceKotlin("1.3")
+            |fun ring(abc: String): String {
+            |    return "My precious " + abc
+            |}
+        """.trimIndent(), configuration
+        ) {
+            documentablesTransformationStage = { module ->
+                @Suppress("UNCHECKED_CAST") val funcs = module.children.single { it.name == "test" }
+                    .children.filter { it.name == "ring" && it is DFunction } as List<DFunction>
+                with(funcs) {
+                    val sinceKotlin = mapOf(
+                        Platform.common to Version("1.3"),
+                        Platform.jvm to Version("1.3"),
+                        Platform.js to Version("1.3"),
+                        Platform.native to Version("1.3")
+                    )
+
+                    for(i in sinceKotlin) {
+                        val tag =
+                            find { it.sourceSets.first().analysisPlatform == i.key }?.documentation?.values?.first()
+                                ?.dfs { it is CustomTagWrapper && it.name == "Since Kotlin" }
+                                .assertNotNull("SinceKotlin[${i.key}]")
+                        assertEquals((tag.children.first() as Text).body, i.value.toString())
+                    }
+                }
             }
         }
     }
