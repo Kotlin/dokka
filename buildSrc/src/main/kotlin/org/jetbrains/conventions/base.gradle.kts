7a1a4308648724298aeafd42e91fa53d878dabd9
Adam <897017+aSemy@users.noreply.github.com>
2022-10-13 22:42:01 +0200

    formatting

--- buildSrc/src/main/kotlin/org/jetbrains/conventions/base.gradle.kts
+++ buildSrc/src/main/kotlin/org/jetbrains/conventions/base.gradle.kts
@@ -1,14 +1,12 @@
 package org.jetbrains.conventions
 
-import org.jetbrains.configureDokkaVersion
-
 plugins {
-  base
+    base
 }
 
 description = "common Gradle configuration that should be applied to all projets"
 
 if (project != rootProject) {
-  project.group = rootProject.group
-  project.version = rootProject.version
+    project.group = rootProject.group
+    project.version = rootProject.version
 }
