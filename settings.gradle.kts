d2e05d08f786cb0aba1876cc540b0536254fb25f
Adam <897017+aSemy@users.noreply.github.com>
2022-10-18 12:51:11 +0200

    Declare the Node.js & Yarn download repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -63,6 +63,20 @@ dependencyResolutionManagement {
         mavenCentral()
         google()
 
+        // Declare the Node.js & Yarn download repositories
+        ivy("https://nodejs.org/dist/") {
+            name = "Node Distributions at $url"
+            patternLayout { artifact("v[revision]/[artifact](-v[revision]-[classifier]).[ext]") }
+            metadataSources { artifact() }
+            content { includeModule("org.nodejs", "node") }
+        }
+        ivy("https://github.com/yarnpkg/yarn/releases/download") {
+            name = "Yarn Distributions at $url"
+            patternLayout { artifact("v[revision]/[artifact](-v[revision]).[ext]") }
+            metadataSources { artifact() }
+            content { includeModule("com.yarnpkg", "yarn") }
+        }
+
         maven("https://www.jetbrains.com/intellij-repository/snapshots")
         maven("https://www.jetbrains.com/intellij-repository/releases")
         maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-ide")
