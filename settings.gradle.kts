a1cfab1206891482a6e5fdf0903a39a25f359867
Adam <897017+aSemy@users.noreply.github.com>
2022-10-18 12:42:21 +0200

    move repositories to settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -62,6 +62,13 @@ dependencyResolutionManagement {
     repositories {
         mavenCentral()
         google()
+
+        maven("https://www.jetbrains.com/intellij-repository/snapshots")
+        maven("https://www.jetbrains.com/intellij-repository/releases")
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-ide")
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-ide-plugin-dependencies")
+        maven("https://cache-redirector.jetbrains.com/intellij-dependencies")
+        maven("https://www.myget.org/F/rd-snapshots/maven/")
     }
 
     pluginManagement {
