8c7f5597e78e93e0205749ba969b7726c64cb32d
Ryan Lewis <ryan@rlew.io>
2022-11-10 08:01:34 +0000

    Introduce jackson-databind constraint for 2.12.7.1 (#2733)
    Fixes CVE-2022-42003

--- plugins/gfm/build.gradle.kts
+++ plugins/gfm/build.gradle.kts
@@ -6,6 +6,12 @@ dependencies {
     testImplementation(project(":plugins:base:base-test-utils"))
     val jackson_version: String by project
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jackson_version")
+    val jackson_databind_version: String by project
+    constraints {
+        implementation("com.fasterxml.jackson.core:jackson-databind:$jackson_databind_version") {
+            because("CVE-2022-42003")
+        }
+    }
 }
 
 registerDokkaArtifactPublication("gfmPlugin") {
