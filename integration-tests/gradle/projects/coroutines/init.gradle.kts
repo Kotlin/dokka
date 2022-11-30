6f7c9cd7b72fd27ebc74f41bdaaa55591ec6caf8
Adam <897017+aSemy@users.noreply.github.com>
2022-11-30 19:30:28 +0100

    try fixing plugin publishing for integration tests...

--- integration-tests/gradle/projects/coroutines/init.gradle.kts
+++ integration-tests/gradle/projects/coroutines/init.gradle.kts
@@ -2,7 +2,6 @@ initscript {
     repositories {
         mavenCentral()
         mavenLocal()
-        maven("https://cache-redirector.jetbrains.com/jcenter")
     }
     dependencies {
         classpath("org.jetbrains.dokka:dokka-gradle-plugin:for-integration-tests-SNAPSHOT")
