4cd0146922b2c989be41f4df9f167f0dfdec7f75
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2023-01-13 01:55:54 +0100

    Add a test ensuring correct caching when localDirectory is set to projectDir

--- integration-tests/gradle/src/integrationTest/kotlin/org/jetbrains/dokka/it/gradle/BasicCachingIntegrationTest.kt
+++ integration-tests/gradle/src/integrationTest/kotlin/org/jetbrains/dokka/it/gradle/BasicCachingIntegrationTest.kt
@@ -24,6 +24,25 @@ class BasicCachingIntegrationTest(override val versions: BuildVersions) : Abstra
         runAndAssertOutcome(TaskOutcome.FROM_CACHE)
     }
 
+    @Test
+    fun localDirectoryPointingToRoot() {
+        fun String.findAndReplace(oldValue: String, newValue: String): String {
+            assertTrue(oldValue in this, "Expected to replace '$oldValue'")
+            return replace(oldValue, newValue)
+        }
+        val projectKts = projectDir.resolve("build.gradle.kts")
+        val projectKtsText = projectKts.readText()
+            .findAndReplace("localDirectory.set(file(\"src/main\"))", "localDirectory.set(projectDir)")
+            .findAndReplace("integration-tests/gradle/projects/it-basic/src/main", "integration-tests/gradle/projects/it-basic")
+        projectKts.writeText(projectKtsText)
+
+        runAndAssertOutcome(TaskOutcome.SUCCESS)
+        projectDir.resolve("unrelated.txt").writeText("modified")
+        // despite projectDir is used as an input in localDirectory, changing its contents shouldn't invalidate the cache
+        runAndAssertOutcome(TaskOutcome.FROM_CACHE)
+    }
+
+
     private fun runAndAssertOutcome(expectedOutcome: TaskOutcome) {
         val result = createGradleRunner(
             "clean",
