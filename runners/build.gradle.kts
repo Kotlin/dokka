49b22c7f67d1dbe558c7a6ef3e8731f302d18a5f
Adam <897017+aSemy@users.noreply.github.com>
2023-01-29 12:33:44 +0100

    rm commented out config

--- runners/build.gradle.kts
+++ runners/build.gradle.kts
@@ -1,14 +1,3 @@
 plugins {
     org.jetbrains.conventions.`kotlin-jvm`
 }
-
-
-//subprojects {
-//    apply {
-//        plugin("maven-publish")
-//    }
-//
-//    tasks.processResources {
-//        duplicatesStrategy = DuplicatesStrategy.WARN
-//    }
-//}
