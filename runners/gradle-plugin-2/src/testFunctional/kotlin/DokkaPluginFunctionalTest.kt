67cd9e5ce31208ee16edd70dbf542483f10b3d14
Adam <897017+aSemy@users.noreply.github.com>
2023-01-29 14:07:11 +0100

    add functional tests for dokka configurations/variants

--- runners/gradle-plugin-2/src/testFunctional/kotlin/DokkaPluginFunctionalTest.kt
+++ runners/gradle-plugin-2/src/testFunctional/kotlin/DokkaPluginFunctionalTest.kt
@@ -9,16 +9,10 @@ class DokkaPluginFunctionalTest {
 
     @Test
     fun `expect Dokka Plugin creates Dokka tasks`() {
-
         val build = gradleKtsProjectTest {
             buildGradleKts = """
                 plugins {
                     id("org.jetbrains.dokka2") version "2.0.0"
-                }
-                
-                repositories {
-                  maven(file("$testMavenRepoDir"))
-                  mavenCentral()
                 }
             """.trimIndent()
         }.runner
@@ -38,6 +32,118 @@ class DokkaPluginFunctionalTest {
                 """.trimIndent()
             ),
             "expect output contains dokka tasks\n\n${build.output.prependIndent("  | ")}"
+        )
+    }
+
+    @Test
+    fun `expect Dokka Plugin creates Dokka outgoing variants`() {
+        val build = gradleKtsProjectTest {
+            buildGradleKts = """
+                plugins {
+                    id("org.jetbrains.dokka2") version "2.0.0"
+                }
+            """.trimIndent()
+        }.runner
+            .withArguments("outgoingVariants")
+            .build()
+
+        println(build.output.prependIndent("   | "))
+
+        assertTrue(
+            build.output.contains(
+                """
+                    --------------------------------------------------
+                    Variant dokkaConfigurationElements
+                    --------------------------------------------------
+                    Provide Dokka Configurations files to other subprojects
+                    
+                    Capabilities
+                        - :test:unspecified (default capability)
+                    Attributes
+                        - org.gradle.category = configuration
+                        - org.gradle.usage    = org.jetbrains.dokka
+                    
+                    --------------------------------------------------
+                    Variant dokkaModuleDescriptorElements
+                    --------------------------------------------------
+                    Provide Dokka module descriptor files to other subprojects
+                    
+                    Capabilities
+                        - :test:unspecified (default capability)
+                    Attributes
+                        - org.gradle.category = module-descriptor
+                        - org.gradle.usage    = org.jetbrains.dokka
+                    Artifacts
+                        - build/dokka/createDokkaModuleConfiguration.json (artifactType = json)
+                    
+                """.trimIndent()
+            ),
+            "expect output contains dokka variants\n\n${build.output.prependIndent("  | ")}"
+        )
+    }
+
+    @Test
+    fun `expect Dokka Plugin creates Dokka resolvable configurations`() {
+        val build = gradleKtsProjectTest {
+            buildGradleKts = """
+                plugins {
+                    id("org.jetbrains.dokka2") version "2.0.0"
+                }
+            """.trimIndent()
+        }.runner
+            .withArguments("resolvableConfigurations")
+            .build()
+
+        println(build.output.prependIndent("   | "))
+
+        assertTrue(
+            build.output.contains(
+                """
+                    --------------------------------------------------
+                    Configuration dokka
+                    --------------------------------------------------
+                    Fetch all Dokka files from all configurations in other subprojects
+                    
+                    Attributes
+                        - org.gradle.usage = org.jetbrains.dokka
+                    
+                    --------------------------------------------------
+                    Configuration dokkaConfigurations
+                    --------------------------------------------------
+                    Fetch Dokka Configuration files from other subprojects
+                    
+                    Attributes
+                        - org.gradle.category = configuration
+                        - org.gradle.usage    = org.jetbrains.dokka
+                    Extended Configurations
+                        - dokka
+                    
+                    --------------------------------------------------
+                    Configuration dokkaModuleDescriptor
+                    --------------------------------------------------
+                    Fetch Dokka module descriptor files from other subprojects
+                    
+                    Attributes
+                        - org.gradle.category = module-descriptor
+                        - org.gradle.usage    = org.jetbrains.dokka
+                    Extended Configurations
+                        - dokka
+                    
+                    --------------------------------------------------
+                    Configuration dokkaRuntimeClasspath
+                    --------------------------------------------------
+                    Dokka generation task runtime classpath
+                    
+                    Attributes
+                        - org.gradle.category            = library
+                        - org.gradle.dependency.bundling = external
+                        - org.gradle.jvm.environment     = standard-jvm
+                        - org.gradle.libraryelements     = jar
+                    
+                    -------------------------------------------------
+                """.trimIndent()
+            ),
+            "expect output contains dokka configurations\n\n${build.output.prependIndent("  | ")}"
         )
     }
 }
