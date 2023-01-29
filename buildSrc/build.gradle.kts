452f141b636642ccec0948e40c47d01eb1fb1488
Adam <897017+aSemy@users.noreply.github.com>
2023-01-29 23:31:54 +0100

    update binary compatibility plugin version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
     implementation("com.github.jengelman.gradle.plugins:shadow:2.0.4")
     implementation("gradle.plugin.com.github.johnrengelman:shadow:7.1.2")
-    implementation("org.jetbrains.kotlinx:binary-compatibility-validator:0.10.1")
+    implementation("org.jetbrains.kotlinx:binary-compatibility-validator:0.12.1")
     implementation("io.github.gradle-nexus:publish-plugin:1.1.0")
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.7.10")
     implementation("com.gradle.publish:plugin-publish-plugin:0.20.0")
