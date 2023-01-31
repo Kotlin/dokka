d85ec1d8ff5d69f0b557363b472cbdf25559797b
Ignat Beresnev <ignat.beresnev@jetbrains.com>
2023-01-31 18:38:09 +0100

    Make implementations of DokkaLogger thread-safe (#2827)

--- runners/maven-plugin/src/main/kotlin/MavenDokkaLogger.kt
+++ runners/maven-plugin/src/main/kotlin/MavenDokkaLogger.kt
@@ -2,14 +2,23 @@ package org.jetbrains.dokka.maven
 
 import org.apache.maven.plugin.logging.Log
 import org.jetbrains.dokka.utilities.DokkaLogger
+import java.util.concurrent.atomic.AtomicInteger
 
 class MavenDokkaLogger(val log: Log) : DokkaLogger {
-    override var warningsCount: Int = 0
-    override var errorsCount: Int = 0
+    private val warningsCounter = AtomicInteger()
+    private val errorsCounter = AtomicInteger()
+
+    override var warningsCount: Int
+        get() = warningsCounter.get()
+        set(value) = warningsCounter.set(value)
+
+    override var errorsCount: Int
+        get() = errorsCounter.get()
+        set(value) = errorsCounter.set(value)
 
     override fun debug(message: String) = log.debug(message)
     override fun info(message: String) = log.info(message)
     override fun progress(message: String) = log.info(message)
-    override fun warn(message: String) = log.warn(message).also { warningsCount++ }
-    override fun error(message: String) = log.error(message).also { errorsCount++ }
+    override fun warn(message: String) = log.warn(message).also { warningsCounter.incrementAndGet() }
+    override fun error(message: String) = log.error(message).also { errorsCounter.incrementAndGet() }
 }
