ed6c67bd55ad0211b9be40dda3027340eecd5919
Róbert Papp <papp.robert.s@gmail.com>
2023-02-01 12:35:09 +0000

    Add missing group and description for Multimodule tasks (#2806)
    * Change warning level as requested https://github.com/Kotlin/dokka/pull/2806#pullrequestreview-1278096135
    
    * Change the group of deprecated tasks to "deprecated" according to conversation https://github.com/Kotlin/dokka/pull/2806/files#r1093042654
    
    * Use lazy resolution of tasks and explicit test for deprecation.

--- runners/gradle-plugin/src/main/kotlin/org/jetbrains/dokka/gradle/DokkaPlugin.kt
+++ runners/gradle-plugin/src/main/kotlin/org/jetbrains/dokka/gradle/DokkaPlugin.kt
@@ -4,6 +4,7 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.Dependency
+import org.gradle.api.plugins.JavaBasePlugin
 import org.gradle.kotlin.dsl.register
 import org.gradle.util.GradleVersion
 
@@ -74,6 +75,8 @@ open class DokkaPlugin : Plugin<Project> {
                 }
 
                 project.tasks.register<DefaultTask>("${name}Multimodule") {
+                    group = "deprecated"
+                    description = "DEPRECATED: 'Multimodule' is deprecated. Use 'MultiModule' instead."
                     dependsOn(multiModuleName)
                     doLast {
                         logger.warn("'Multimodule' is deprecated. Use 'MultiModule' instead")
