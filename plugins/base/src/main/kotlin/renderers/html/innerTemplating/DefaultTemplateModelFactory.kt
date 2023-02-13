Kotlin/dokka

commit b64ca545a23b46996b8acc8167c17ce34d83ee33

Display source set / platform indicators at all times (#2848)
Fixes #2847

plugins/base/src/main/kotlin/renderers/html/innerTemplating/DefaultTemplateModelFactory.kt
@@ -40,7 +40,6 @@ class DefaultTemplateModelFactory(val context: DokkaContext) : TemplateModelFact
         page: PageNode,
         resources: List<String>,
         locationProvider: LocationProvider,
-        shouldRenderSourceSetBubbles: Boolean,
         content: String
     ): TemplateMap {
         val path = locationProvider.resolve(page)
@@ -66,7 +65,7 @@ class DefaultTemplateModelFactory(val context: DokkaContext) : TemplateModelFact
         }
         mapper["template_cmd"] = TemplateDirective(context.configuration, pathToRoot)
 
-        if (shouldRenderSourceSetBubbles && page is ContentPage) {
+        if (page is ContentPage) {
             val sourceSets = page.content.withDescendants()
                 .flatMap { it.sourceSets }
                 .distinct()

plugins/base/api/base.api
@@ -621,7 +621,7 @@ public final class org/jetbrains/dokka/base/renderers/html/command/consumers/Res
 
 public final class org/jetbrains/dokka/base/renderers/html/innerTemplating/DefaultTemplateModelFactory : org/jetbrains/dokka/base/renderers/html/innerTemplating/TemplateModelFactory {
 	public fun <init> (Lorg/jetbrains/dokka/plugability/DokkaContext;)V
-	public fun buildModel (Lorg/jetbrains/dokka/pages/PageNode;Ljava/util/List;Lorg/jetbrains/dokka/base/resolvers/local/LocationProvider;ZLjava/lang/String;)Ljava/util/Map;
+	public fun buildModel (Lorg/jetbrains/dokka/pages/PageNode;Ljava/util/List;Lorg/jetbrains/dokka/base/resolvers/local/LocationProvider;Ljava/lang/String;)Ljava/util/Map;
 	public fun buildSharedModel ()Ljava/util/Map;
 	public final fun getContext ()Lorg/jetbrains/dokka/plugability/DokkaContext;
 }
@@ -660,7 +660,7 @@ public final class org/jetbrains/dokka/base/renderers/html/innerTemplating/HtmlT
 }
 
 public abstract interface class org/jetbrains/dokka/base/renderers/html/innerTemplating/TemplateModelFactory {
-	public abstract fun buildModel (Lorg/jetbrains/dokka/pages/PageNode;Ljava/util/List;Lorg/jetbrains/dokka/base/resolvers/local/LocationProvider;ZLjava/lang/String;)Ljava/util/Map;
+	public abstract fun buildModel (Lorg/jetbrains/dokka/pages/PageNode;Ljava/util/List;Lorg/jetbrains/dokka/base/resolvers/local/LocationProvider;Ljava/lang/String;)Ljava/util/Map;
 	public abstract fun buildSharedModel ()Ljava/util/Map;
 }
 

plugins/base/src/main/kotlin/renderers/html/HtmlRenderer.kt
@@ -41,7 +41,7 @@ open class HtmlRenderer(
         setupSharedModel(templateModelMerger.invoke(templateModelFactories) { buildSharedModel() })
     }
 
-    private var shouldRenderSourceSetBubbles: Boolean = false
+    private var shouldRenderSourceSetTabs: Boolean = false
 
     override val preprocessors = context.plugin<DokkaBase>().query { htmlPreprocessors }
 
@@ -175,7 +175,7 @@ open class HtmlRenderer(
         pageContext: ContentPage,
         extra: PropertyContainer<ContentNode> = PropertyContainer.empty(),
         styles: Set<Style> = emptySet(),
-        shouldHaveTabs: Boolean = shouldRenderSourceSetBubbles
+        shouldHaveTabs: Boolean = shouldRenderSourceSetTabs
     ) {
         val contents = contentsForSourceSetDependent(nodes, pageContext)
         val isOnlyCommonContent = contents.singleOrNull()?.let { (sourceSet, _) ->
@@ -500,19 +500,17 @@ open class HtmlRenderer(
     }
 
     private fun FlowContent.createPlatformTagBubbles(sourceSets: List<DisplaySourceSet>, cssClasses: String = "") {
-        if (shouldRenderSourceSetBubbles) {
-            div("platform-tags $cssClasses") {
-                sourceSets.sortedBy { it.name }.forEach {
-                    div("platform-tag") {
-                        when (it.platform.key) {
-                            "common" -> classes = classes + "common-like"
-                            "native" -> classes = classes + "native-like"
-                            "jvm" -> classes = classes + "jvm-like"
-                            "js" -> classes = classes + "js-like"
-                            "wasm" -> classes = classes + "wasm-like"
-                        }
-                        text(it.name)
+        div("platform-tags $cssClasses") {
+            sourceSets.sortedBy { it.name }.forEach {
+                div("platform-tag") {
+                    when (it.platform.key) {
+                        "common" -> classes = classes + "common-like"
+                        "native" -> classes = classes + "native-like"
+                        "jvm" -> classes = classes + "jvm-like"
+                        "js" -> classes = classes + "js-like"
+                        "wasm" -> classes = classes + "wasm-like"
                     }
+                    text(it.name)
                 }
             }
         }
@@ -779,7 +777,7 @@ open class HtmlRenderer(
     }
 
     override fun render(root: RootPageNode) {
-        shouldRenderSourceSetBubbles = shouldRenderSourceSetBubbles(root)
+        shouldRenderSourceSetTabs = shouldRenderSourceSetTabs(root)
         super.render(root)
     }
 
@@ -805,7 +803,6 @@ open class HtmlRenderer(
                     page,
                     resources,
                     locationProvider,
-                    shouldRenderSourceSetBubbles,
                     generatedContent
                 )
             }

plugins/base/src/main/kotlin/renderers/html/innerTemplating/TemplateModelFactory.kt
@@ -8,7 +8,6 @@ interface TemplateModelFactory {
         page: PageNode,
         resources: List<String>,
         locationProvider: LocationProvider,
-        shouldRenderSourceSetBubbles: Boolean,
         content: String
     ): TemplateMap
 

plugins/base/src/main/kotlin/renderers/html/shouldRenderSourceSetBubbles.kt
@@ -4,7 +4,7 @@ import org.jetbrains.dokka.model.withDescendants
 import org.jetbrains.dokka.pages.ContentPage
 import org.jetbrains.dokka.pages.RootPageNode
 
-internal fun shouldRenderSourceSetBubbles(page: RootPageNode): Boolean {
+internal fun shouldRenderSourceSetTabs(page: RootPageNode): Boolean {
     return page.withDescendants()
         .flatMap { pageNode ->
             if (pageNode is ContentPage) pageNode.content.withDescendants()

commit e9bfda3a1c4f0f407b720e296adf3536f5878095

Do not render filter-section for empty source sets
Regression from #2848

plugins/base/src/main/kotlin/renderers/html/innerTemplating/DefaultTemplateModelFactory.kt
@@ -72,7 +72,10 @@ class DefaultTemplateModelFactory(val context: DokkaContext) : TemplateModelFact
                 .sortedBy { it.comparableKey }
                 .map { SourceSetModel(it.name, it.platform.key, it.sourceSetIDs.merged.toString()) }
                 .toList()
-            mapper["sourceSets"] = sourceSets
+
+            if (sourceSets.isNotEmpty()) {
+                mapper["sourceSets"] = sourceSets
+            }
         }
         return mapper
     }

plugins/base/src/main/resources/dokka/templates/includes/source_set_selector.ftl
@@ -1,5 +1,5 @@
 <#macro display>
-    <#if sourceSets??>
+    <#if sourceSets?has_content>
         <div class="filter-section" id="filter-section">
             <#list sourceSets as ss>
                 <button class="platform-tag platform-selector ${ss.platform}-like" data-active="" data-filter="${ss.filter}">${ss.name}</button>