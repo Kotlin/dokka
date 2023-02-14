2c5e250a1b960b017c07adb2b6fcca2e00278e49
Goooler <wangzongler@gmail.com>
2023-02-14 15:35:42 +0800

    binary-compatibility-validator 0.12.1

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,6 +8,6 @@ repositories {
 
 dependencies {
     implementation("gradle.plugin.com.github.johnrengelman:shadow:7.1.2")
-    implementation("org.jetbrains.kotlinx:binary-compatibility-validator:0.10.1")
+    implementation("org.jetbrains.kotlinx:binary-compatibility-validator:0.12.1")
     implementation("io.github.gradle-nexus:publish-plugin:1.1.0")
 }
