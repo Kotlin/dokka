a095ab1ceb0144186c65e681cdc6f03f501c2eb0
Adam <897017+aSemy@users.noreply.github.com>
2023-02-16 10:34:57 +0100

    formatting

--- runners/gradle-plugin/src/main/kotlin/org/jetbrains/dokka/gradle/internal/LoggerAdapter.kt
+++ runners/gradle-plugin/src/main/kotlin/org/jetbrains/dokka/gradle/internal/LoggerAdapter.kt
@@ -1,10 +1,8 @@
 package org.jetbrains.dokka.gradle.internal
 
-import java.util.concurrent.atomic.AtomicInteger
-import kotlin.reflect.KClass
 import org.gradle.api.logging.Logger
-import org.gradle.api.logging.Logging
 import org.jetbrains.dokka.utilities.DokkaLogger
+import java.util.concurrent.atomic.AtomicInteger
 
 /**
  * Adapt a Gradle [Logger] to a [DokkaLogger], to be used by Dokka when generating documentation.
@@ -18,31 +16,31 @@ import org.jetbrains.dokka.utilities.DokkaLogger
  * @see org.jetbrains.dokka.gradle.AbstractDokkaTask.generateDocumentation
  */
 internal class LoggerAdapter(
-  private val logger: Logger
+    private val logger: Logger
 ) : DokkaLogger {
 
-  private val warningsCounter = AtomicInteger()
-  private val errorsCounter = AtomicInteger()
+    private val warningsCounter = AtomicInteger()
+    private val errorsCounter = AtomicInteger()
 
-  override var warningsCount: Int
-    get() = warningsCounter.get()
-    set(value) = warningsCounter.set(value)
+    override var warningsCount: Int
+        get() = warningsCounter.get()
+        set(value) = warningsCounter.set(value)
 
-  override var errorsCount: Int
-    get() = errorsCounter.get()
-    set(value) = errorsCounter.set(value)
+    override var errorsCount: Int
+        get() = errorsCounter.get()
+        set(value) = errorsCounter.set(value)
 
-  override fun debug(message: String) = logger.debug(message)
-  override fun progress(message: String) = logger.lifecycle(message)
-  override fun info(message: String) = logger.info(message)
+    override fun debug(message: String) = logger.debug(message)
+    override fun progress(message: String) = logger.lifecycle(message)
+    override fun info(message: String) = logger.info(message)
 
-  override fun warn(message: String) {
-    warningsCount++
-    logger.warn(message)
-  }
+    override fun warn(message: String) {
+        warningsCount++
+        logger.warn(message)
+    }
 
-  override fun error(message: String) {
-    errorsCount++
-    logger.error(message)
-  }
+    override fun error(message: String) {
+        errorsCount++
+        logger.error(message)
+    }
 }
