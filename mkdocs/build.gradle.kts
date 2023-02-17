1e10c6f47d605e1e730cb58aa44f3051ec5a493b
Ignat Beresnev <ignat.beresnev@jetbrains.com>
2023-02-17 22:18:48 +0100

    Fix mkdocs documentation leading to non-stable versions (#2870)
    Fixes #2869

--- mkdocs/build.gradle.kts
+++ mkdocs/build.gradle.kts
@@ -1,3 +1,15 @@
+import org.jetbrains.dokkaVersionType
+import org.jetbrains.DokkaVersionType
+
 plugins {
     id("ru.vyarus.mkdocs") version "2.3.0"
+}
+
+if (dokkaVersionType != DokkaVersionType.RELEASE) {
+    // Do not generate the root index.html file with the redirect
+    // to a non-release version, otherwise GitHub pages based documentation
+    // will always lead to the non-stable documentation.
+    // For more details, see https://github.com/Kotlin/dokka/issues/2869.
+    // For configuration details, see https://xvik.github.io/gradle-mkdocs-plugin/3.0.0/examples/#simple-multi-version.
+    mkdocs.publish.rootRedirect = false
 }
