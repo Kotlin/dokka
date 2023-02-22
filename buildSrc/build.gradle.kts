4d8649d948e171f9e2432ce09d630bea1d564b78
Adam <897017+aSemy@users.noreply.github.com>
2023-02-22 10:27:36 +0100

    update Kotlin version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.*
+
 plugins {
     `kotlin-dsl`
 }
@@ -6,13 +8,20 @@ repositories {
     gradlePluginPortal()
 }
 
+// TODO define versions in Gradle Version Catalog
+val properties = file("../gradle.properties").inputStream().use {
+    Properties().apply { load(it) }
+}
+
+val kotlinVersion = properties["kotlin_version"]
+
 dependencies {
     // Import Gradle Plugins that will be used in the buildSrc pre-compiled script plugins, and any `build.gradle.kts`
     // files in the project.
     // Use their Maven coordinates (plus versions), not Gradle plugin IDs!
     // This should be the only place that Gradle plugin versions are defined, so they are aligned across all build scripts
 
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     implementation("com.github.jengelman.gradle.plugins:shadow:2.0.4")
     implementation("gradle.plugin.com.github.johnrengelman:shadow:7.1.2")
     implementation("org.jetbrains.kotlinx:binary-compatibility-validator:0.12.1")
