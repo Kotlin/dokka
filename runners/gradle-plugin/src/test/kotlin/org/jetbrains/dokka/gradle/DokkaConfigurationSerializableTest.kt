6af794d4173d6aef9b84dbc7647eb3a1a54677d5
aSemy <897017+aSemy@users.noreply.github.com>
2023-02-27 16:50:38 +0100

    Use Gradle file property types for task inputs & outputs (#2707)

--- runners/gradle-plugin/src/test/kotlin/org/jetbrains/dokka/gradle/DokkaConfigurationSerializableTest.kt
+++ runners/gradle-plugin/src/test/kotlin/org/jetbrains/dokka/gradle/DokkaConfigurationSerializableTest.kt
@@ -4,6 +4,9 @@ import org.gradle.kotlin.dsl.withType
 import org.gradle.testfixtures.ProjectBuilder
 import org.jetbrains.dokka.DokkaConfiguration
 import org.jetbrains.dokka.PluginConfigurationImpl
+import org.jetbrains.dokka.gradle.utils.create_
+import org.jetbrains.dokka.gradle.utils.externalDocumentationLink_
+import org.jetbrains.dokka.gradle.utils.withDependencies_
 import org.junit.Rule
 import org.junit.rules.TemporaryFolder
 import java.io.File
@@ -24,19 +27,31 @@ class DokkaConfigurationSerializableTest {
         val project = ProjectBuilder.builder().build()
         project.plugins.apply("org.jetbrains.dokka")
         val dokkaTask = project.tasks.withType<DokkaTask>().first()
-        dokkaTask.plugins.withDependencies { clear() }
+        dokkaTask.plugins.withDependencies_ { clear() }
         dokkaTask.apply {
             this.failOnWarning.set(true)
             this.offlineMode.set(true)
             this.outputDirectory.set(File("customOutputDir"))
             this.cacheRoot.set(File("customCacheRoot"))
-            this.pluginsConfiguration.add(PluginConfigurationImpl("A", DokkaConfiguration.SerializationFormat.JSON, """ { "key" : "value1" } """))
-            this.pluginsConfiguration.add(PluginConfigurationImpl("B", DokkaConfiguration.SerializationFormat.JSON, """ { "key" : "value2" } """))
-            this.dokkaSourceSets.create("main") {
+            this.pluginsConfiguration.add(
+                PluginConfigurationImpl(
+                    "A",
+                    DokkaConfiguration.SerializationFormat.JSON,
+                    """ { "key" : "value1" } """
+                )
+            )
+            this.pluginsConfiguration.add(
+                PluginConfigurationImpl(
+                    "B",
+                    DokkaConfiguration.SerializationFormat.JSON,
+                    """ { "key" : "value2" } """
+                )
+            )
+            this.dokkaSourceSets.create_("main") {
                 displayName.set("customSourceSetDisplayName")
                 reportUndocumented.set(true)
 
-                externalDocumentationLink {
+                externalDocumentationLink_ {
                     packageListUrl.set(URL("http://some.url"))
                     url.set(URL("http://some.other.url"))
                 }
