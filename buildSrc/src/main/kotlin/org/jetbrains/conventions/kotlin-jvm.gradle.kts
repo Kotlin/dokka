7c49e099837b3a605e7022d6684c2ddb166831ac
Adam <897017+aSemy@users.noreply.github.com>
2023-02-28 22:46:05 +0100

    tidy up compiler args

--- buildSrc/src/main/kotlin/org/jetbrains/conventions/kotlin-jvm.gradle.kts
+++ buildSrc/src/main/kotlin/org/jetbrains/conventions/kotlin-jvm.gradle.kts
@@ -15,12 +15,10 @@ val language_version: String by project
 
 tasks.withType<KotlinCompile>().configureEach {
     kotlinOptions {
-        freeCompilerArgs = freeCompilerArgs + listOf(
+        freeCompilerArgs += listOf(
             "-opt-in=kotlin.RequiresOptIn",
             "-Xjsr305=strict",
             "-Xskip-metadata-version-check",
-            // need 1.4 support, otherwise there might be problems with Gradle 6.x (it's bundling Kotlin 1.4)
-            "-Xsuppress-version-warnings"
         )
         allWarningsAsErrors = true
         languageVersion = language_version
