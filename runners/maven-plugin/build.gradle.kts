d3c4c7c67c9fa2515d5aa19db7c698d21e91c5fa
Adam <897017+aSemy@users.noreply.github.com>
2023-03-09 00:37:58 +0100

    tidying up, tests working

--- runners/maven-plugin/build.gradle.kts
+++ runners/maven-plugin/build.gradle.kts
@@ -1,3 +1,4 @@
+import org.gradle.kotlin.dsl.support.appendReproducibleNewLine
 import org.jetbrains.CrossPlatformExec
 import org.jetbrains.registerDokkaArtifactPublication
 
@@ -6,8 +7,6 @@ plugins {
     org.jetbrains.conventions.`maven-publish`
     org.jetbrains.conventions.`maven-cli-setup`
 }
-
-val pomTemplateFile = layout.projectDirectory.file("pom.tpl.xml")
 
 dependencies {
     implementation(project(":core"))
@@ -18,11 +17,16 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
 }
 
+val mavenPluginTaskGroup = "maven plugin"
+
 val generatePom by tasks.registering(Sync::class) {
     description = "Generate pom.xml for Maven Plugin Plugin"
+    group = mavenPluginTaskGroup
 
     val dokka_version: String by project
     inputs.property("dokka_version", dokka_version)
+
+    val pomTemplateFile = layout.projectDirectory.file("pom.tpl.xml")
 
     from(pomTemplateFile) {
         rename { it.replace(".tpl.xml", ".xml") }
@@ -37,31 +41,49 @@ val generatePom by tasks.registering(Sync::class) {
     into(temporaryDir)
 }
 
-val prepareCompiledClasses by tasks.registering(Sync::class) {
-    description = "Prepare compiled classes for Maven Plugin task execution"
-
-    from(tasks.compileKotlin.flatMap { it.destinationDirectory })
-    from(tasks.compileJava.flatMap { it.destinationDirectory })
-    into(temporaryDir)
-}
-
 val prepareMavenPluginBuildDir by tasks.registering(Sync::class) {
-    description = "Prepare files for Maven Plugin task execution"
-    from(prepareCompiledClasses) { into("classes/java/main") }
+    description = "Prepares all files for Maven Plugin task execution"
+    group = mavenPluginTaskGroup
+
+    from(tasks.compileKotlin.flatMap { it.destinationDirectory }) { into("classes/java/main") }
+    from(tasks.compileJava.flatMap { it.destinationDirectory }) { into("classes/java/main") }
+
     from(generatePom)
+
     into(setupMavenProperties.mavenBuildDir)
 }
 
 val helpMojo by tasks.registering(CrossPlatformExec::class) {
+    group = mavenPluginTaskGroup
+
     dependsOn(tasks.installMavenBinary, prepareMavenPluginBuildDir)
     workingDir(setupMavenProperties.mavenBuildDir)
     executable(setupMavenProperties.mvn.get())
     args("-e", "-B", "org.apache.maven.plugins:maven-plugin-plugin:helpmojo")
 
     outputs.dir(setupMavenProperties.mavenBuildDir)
+
+    doLast("normalize maven-plugin-help.properties") {
+        // The maven-plugin-help.properties file contains a timestamp by default.
+        // It should be removed as it is not reproducible and impacts Gradle caching
+        val pluginHelpProperties = workingDir.resolve("maven-plugin-help.properties")
+        pluginHelpProperties.writeText(
+            buildString {
+                val lines = pluginHelpProperties.readText().lines().iterator()
+                // the first line is a descriptive comment
+                appendReproducibleNewLine(lines.next())
+                // the second line is the timestamp, which should be ignored
+                lines.next()
+                // the remaining lines are properties
+                lines.forEach { appendReproducibleNewLine(it) }
+            }
+        )
+    }
 }
 
 val pluginDescriptor by tasks.registering(CrossPlatformExec::class) {
+    group = mavenPluginTaskGroup
+
     dependsOn(tasks.installMavenBinary, prepareMavenPluginBuildDir)
     workingDir(setupMavenProperties.mavenBuildDir)
     executable(setupMavenProperties.mvn.get())
@@ -73,7 +95,7 @@ val pluginDescriptor by tasks.registering(CrossPlatformExec::class) {
 tasks.jar {
     dependsOn(pluginDescriptor, helpMojo)
     metaInf {
-        from("${setupMavenProperties.mavenBuildDir}/classes/java/main/META-INF")
+        from(setupMavenProperties.mavenBuildDir.map { it.dir("classes/java/main/META-INF") })
     }
     manifest {
         attributes("Class-Path" to configurations.runtimeClasspath.map { configuration ->
