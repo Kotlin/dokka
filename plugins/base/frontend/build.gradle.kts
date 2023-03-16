392dea23bab5e95c532cf54e9af9af49183b0991
Ignat Beresnev <ignat.beresnev@jetbrains.com>
2023-03-16 21:03:47 +0100

    Use cache-redirector for nodejs distributions

--- plugins/base/frontend/build.gradle.kts
+++ plugins/base/frontend/build.gradle.kts
@@ -5,7 +5,10 @@ plugins {
 
 node {
     version.set(libs.versions.node)
+
+    // https://github.com/node-gradle/gradle-node-plugin/blob/3.5.1/docs/faq.md#is-this-plugin-compatible-with-centralized-repositories-declaration
     download.set(true)
+    distBaseUrl.set(null as String?) // Strange cast to avoid overload ambiguity
 }
 
 val npmRunBuild = tasks.getByName("npm_run_build") {
