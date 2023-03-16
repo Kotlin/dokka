8f0ab249e2682576c4ed872c042318c49fccb542
Adam <897017+aSemy@users.noreply.github.com>
2023-03-16 17:21:53 +0100

    use JUnit Platform & add necessary runtime dependencies

--- build-logic/src/main/kotlin/org/jetbrains/conventions/base-java.gradle.kts
+++ build-logic/src/main/kotlin/org/jetbrains/conventions/base-java.gradle.kts
@@ -23,9 +23,22 @@ java {
 }
 
 tasks.withType<Test>().configureEach {
-    useJUnit()
+    useJUnitPlatform()
 
     javaLauncher.set(javaToolchains.launcherFor {
         languageVersion.set(dokkaBuild.testJavaLauncherVersion)
     })
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.2"))
+    // TODO Upgrade all subprojects to use JUnit Jupiter https://github.com/Kotlin/dokka/issues/2924
+    // Ideally convention plugins should only provide sensible defaults that can be overridden by subprojects.
+    // If a convention plugin defines dependencies, these cannot be easily overridden by subprojects, and so
+    // this should be avoided. However, for now , both JUnit 4 and 5 must be supported, and since these are test
+    // runtime-only dependencies they are not going to have a significant impact subprojects.
+    // These dependencies should be revisited in #2924, and (for example) moved to each subproject (which is more
+    // repetitive, but more declarative and clear), or some other solution.
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine")
 }
