a586e250b981dce7745d1820363f8d9dfc0caf99
Adam <897017+aSemy@users.noreply.github.com>
2023-03-16 14:11:57 +0100

    add JUnit engines for both JUnit 4 and 5

--- test-utils/build.gradle.kts
+++ test-utils/build.gradle.kts
@@ -4,4 +4,9 @@ plugins {
 
 dependencies {
     api(kotlin("test-junit"))
+
+    api(platform("org.junit:junit-bom:5.9.2"))
+    // support both JUnit 4 and JUnit 5 tests
+    api("org.junit.jupiter:junit-jupiter-engine")
+    api("org.junit.vintage:junit-vintage-engine")
 }
