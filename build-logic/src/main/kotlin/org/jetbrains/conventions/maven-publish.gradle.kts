fe265baed7e72cb3694b9dea6575a870bf1cbf62
aSemy <897017+aSemy@users.noreply.github.com>
2023-04-17 14:43:15 +0200

    Disable publication of shadowRuntimeElements variant (#2961)

--- build-logic/src/main/kotlin/org/jetbrains/conventions/maven-publish.gradle.kts
+++ build-logic/src/main/kotlin/org/jetbrains/conventions/maven-publish.gradle.kts
@@ -1,5 +1,7 @@
 package org.jetbrains.conventions
 
+import com.github.jengelman.gradle.plugins.shadow.ShadowPlugin
+
 plugins {
     id("org.jetbrains.conventions.base")
     `maven-publish`
@@ -55,4 +57,12 @@ publishing {
             }
         }
     }
+}
+
+plugins.withType<ShadowPlugin>().configureEach {
+    // manually disable publication of Shadow elements https://github.com/johnrengelman/shadow/issues/651#issue-839148311
+    // This is done to preserve compatibility and have the same behaviour as previous versions of Dokka.
+    // For more details, see https://github.com/Kotlin/dokka/pull/2704#issuecomment-1499517930
+    val javaComponent = components["java"] as AdhocComponentWithVariants
+    javaComponent.withVariantsFromConfiguration(configurations["shadowRuntimeElements"]) { skip() }
 }
