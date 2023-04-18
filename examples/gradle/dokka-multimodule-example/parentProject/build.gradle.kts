0bab7db6d61f815a2398ac1fd3fb1189f9cffccb
Ignat Beresnev <ignat.beresnev@jetbrains.com>
2023-04-18 12:10:33 +0200

    Add source links to the dokka-multimodule-example project

--- examples/gradle/dokka-multimodule-example/parentProject/build.gradle.kts
+++ examples/gradle/dokka-multimodule-example/parentProject/build.gradle.kts
@@ -1,5 +1,6 @@
 import org.jetbrains.dokka.DokkaConfiguration.Visibility
 import org.jetbrains.dokka.gradle.DokkaTaskPartial
+import java.net.URL
 
 plugins {
     kotlin("jvm")
@@ -17,6 +18,19 @@ subprojects {
                 Visibility.PUBLIC,
                 Visibility.PROTECTED
             ))
+
+            // In multi-project builds, `remoteUrl` must point to that project's dir specifically, so if you
+            // want to configure sourceLinks at once in `subprojects {}`, you have to find the relative path.
+            // Alternatively, you can move this configuration up into subproject build scripts,
+            // and just hardcode the exact paths as demonstrated in the basic dokka-gradle-example.
+            sourceLink {
+                val exampleDir = "https://github.com/Kotlin/dokka/tree/master/examples/gradle/dokka-multimodule-example"
+                val projectRelativePath = rootProject.projectDir.toPath().relativize(projectDir.toPath())
+
+                localDirectory.set(file("src"))
+                remoteUrl.set(URL("$exampleDir/$projectRelativePath/src"))
+                remoteLineSuffix.set("#L")
+            }
         }
     }
 }
