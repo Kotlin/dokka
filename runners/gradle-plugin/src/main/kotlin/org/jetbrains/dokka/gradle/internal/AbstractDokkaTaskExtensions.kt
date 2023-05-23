acd6257a674d4415860b6eac3f70a8c4394ade89
Ignat Beresnev <ignat.beresnev@jetbrains.com>
2023-05-23 20:39:46 +0200

    Add the ability to get serialized configuration of a task

---
+++ runners/gradle-plugin/src/main/kotlin/org/jetbrains/dokka/gradle/internal/AbstractDokkaTaskExtensions.kt
@@ -0,0 +1,18 @@
+package org.jetbrains.dokka.gradle.internal
+
+import org.jetbrains.dokka.InternalDokkaApi
+import org.jetbrains.dokka.gradle.AbstractDokkaTask
+import org.jetbrains.dokka.toPrettyJsonString
+import org.jetbrains.dokka.DokkaConfiguration
+
+/**
+ * Serializes [DokkaConfiguration] of this [AbstractDokkaTask] as pretty json
+ *
+ * Should be used for debugging only, no guarantees are given for the support of this API.
+ *
+ * Better alternative should be introduced as part of [#2873](https://github.com/Kotlin/dokka/issues/2873).
+ */
+@InternalDokkaApi
+fun AbstractDokkaTask.getJsonConfiguration(): String {
+    return this.buildDokkaConfiguration().toPrettyJsonString()
+}
