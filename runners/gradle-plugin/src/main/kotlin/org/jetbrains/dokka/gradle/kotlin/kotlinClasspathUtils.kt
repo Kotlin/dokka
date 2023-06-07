c7154ef26e1d2aa31a41b582b723e544e797c8f8
IgnatBeresnev <ignat.beresnev@jetbrains.com>
2023-06-07 14:48:34 +0200

    Remove the HMPP filter

--- runners/gradle-plugin/src/main/kotlin/org/jetbrains/dokka/gradle/kotlin/kotlinClasspathUtils.kt
+++ runners/gradle-plugin/src/main/kotlin/org/jetbrains/dokka/gradle/kotlin/kotlinClasspathUtils.kt
@@ -16,14 +16,6 @@ internal fun Project.classpathOf(sourceSet: KotlinSourceSet): FileCollection {
     val compilations = compilationsOf(sourceSet)
     return if (compilations.isNotEmpty()) {
         compilations
-            /**
-             * If the project has enabled Compatibility Metadata Variant (produces legacy variant),
-             * we don't touch it due to some dependant library
-             * might be published without Compatibility Metadata Variant.
-             * Dokka needs only HMPP variant
-             * Ignore [org.jetbrains.kotlin.gradle.plugin.mpp.KotlinCommonCompilation]  for `commonMain`  sourceSet with name `main`
-             */
-            .filterNot { compilation -> isHMPPEnabled && compilation is KotlinCommonCompilation && compilation.name == "main" }
             .map { compilation -> compilation.compileClasspathOf(project = this) }
             .reduce { acc, fileCollection -> acc + fileCollection }
     } else {
