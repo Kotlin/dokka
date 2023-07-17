c226d00a3cab1e0f522344cb7b74d8e5903f6e31
Ignat Beresnev <ignat.beresnev@jetbrains.com>
2023-07-17 11:22:35 +0200

    Hardcode documentation for the synthetic Enum.entries property (#3071)

--- subprojects/analysis-kotlin-descriptors/compiler/src/main/kotlin/org/jetbrains/dokka/analysis/kotlin/descriptors/compiler/translator/SyntheticDescriptorDocumentationProvider.kt
+++ subprojects/analysis-kotlin-descriptors/compiler/src/main/kotlin/org/jetbrains/dokka/analysis/kotlin/descriptors/compiler/translator/SyntheticDescriptorDocumentationProvider.kt
@@ -6,10 +6,15 @@ import org.jetbrains.dokka.analysis.kotlin.descriptors.compiler.configuration.fr
 import org.jetbrains.dokka.analysis.markdown.jb.MarkdownParser
 import org.jetbrains.dokka.links.DRI
 import org.jetbrains.dokka.model.doc.DocumentationNode
+import org.jetbrains.kotlin.builtins.StandardNames
+import org.jetbrains.kotlin.descriptors.CallableMemberDescriptor
 import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
 import org.jetbrains.kotlin.descriptors.FunctionDescriptor
+import org.jetbrains.kotlin.descriptors.PropertyDescriptor
 import org.jetbrains.kotlin.resolve.DescriptorFactory
+import org.jetbrains.kotlin.resolve.DescriptorUtils
 
+private const val ENUM_ENTRIES_TEMPLATE_PATH = "/dokka/docs/kdoc/EnumEntries.kt.template"
 private const val ENUM_VALUEOF_TEMPLATE_PATH = "/dokka/docs/kdoc/EnumValueOf.kt.template"
 private const val ENUM_VALUES_TEMPLATE_PATH = "/dokka/docs/kdoc/EnumValues.kt.template"
 
@@ -17,14 +22,41 @@ internal class SyntheticDescriptorDocumentationProvider(
     private val kDocFinder: KDocFinder,
     private val sourceSet: DokkaConfiguration.DokkaSourceSet
 ) {
-    fun isDocumented(descriptor: DeclarationDescriptor): Boolean = descriptor is FunctionDescriptor
-            && (DescriptorFactory.isEnumValuesMethod(descriptor) || DescriptorFactory.isEnumValueOfMethod(descriptor))
+    fun isDocumented(descriptor: DeclarationDescriptor): Boolean {
+        return when(descriptor) {
+            is PropertyDescriptor -> descriptor.isEnumEntries()
+            is FunctionDescriptor -> {
+                DescriptorFactory.isEnumValuesMethod(descriptor) || DescriptorFactory.isEnumValueOfMethod(descriptor)
+            }
+            else -> false
+        }
+    }
+
+    private fun PropertyDescriptor.isEnumEntries(): Boolean {
+        return this.name == StandardNames.ENUM_ENTRIES
+                && this.kind == CallableMemberDescriptor.Kind.SYNTHESIZED
+                && DescriptorUtils.isEnumClass(this.containingDeclaration)
+    }
 
     fun getDocumentation(descriptor: DeclarationDescriptor): DocumentationNode? {
-        val function = descriptor as? FunctionDescriptor ?: return null
+        return when(descriptor) {
+            is PropertyDescriptor -> descriptor.getDocumentation()
+            is FunctionDescriptor -> descriptor.getDocumentation()
+            else -> null
+        }
+    }
+
+    private fun PropertyDescriptor.getDocumentation(): DocumentationNode? {
         return when {
-            DescriptorFactory.isEnumValuesMethod(function) -> loadTemplate(descriptor, ENUM_VALUES_TEMPLATE_PATH)
-            DescriptorFactory.isEnumValueOfMethod(function) -> loadTemplate(descriptor, ENUM_VALUEOF_TEMPLATE_PATH)
+            this.isEnumEntries() -> loadTemplate(this, ENUM_ENTRIES_TEMPLATE_PATH)
+            else -> null
+        }
+    }
+
+    private fun FunctionDescriptor.getDocumentation(): DocumentationNode? {
+        return when {
+            DescriptorFactory.isEnumValuesMethod(this) -> loadTemplate(this, ENUM_VALUES_TEMPLATE_PATH)
+            DescriptorFactory.isEnumValueOfMethod(this) -> loadTemplate(this, ENUM_VALUEOF_TEMPLATE_PATH)
             else -> null
         }
     }
