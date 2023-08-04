b559131ddda8efea3394a0ea641460c4189769db
Ignat Beresnev <ignat.beresnev@jetbrains.com>
2023-08-04 18:57:00 +0200

    Make the compatibility metadata variant check more specific (#3103)

--- runners/gradle-plugin/src/main/kotlin/org/jetbrains/dokka/gradle/kotlin/kotlinClasspathUtils.kt
+++ runners/gradle-plugin/src/main/kotlin/org/jetbrains/dokka/gradle/kotlin/kotlinClasspathUtils.kt
@@ -5,25 +5,12 @@ import org.gradle.api.file.FileCollection
 import org.jetbrains.dokka.gradle.isAndroidTarget
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jetbrains.kotlin.gradle.plugin.mpp.AbstractKotlinNativeCompilation
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinCommonCompilation
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-val Project.isHMPPEnabled
-    // [KotlinCommonCompilation.isKlibCompilation] is internal, so we use this
-    get() = (this.findProperty("kotlin.mpp.enableGranularSourceSetsMetadata") as? String)?.toBoolean() ?: false
 
 internal fun Project.classpathOf(sourceSet: KotlinSourceSet): FileCollection {
     val compilations = compilationsOf(sourceSet)
     return if (compilations.isNotEmpty()) {
         compilations
-            /**
-             * If the project has enabled Compatibility Metadata Variant (produces legacy variant),
-             * we don't touch it due to some dependant library
-             * might be published without Compatibility Metadata Variant.
-             * Dokka needs only HMPP variant
-             * Ignore [org.jetbrains.kotlin.gradle.plugin.mpp.KotlinCommonCompilation]  for `commonMain`  sourceSet with name `main`
-             */
-            .filterNot { compilation -> isHMPPEnabled && compilation is KotlinCommonCompilation && compilation.name == "main" }
             .map { compilation -> compilation.compileClasspathOf(project = this) }
             .reduce { acc, fileCollection -> acc + fileCollection }
     } else {
