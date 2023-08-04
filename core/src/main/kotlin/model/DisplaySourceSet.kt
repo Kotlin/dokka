a6e876223703b35b10b1211fef35d9d72b105604
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2023-08-04 18:42:54 +0200

    Deprecate internal API SelfRepresentingSingletonSet for removal as being harmful and unimplement it in DisplaySourceSet
    * Provide no automatic migration for DisplaySourceSet as there is no mechanisms for that. Manual migration is replacement of 'dss' to `setOf(dss)` where applicable
    * Introduce a convenience-member DefaultRenderer.buildContentNode to avoid wrapping DSS into set manually
    
    Fixes #2897

--- core/src/main/kotlin/model/DisplaySourceSet.kt
+++ core/src/main/kotlin/model/DisplaySourceSet.kt
@@ -3,21 +3,13 @@ package org.jetbrains.dokka.model
 import org.jetbrains.dokka.DokkaConfiguration.DokkaSourceSet
 import org.jetbrains.dokka.DokkaSourceSetID
 import org.jetbrains.dokka.Platform
-import org.jetbrains.dokka.utilities.SelfRepresentingSingletonSet
 
-/**
- * TODO: fix the example (asymmetric equivalence relation with [Set]):
- * ```
- * val ds = DokkaSourceSetImpl(sourceSetID = DokkaSourceSetID("", "")).toDisplaySourceSet()
- * println(setOf(ds) == ds) // true
- * println(ds == setOf(ds)) // false
- * ```
- */
+
 data class DisplaySourceSet(
     val sourceSetIDs: CompositeSourceSetID,
     val name: String,
     val platform: Platform
-) : SelfRepresentingSingletonSet<DisplaySourceSet> {
+) {
     constructor(sourceSet: DokkaSourceSet) : this(
         sourceSetIDs = CompositeSourceSetID(sourceSet.sourceSetID),
         name = sourceSet.displayName,
