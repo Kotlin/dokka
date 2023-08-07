3225c5afaa365d9431c2e27c6f6c49086dd6b0c4
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2023-08-07 20:23:24 +0200

    Get rid of bintray and jcenter mentions in our buildscript (#3109)
    Otherwise, our integration tests try to resolve these repositories on failures

--- integration-tests/gradle/projects/template.root.gradle.kts
+++ integration-tests/gradle/projects/template.root.gradle.kts
@@ -1,11 +1,8 @@
 allprojects {
     repositories {
-        maven("https://cache-redirector.jetbrains.com/jcenter.bintray.com")
         mavenLocal()
         mavenCentral()
         google()
-        maven("https://cache-redirector.jetbrains.com/dl.bintray.com/kotlin/kotlin-eap")
-        maven("https://cache-redirector.jetbrains.com/dl.bintray.com/kotlin/kotlin-dev")
         maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven") {
             content {
                 includeGroup("org.jetbrains.kotlinx")
