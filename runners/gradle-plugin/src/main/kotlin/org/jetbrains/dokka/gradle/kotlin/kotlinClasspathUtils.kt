4096e7d0dff51ab804cc8cfebec3cbc40cec22dc
IgnatBeresnev <ignat.beresnev@jetbrains.com>
2023-08-31 19:42:46 +0200

    Add hidden workaround for native dependencies

--- runners/gradle-plugin/src/main/kotlin/org/jetbrains/dokka/gradle/kotlin/kotlinClasspathUtils.kt
+++ runners/gradle-plugin/src/main/kotlin/org/jetbrains/dokka/gradle/kotlin/kotlinClasspathUtils.kt
@@ -7,6 +7,13 @@ import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jetbrains.kotlin.gradle.plugin.mpp.AbstractKotlinNativeCompilation
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompileTool
+import org.jetbrains.kotlin.gradle.utils.NativeCompilerDownloader
+import org.jetbrains.kotlin.konan.library.KONAN_DISTRIBUTION_COMMON_LIBS_DIR
+import org.jetbrains.kotlin.konan.library.KONAN_DISTRIBUTION_KLIB_DIR
+import org.jetbrains.kotlin.konan.library.KONAN_DISTRIBUTION_PLATFORM_LIBS_DIR
+import org.jetbrains.kotlin.konan.library.KONAN_STDLIB_NAME
+import org.jetbrains.kotlin.konan.target.KonanTarget
+import java.io.File
 
 internal fun Project.classpathOf(sourceSet: KotlinSourceSet): FileCollection {
     val compilations = compilationsOf(sourceSet)
@@ -78,12 +85,66 @@ private fun KotlinCompilation.getKotlinCompileTask(kgpVersion: KotlinGradlePlugi
 
 private fun KotlinCompilation.platformDependencyFiles(project: Project): FileCollection {
     val excludePlatformDependencyFiles = project.classpathProperty("excludePlatformDependencyFiles", default = false)
-
     if (excludePlatformDependencyFiles) return project.files()
+
+    val useNativeDistributionAccessor = project.classpathProperty("useNativeDistributionAccessor", default = false)
+    if (useNativeDistributionAccessor) return this.getPlatformDependenciesFromNativeDistributionAccessor(project)
+
+    val useKonanDistribution = project.classpathProperty("useKonanDistribution", default = false)
+    if (useKonanDistribution) return this.getPlatfromDependenciesFromKonanDistribution(project)
+
     return (this as? AbstractKotlinNativeCompilation)
         ?.target?.project?.configurations
         ?.findByName(@Suppress("DEPRECATION") this.defaultSourceSet.implementationMetadataConfigurationName) // KT-58640
         ?: project.files()
+}
+
+// https://github.com/Kotlin/dokka/pull/3147
+private fun KotlinCompilation.getPlatformDependenciesFromNativeDistributionAccessor(project: Project): FileCollection {
+    return if (this is AbstractKotlinNativeCompilation) {
+        val result = project.objects.fileCollection()
+        val kotlinNativeDistributionAccessor = KotlinNativeDistributionAccessor(project)
+        result.from(kotlinNativeDistributionAccessor.stdlibDir)
+        result.from(kotlinNativeDistributionAccessor.platformDependencies(konanTarget))
+        result
+    } else {
+        project.files()
+    }
+}
+
+// -------- The hack for platform dependencies from compiler ------------------
+// adapted from https://github.com/jetbrains/kotlin/blob/b6a215d681695a2fe0cc798308966c5675de447f/libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/native/internal/KotlinNativePlatformDependencies.kt#L39
+// https://github.com/Kotlin/dokka/pull/3145
+private fun KotlinCompilation.getPlatfromDependenciesFromKonanDistribution(project: Project): FileCollection {
+    return if (this is AbstractKotlinNativeCompilation)
+        project.files(project.getStdLibFromKonanDistribution()) + project.files(project.getPlatformLibsFromKonanDistribution(this.konanTarget))
+    else
+        project.files()
+}
+
+private fun Project.getStdLibFromKonanDistribution(): File {
+    val root = file(this.konanHome())
+    val konanCommonLibraries = root.resolve(KONAN_DISTRIBUTION_KLIB_DIR).resolve(KONAN_DISTRIBUTION_COMMON_LIBS_DIR)
+    val stdlib = konanCommonLibraries.resolve(KONAN_STDLIB_NAME)
+    return stdlib
+}
+
+// copy-pasted from https://github.com/jetbrains/kotlin/blob/c9aeadd31f763646237faffab38a57923c520fa1/libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/compilerRunner/nativeToolRunners.kt#L29
+private fun Project.konanHome(): String {
+    return nativeHome()?.let { file(it).absolutePath }
+        ?: NativeCompilerDownloader(project).compilerDirectory.absolutePath
+}
+
+private fun Project.nativeHome(): String? =
+    this.findProperty("org.jetbrains.kotlin.native.home") as? String
+
+//copy-pasted from https://github.com/jetbrains/kotlin/blob/05a6d89151e6a7230faf733e51161b5f07ae10a7/native/commonizer/src/org/jetbrains/kotlin/commonizer/repository/KonanDistributionRepository.kt#L20
+// https://github.com/jetbrains/kotlin/blob/b6a215d681695a2fe0cc798308966c5675de447f/libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/native/internal/KotlinNativePlatformDependencies.kt#L83
+private fun Project.getPlatformLibsFromKonanDistribution(target: KonanTarget): Array<out File>? {
+    val root = file(this.konanHome())
+    val platformLibsDir = root.resolve(KONAN_DISTRIBUTION_KLIB_DIR).resolve(KONAN_DISTRIBUTION_PLATFORM_LIBS_DIR)
+    val libs = platformLibsDir.resolve(target.name).takeIf { it.isDirectory }?.listFiles()
+    return libs
 }
 
 private fun Project.classpathProperty(name: String, default: Boolean): Boolean =
