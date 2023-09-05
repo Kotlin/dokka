55bc769635dcd180342944881177c036ab90b8f8
Adam <897017+aSemy@users.noreply.github.com>
2023-09-06 00:29:09 +0200

    add version-catalog def in integration-tests

--- dokka-integration-tests/settings.gradle.kts
+++ dokka-integration-tests/settings.gradle.kts
@@ -19,6 +19,12 @@ dependencyResolutionManagement {
     repositories {
         mavenCentral()
     }
+
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
 }
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
