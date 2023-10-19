d075fb483057de42b1bd240a3d1fc9503221d09f
IgnatBeresnev <ignat.beresnev@jetbrains.com>
2023-10-19 13:16:44 +0200

    Add composite build parent tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
     id("org.jetbrains.conventions.base")
     id("org.jetbrains.conventions.dokka")
 
+// TODO [structure-refactoring] enable
 //    alias(libs.plugins.kotlinx.binaryCompatibilityValidator)
     alias(libs.plugins.nexusPublish)
 }
@@ -35,6 +36,27 @@ nexusPublishing {
 val dokkaPublish by tasks.registering {
     if (publicationChannels.any { it.isMavenRepository() }) {
         finalizedBy(tasks.named("closeAndReleaseSonatypeStagingRepository"))
+    }
+}
+
+addDependencyOnSameTaskOfIncludedBuilds("assemble")
+addDependencyOnSameTaskOfIncludedBuilds("build")
+addDependencyOnSameTaskOfIncludedBuilds("clean")
+addDependencyOnSameTaskOfIncludedBuilds("check")
+
+registerParentTaskOfIncludedBuilds("test", groupName = "verification")
+
+fun addDependencyOnSameTaskOfIncludedBuilds(existingTaskName: String) {
+    tasks.named(existingTaskName) {
+        dependsOn(gradle.includedBuilds.map { it.task(":$existingTaskName") })
+    }
+}
+
+fun registerParentTaskOfIncludedBuilds(taskName: String, groupName: String) {
+    tasks.register(taskName) {
+        group = groupName
+        description = "Runs $taskName tasks of all included builds"
+        dependsOn(gradle.includedBuilds.map { it.task(":$taskName") })
     }
 }
 
--- dokka-subprojects/build.gradle.kts
+++ dokka-subprojects/build.gradle.kts
@@ -10,3 +10,33 @@ val dokka_version: String by project
 
 group = "org.jetbrains.dokka"
 version = dokka_version
+
+addDependencyToSubprojectTasks("assemble")
+addDependencyToSubprojectTasks("build")
+addDependencyToSubprojectTasks("clean")
+addDependencyToSubprojectTasks("check")
+
+registerParentTask("test", groupName = "verification")
+
+fun addDependencyToSubprojectTasks(existingTaskName: String) {
+    tasks.named(existingTaskName) {
+        val subprojectTasks = subprojects
+            .filter { it.getTasksByName(existingTaskName, false).isNotEmpty() }
+            .map { ":${it.name}:$existingTaskName" }
+
+        dependsOn(subprojectTasks)
+    }
+}
+
+fun registerParentTask(taskName: String, groupName: String) {
+    tasks.register(taskName) {
+        group = groupName
+        description = "Runs $taskName tasks of all subprojects"
+
+        val subprojectTasks = subprojects
+            .filter { it.getTasksByName(taskName, false).isNotEmpty() }
+            .map { ":${it.name}:$taskName" }
+
+        dependsOn(subprojectTasks)
+    }
+}
