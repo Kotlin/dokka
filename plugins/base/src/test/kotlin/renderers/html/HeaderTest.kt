a9c8eb33d17ac2fb89356c4f43df7492bac12770
Oleg Yukhnevich <whyoleg@gmail.com>
2023-10-24 12:48:41 +0300

    refactor tests to use BaseAbstractTest

--- plugins/base/src/test/kotlin/renderers/html/HeaderTest.kt
+++ plugins/base/src/test/kotlin/renderers/html/HeaderTest.kt
@@ -4,20 +4,33 @@
 
 package renderers.html
 
+import org.jetbrains.dokka.DokkaConfigurationImpl
 import org.jetbrains.dokka.SourceLinkDefinitionImpl
-import org.jetbrains.dokka.base.renderers.html.HtmlRenderer
+import org.jetbrains.dokka.base.testApi.testRunner.BaseAbstractTest
+import org.jetbrains.dokka.pages.RootPageNode
+import org.jetbrains.dokka.plugability.DokkaContext
 import org.jsoup.Jsoup
-import org.jsoup.nodes.Element
-import renderers.testPage
+import utils.TestOutputWriter
+import utils.TestOutputWriterPlugin
 import java.net.URL
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 import kotlin.test.assertNull
 
-class HeaderTest : HtmlRenderingOnlyTestBase() {
-    override val renderedContent: Element
-        get() = files.contents.getValue("test-page.html").let { Jsoup.parse(it) }.select(".navigation").single()
+class HeaderTest : BaseAbstractTest() {
+    private val configuration = dokkaConfiguration {
+        sourceSets {
+            sourceSet {
+                name = "jvm"
+                sourceRoots = listOf("src/jvm")
+            }
+            sourceSet {
+                name = "js"
+                sourceRoots = listOf("src/js")
+            }
+        }
+    }
 
     @Test
     fun `should include link to github if sourceLinks are pointed to github`() {
@@ -26,20 +39,20 @@ class HeaderTest : HtmlRenderingOnlyTestBase() {
             remoteUrl = URL("https://github.com/Kotlin/dokka/tree/main"),
             remoteLineSuffix = null
         )
-        val context = context(
+        testRendering(
             configuration.copy(
                 sourceSets = configuration.sourceSets.map {
                     it.copy(sourceLinks = setOf(sourceLink))
                 }
             )
-        )
+        ) { _, _, writer ->
+            val renderedContent = navigationElement(writer)
 
-        HtmlRenderer(context).render(testPage { })
-
-        val sourceLinkElement =
-            assertNotNull(renderedContent.getElementById("source-link"), "Source link element not found")
-        val aElement = assertNotNull(sourceLinkElement.selectFirst("a"))
-        assertEquals("https://github.com/Kotlin/dokka/", aElement.attr("href"))
+            val sourceLinkElement =
+                assertNotNull(renderedContent.getElementById("source-link"), "Source link element not found")
+            val aElement = assertNotNull(sourceLinkElement.selectFirst("a"))
+            assertEquals("https://github.com/Kotlin/dokka/", aElement.attr("href"))
+        }
     }
 
     @Test
@@ -49,18 +62,17 @@ class HeaderTest : HtmlRenderingOnlyTestBase() {
             remoteUrl = URL("https://github.com/Kotlin/dokka/tree/main"),
             remoteLineSuffix = null
         )
-        val context = context(
+        testRendering(
             configuration.copy(
                 sourceSets = listOf(
-                    js.copy(sourceLinks = setOf(sourceLink)),
-                    jvm.copy(sourceLinks = setOf(sourceLink.copy(remoteUrl = URL("https://github.com/Kotlin/dokkatoo/tree/main"))))
+                    configuration.sourceSets[0].copy(sourceLinks = setOf(sourceLink)),
+                    configuration.sourceSets[1].copy(sourceLinks = setOf(sourceLink.copy(remoteUrl = URL("https://github.com/Kotlin/dokkatoo/tree/main"))))
                 )
             )
-        )
-
-        HtmlRenderer(context).render(testPage { })
-
-        assertNull(renderedContent.getElementById("source-link"), "Source link element found")
+        ) { _, _, writer ->
+            val renderedContent = navigationElement(writer)
+            assertNull(renderedContent.getElementById("source-link"), "Source link element found")
+        }
     }
 
     @Test
@@ -70,25 +82,54 @@ class HeaderTest : HtmlRenderingOnlyTestBase() {
             remoteUrl = URL("https://gitlab.com/Kotlin/dokka/tree/main"),
             remoteLineSuffix = null
         )
-        val context = context(
+        testRendering(
             configuration.copy(
                 sourceSets = configuration.sourceSets.map {
                     it.copy(sourceLinks = setOf(sourceLink))
                 }
             )
-        )
-
-        HtmlRenderer(context).render(testPage { })
-
-        assertNull(renderedContent.getElementById("source-link"), "Source link element found")
+        ) { _, _, writer ->
+            val renderedContent = navigationElement(writer)
+            assertNull(renderedContent.getElementById("source-link"), "Source link element found")
+        }
     }
 
     @Test
     fun `should not include link to github if there are no sourceLinks`() {
-        val context = context(configuration)
-
-        HtmlRenderer(context).render(testPage { })
-
-        assertNull(renderedContent.getElementById("source-link"), "Source link element found")
+        testRendering(configuration) { _, _, writer ->
+            val renderedContent = navigationElement(writer)
+            assertNull(renderedContent.getElementById("source-link"), "Source link element found")
+        }
     }
+
+
+    private fun testRendering(
+        configuration: DokkaConfigurationImpl = this.configuration,
+        block: (RootPageNode, DokkaContext, writer: TestOutputWriter) -> Unit
+    ) {
+        val writerPlugin = TestOutputWriterPlugin()
+        testInline(
+            """
+            |/src/jvm/Test.kt
+            |fun test() {}
+            |/src/js/Test.kt
+            |fun test() {}
+            """,
+            configuration,
+            pluginOverrides = listOf(writerPlugin)
+        ) {
+            renderingStage = { node, context ->
+                block(node, context, writerPlugin.writer)
+            }
+        }
+    }
+
+    private fun navigationElement(writer: TestOutputWriter) =
+        writer
+            .contents
+            .getValue("index.html")
+            .let(Jsoup::parse)
+            .select(".navigation")
+            .single()
+
 }
