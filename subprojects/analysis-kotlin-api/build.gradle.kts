edcd1fb24d01e11b5a8185328255f2005aadf037
Ignat Beresnev <ignat.beresnev@jetbrains.com>
2023-10-27 13:11:41 +0200

    Implement analysis test API (#3184)

--- subprojects/analysis-kotlin-api/build.gradle.kts
+++ subprojects/analysis-kotlin-api/build.gradle.kts
@@ -7,10 +7,30 @@ import org.jetbrains.registerDokkaArtifactPublication
 plugins {
     id("org.jetbrains.conventions.kotlin-jvm")
     id("org.jetbrains.conventions.maven-publish")
+    `java-test-fixtures`
 }
 
 dependencies {
     compileOnly(projects.core)
+
+    testFixturesApi(projects.core)
+
+    testImplementation(kotlin("test"))
+    testImplementation(projects.subprojects.analysisKotlinDescriptors)
+}
+
+disableTestFixturesPublishing()
+
+/**
+ * Test fixtures are automatically published by default, which at this moment in time is unwanted
+ * as the test api is unstable and is internal to the Dokka project, so it shouldn't be used outside of it.
+ *
+ * @see https://docs.gradle.org/current/userguide/java_testing.html#ex-disable-publishing-of-test-fixtures-variants
+ */
+fun disableTestFixturesPublishing() {
+    val javaComponent = components["java"] as AdhocComponentWithVariants
+    javaComponent.withVariantsFromConfiguration(configurations["testFixturesApiElements"]) { skip() }
+    javaComponent.withVariantsFromConfiguration(configurations["testFixturesRuntimeElements"]) { skip() }
 }
 
 registerDokkaArtifactPublication("analysisKotlinApi") {
