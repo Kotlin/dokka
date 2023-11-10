5bd7c4a22abf07deff36ee1bc14c2dbee2cec944
IgnatBeresnev <ignat.beresnev@jetbrains.com>
2023-11-10 13:33:17 +0100

    Fix developer docs being redirected to the SNAPSHOT version
    Fixes a mistake introduced in #3174

--- docs-developer/build.gradle.kts
+++ docs-developer/build.gradle.kts
@@ -6,10 +6,10 @@ plugins {
     id("ru.vyarus.mkdocs") version "2.4.0"
 }
 
-if (!project.version.toString().endsWith("-SNAPSHOT")) {
+if (project.version.toString().endsWith("-SNAPSHOT")) {
     // Do not generate the root index.html file with the redirect
-    // to a non-release version, otherwise GitHub pages based documentation
-    // will always lead to the non-stable documentation.
+    // to a snapshot version, otherwise GitHub Pages-based documentation
+    // will always lead to the non-yet-released documentation.
     // For more details, see https://github.com/Kotlin/dokka/issues/2869.
     // For configuration details, see https://xvik.github.io/gradle-mkdocs-plugin/3.0.0/examples/#simple-multi-version.
     mkdocs.publish.rootRedirect = false
