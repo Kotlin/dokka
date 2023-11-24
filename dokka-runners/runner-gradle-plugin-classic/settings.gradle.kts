20f507ed82876256a48a1bf964a4b479ee3d0a95
Oleg Yukhnevich <whyoleg@gmail.com>
2023-11-24 20:54:08 +0200

    Fix enum test, add toolchains and running on JDK 21

--- dokka-runners/runner-gradle-plugin-classic/settings.gradle.kts
+++ dokka-runners/runner-gradle-plugin-classic/settings.gradle.kts
@@ -15,6 +15,10 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+}
+
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
