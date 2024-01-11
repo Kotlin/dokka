63e0d6ca9b81ca75da77a847ccc39c19e02c3de4
Vadim Mishenev <vad-mishenev@yandex.ru>
2024-01-11 17:57:21 +0200

    Fix cross module resolution local links (#3425)
    Fixes #2272. There is a situation where 2 (or more) gradle modules have the same package and dokka doesn’t know where to link.
    E.g.
    The class com.example.classA is in moduleA
    and com.example.classB is in moduleB
    moduleC depends on moduleA and moduleB.
    In moduleC: there is a link [com.example.classB]. Having only package-list, dokka doesn’t know where to link, since the package com.example is in both modules.
    
    This is short-term and temporary solution of #3368 to cover a case of links between only local modules in a project.
    It is based on #2901 from @EddieRingle. It generates an external link depending on a check if the local link exists. The check works only when the number of possible resolved links are more than one.
    
    
    
    * all-modules-page: Validate cross-module links during resolution
    
    The existing behavior was just taking the first module that includes the
    package name of a given reference, so this resulted in broken links when
    a package is split across multiple modules.
    
    This change checks to ensure the resolved file exists before choosing it
    as the target for the resolved link.
    
    Signed-off-by: Eddie Ringle <eddie@ringle.io>
    
    * Check links only if resolved links has more than one link
    
    * Add integration test
    
    * Refactor
    
    * Add comment
    
    ---------
    
    Signed-off-by: Eddie Ringle <eddie@ringle.io>
    Co-authored-by: Eddie Ringle <eddie@ringle.io>

---
+++ dokka-integration-tests/gradle/projects/it-multimodule-inter-module-links/moduleA/moduleB/src/main/kotlin/org/jetbrains/dokka/it/ModuleB.kt
@@ -0,0 +1,6 @@
+package org.jetbrains.dokka.it
+
+@Suppress("unused")
+class ModuleB {
+    fun undocumentedPublicFunction() {}
+}
---
+++ dokka-integration-tests/gradle/projects/it-multimodule-inter-module-links/moduleA/moduleD/src/main/kotlin/org/jetbrains/dokka/moduleD/ModuleD.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2014-2023 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package org.jetbrains.dokka.moduleD
+
+import org.jetbrains.dokka.it.ModuleB
+import org.jetbrains.dokka.it.ModuleC
+
+/**
+ * ModuleB: [ModuleB]
+ * ModuleC: [ModuleC]
+ */
+@Suppress("unused")
+class ModuleD {
+    fun undocumentedPublicFunction() {}
+}
