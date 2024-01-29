cebc011fcd3ffe911198de02a5d9e31e43145b56
Oleg Yukhnevich <whyoleg@gmail.com>
2024-01-29 18:37:49 +0200

    Update dokka-integration-tests/cli/projects/it-cli/src/main/kotlin/RootPackageClass.kt
    Co-authored-by: Ignat Beresnev <ignat.beresnev@jetbrains.com>

--- dokka-integration-tests/cli/projects/it-cli/src/main/kotlin/RootPackageClass.kt
+++ dokka-integration-tests/cli/projects/it-cli/src/main/kotlin/RootPackageClass.kt
@@ -3,7 +3,7 @@
 /**
  * A class that lives inside the root package
  *
- * <me@mail.com>
+ * <me@mail.com> - checks markdown parsing of emails and non-trivial cases like #3329, should compile
  */
 class RootPackageClass {
     val description = "I do live in the root package!"
