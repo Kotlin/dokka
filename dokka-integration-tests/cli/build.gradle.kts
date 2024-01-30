b8ef2dd3e847c71aeb6db979baf32e71b497fb0f
Oleg Yukhnevich <whyoleg@gmail.com>
2024-01-30 20:45:27 +0200

    Fix fastutil shadowing (#3476)
    * support running CLI tests with K2
    * explicitly control dependencies in CLI integration tests
    
    ---------
    
    Co-authored-by: Ignat Beresnev <ignat.beresnev@jetbrains.com>
    (cherry picked from commit 3c989c173ceb8305d9aa0e63774d2479c913871a)

--- dokka-integration-tests/cli/build.gradle.kts
+++ dokka-integration-tests/cli/build.gradle.kts
@@ -15,39 +15,54 @@ dependencies {
     implementation(projects.utilities)
 }
 
-/* Create a fat base plugin jar for cli tests */
-val basePluginShadow: Configuration by configurations.creating {
+val cliPluginsClasspath: Configuration by configurations.creating {
+    description = "plugins/dependencies required to run CLI with base plugin"
     attributes {
-        attribute(Usage.USAGE_ATTRIBUTE, project.objects.named(Usage::class.java, "java-runtime"))
+        attribute(Usage.USAGE_ATTRIBUTE, project.objects.named(Usage.JAVA_RUNTIME))
     }
+
+    // we don't fetch transitive dependencies here to be able to control external dependencies explicitly
+    isTransitive = false
 }
 
-val cliConfiguration: Configuration by configurations.creating {
+val cliClasspath: Configuration by configurations.creating {
+    description = "dependency on CLI JAR"
     attributes {
-        attribute(Usage.USAGE_ATTRIBUTE, project.objects.named(Usage::class.java, Usage.JAVA_RUNTIME))
-        attribute(Bundling.BUNDLING_ATTRIBUTE, project.objects.named(Bundling::class.java, Bundling.SHADOWED))
+        attribute(Usage.USAGE_ATTRIBUTE, project.objects.named(Usage.JAVA_RUNTIME))
+        attribute(Bundling.BUNDLING_ATTRIBUTE, project.objects.named(Bundling.SHADOWED))
     }
     // we should have single artifact here
     isTransitive = false
 }
 
 dependencies {
-    cliConfiguration("org.jetbrains.dokka:runner-cli")
+    cliClasspath("org.jetbrains.dokka:runner-cli")
 
-    basePluginShadow("org.jetbrains.dokka:plugin-base")
+    cliPluginsClasspath("org.jetbrains.dokka:plugin-base")
+    // required dependencies of `plugin-base`
+    cliPluginsClasspath(libs.freemarker)
+    cliPluginsClasspath(libs.kotlinx.html)
 
-    // TODO [beresnev] analysis switcher
-    basePluginShadow("org.jetbrains.dokka:analysis-kotlin-descriptors") {
+    val tryK2 = project.providers
+        .gradleProperty("org.jetbrains.dokka.experimental.tryK2")
+        .map(String::toBoolean)
+        .orNull ?: false
+
+    val analysisDependency = when {
+        tryK2 -> "org.jetbrains.dokka:analysis-kotlin-symbols"
+        else -> "org.jetbrains.dokka:analysis-kotlin-descriptors"
+    }
+
+    cliPluginsClasspath(analysisDependency) {
         attributes {
-            attribute(Bundling.BUNDLING_ATTRIBUTE, project.objects.named(Bundling::class.java, Bundling.SHADOWED))
+            attribute(Bundling.BUNDLING_ATTRIBUTE, project.objects.named(Bundling.SHADOWED))
         }
     }
 }
 
-val basePluginShadowJar by tasks.register("basePluginShadowJar", ShadowJar::class) {
-    configurations = listOf(basePluginShadow)
-    archiveFileName.set("fat-base-plugin-${project.version}.jar")
-    archiveClassifier.set("")
+val cliPluginsShadowJar by tasks.registering(ShadowJar::class) {
+    archiveFileName.set("cli-plugins-${project.version}.jar")
+    configurations = listOf(cliPluginsClasspath)
 
     // service files are merged to make sure all Dokka plugins
     // from the dependencies are loaded, and not just a single one.
@@ -55,10 +70,10 @@ val basePluginShadowJar by tasks.register("basePluginShadowJar", ShadowJar::clas
 }
 
 tasks.integrationTest {
-    dependsOn(cliConfiguration)
-    dependsOn(basePluginShadowJar)
+    dependsOn(cliClasspath)
+    dependsOn(cliPluginsShadowJar)
 
     inputs.dir(file("projects"))
-    environment("CLI_JAR_PATH", cliConfiguration.singleFile)
-    environment("BASE_PLUGIN_JAR_PATH", basePluginShadowJar.archiveFile.get())
+    environment("CLI_JAR_PATH", cliClasspath.singleFile)
+    environment("BASE_PLUGIN_JAR_PATH", cliPluginsShadowJar.get().archiveFile.get())
 }
---
+++ dokka-integration-tests/cli/projects/it-cli/src/main/kotlin/RootPackageClass.kt
@@ -0,0 +1,10 @@
+@file:Suppress("unused")
+
+/**
+ * A class that lives inside the root package
+ *
+ * <me@mail.com> - checks markdown parsing of emails and non-trivial cases like #3329, should compile
+ */
+class RootPackageClass {
+    val description = "I do live in the root package!"
+}
