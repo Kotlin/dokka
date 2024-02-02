b3441ac15c979543db27b00e492108795f70d0ac
Adam Semenenko <152864218+adam-enko@users.noreply.github.com>
2024-02-02 10:44:44 +0100

    Replace git submodules for test projects with GitCheckoutTask
    This improves the experience when running Dokka tests. External projects used for testing can be managed using Gradle. Because the commitId is defined in code it's easier to see what version the external projects are used.

---
+++ build-logic/src/main/kotlin/dokkabuild/tasks/GitCheckoutTask.kt
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2014-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+package dokkabuild.tasks
+
+import org.eclipse.jgit.api.Git
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.file.FileSystemOperations
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.CacheableTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.TaskAction
+import javax.inject.Inject
+
+/**
+ * Clones a remote Git repository into [GitCheckoutTask.destination].
+ */
+@CacheableTask
+abstract class GitCheckoutTask @Inject constructor(
+    private val fs: FileSystemOperations
+) : DefaultTask() {
+
+    @get:OutputDirectory
+    abstract val destination: DirectoryProperty
+
+    /** Public HTTP URI of the Git repo. */
+    @get:Input
+    abstract val uri: Property<String>
+
+    /** Specific git hash to checkout. */
+    @get:Input
+    abstract val commitId: Property<String>
+
+    init {
+        group = "git checkout"
+    }
+
+    @TaskAction
+    fun action() {
+        val uri = uri.get()
+        val commitId = commitId.get()
+
+        fs.delete { delete(temporaryDir) }
+
+        Git.cloneRepository()
+            .setURI(uri)
+            .setDirectory(temporaryDir)
+            .call().use { git ->
+                git.pull().call()
+
+                git.checkout()
+                    .setName(commitId)
+                    .call()
+
+
+                fs.sync {
+                    from(temporaryDir)
+                    into(destination)
+                    // exclude the .git dir to prevent the root Dokka repo getting confused with the nested subproject
+                    exclude(".git/")
+                }
+
+                logger.lifecycle("initialized git repo $uri in ${destination.asFile.orNull}")
+            }
+    }
+}
--- dokka-integration-tests/maven/build.gradle.kts
+++ dokka-integration-tests/maven/build.gradle.kts
@@ -1,6 +1,7 @@
 /*
  * Copyright 2014-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
+import dokkabuild.tasks.GitCheckoutTask
 
 plugins {
     id("dokkabuild.test-integration")
@@ -14,6 +15,8 @@ dependencies {
     implementation(libs.junit.jupiterApi)
 }
 
+val templateProjectsDir = layout.projectDirectory.dir("projects")
+
 val dokkaSubprojects = gradle.includedBuild("dokka")
 val mavenPlugin = gradle.includedBuild("runner-maven-plugin")
 
@@ -21,6 +24,7 @@ tasks.integrationTest {
     dependsOn(
         dokkaSubprojects.task(":publishToMavenLocal"),
         mavenPlugin.task(":publishToMavenLocal"),
+        checkoutBioJava,
     )
 
     dependsOn(tasks.installMavenBinary)
@@ -32,4 +36,10 @@ tasks.integrationTest {
         environment("DOKKA_VERSION", project.version)
         environment("MVN_BINARY_PATH", mvn.get().asFile.invariantSeparatorsPath)
     }
+}
+
+val checkoutBioJava by tasks.registering(GitCheckoutTask::class) {
+    uri = "https://github.com/biojava/biojava.git"
+    commitId = "059fbf1403d0704801df1427b0ec925102a645cd"
+    destination = templateProjectsDir.dir("biojava/biojava")
 }
