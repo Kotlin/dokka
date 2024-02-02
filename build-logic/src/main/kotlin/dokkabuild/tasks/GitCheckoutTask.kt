9ec11c2699a4a6080309d79226033a35bada38b6
Adam Semenenko <152864218+adam-enko@users.noreply.github.com>
2024-02-02 18:05:43 +0100

    improve checkout/reset logic for checking out repos

--- build-logic/src/main/kotlin/dokkabuild/tasks/GitCheckoutTask.kt
+++ build-logic/src/main/kotlin/dokkabuild/tasks/GitCheckoutTask.kt
@@ -4,6 +4,12 @@
 package dokkabuild.tasks
 
 import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.api.ResetCommand.ResetType.HARD
+import org.eclipse.jgit.lib.NullProgressMonitor
+import org.eclipse.jgit.lib.ProgressMonitor
+import org.eclipse.jgit.lib.RepositoryCache
+import org.eclipse.jgit.lib.TextProgressMonitor
+import org.eclipse.jgit.util.FS
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.file.FileSystemOperations
@@ -12,6 +18,7 @@ import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
+import java.io.File
 import javax.inject.Inject
 
 /**
@@ -33,30 +40,26 @@ abstract class GitCheckoutTask @Inject constructor(
     @get:Input
     abstract val commitId: Property<String>
 
+    private val localRepoDir: File
+        get() = temporaryDir.resolve("repo")
+
+    private val gitOperationsPrinter: ProgressMonitor =
+        if (logger.isInfoEnabled) {
+            TextProgressMonitor()
+        } else {
+            NullProgressMonitor.INSTANCE
+        }
+
     init {
         group = "git checkout"
     }
 
     @TaskAction
     fun action() {
-        val uri = uri.get()
-        val commitId = commitId.get()
-
-        fs.delete { delete(temporaryDir) }
-
-        Git.cloneRepository()
-            .setURI(uri)
-            .setDirectory(temporaryDir)
-            .call().use { git ->
-                git.pull().call()
-
-                git.checkout()
-                    .setName(commitId)
-                    .call()
-            }
+        initializeRepo()
 
         fs.sync {
-            from(temporaryDir)
+            from(localRepoDir)
             into(destination)
             // exclude the .git dir:
             // - prevent the root git repo getting confused with a nested git repo
@@ -65,5 +68,49 @@ abstract class GitCheckoutTask @Inject constructor(
         }
 
         logger.lifecycle("initialized git repo $uri in ${destination.asFile.orNull}")
+    }
+
+    /**
+     * Initialized [uri] in [localRepoDir], forcibly resetting it and deleting any untracked files or changes.
+     */
+    private fun initializeRepo() {
+        val uri = uri.get()
+        val commitId = commitId.get()
+
+        val gitRepoInitialized = RepositoryCache.FileKey.isGitRepository(localRepoDir, FS.DETECTED)
+
+        val repo = if (gitRepoInitialized) {
+            Git.open(localRepoDir)
+        } else {
+            fs.delete { delete(localRepoDir) }
+
+            Git.cloneRepository()
+                .setProgressMonitor(gitOperationsPrinter)
+                .setNoCheckout(true)
+                .setURI(uri)
+                .setDirectory(localRepoDir)
+                .call()
+        }
+
+        repo.use { git ->
+            git.checkout()
+                .setProgressMonitor(gitOperationsPrinter)
+                .setForced(true)
+                .setName(commitId)
+                .call()
+
+            // git reset --hard
+            git.reset()
+                .setProgressMonitor(gitOperationsPrinter)
+                .setMode(HARD)
+                .call()
+
+            // git clean -fdx
+            git.clean()
+                .setForce(true)
+                .setCleanDirectories(true)
+                .setIgnore(false)
+                .call()
+        }
     }
 }
