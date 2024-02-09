7671d5b8f0c696ab7e15a7ca4af9f857cf3f4903
Adam Semenenko <152864218+adam-enko@users.noreply.github.com>
2024-02-09 18:23:00 +0100

    update developer docs build config
    - specify requirements.txt
    - update site: add dark/light mode toggle
    - use `com.pswidersk.python-plugin` (it's more compatible with Configuration Cache)
    - define tasks for installing & building mkdocs
    - log link to site using IntelliJ built-in server

--- docs-developer/build.gradle.kts
+++ docs-developer/build.gradle.kts
@@ -1,16 +1,63 @@
 /*
  * Copyright 2014-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
+import com.pswidersk.gradle.python.VenvTask
+import org.gradle.api.tasks.PathSensitivity.RELATIVE
+import org.jetbrains.kotlin.util.parseSpaceSeparatedArgs
 
 plugins {
-    id("ru.vyarus.mkdocs") version "2.4.0"
+    id("com.pswidersk.python-plugin") version "2.4.0"
 }
 
-if (project.version.toString().endsWith("-SNAPSHOT")) {
-    // Do not generate the root index.html file with the redirect
-    // to a snapshot version, otherwise GitHub Pages-based documentation
-    // will always lead to the non-yet-released documentation.
-    // For more details, see https://github.com/Kotlin/dokka/issues/2869.
-    // For configuration details, see https://xvik.github.io/gradle-mkdocs-plugin/3.0.0/examples/#simple-multi-version.
-    mkdocs.publish.rootRedirect = false
+val mkdocsSetup by tasks.registering(VenvTask::class) {
+    venvExec = "pip"
+    args = parseSpaceSeparatedArgs("install -r requirements.txt")
+}
+
+val pipPrintRequirements by tasks.registering(VenvTask::class) {
+    description = "Log the current requirements"
+    venvExec = "pip"
+    args = parseSpaceSeparatedArgs("freeze")
+}
+
+val mkdocsBuild by tasks.registering(VenvTask::class) {
+    dependsOn(mkdocsSetup)
+    venvExec = "mkdocs"
+    args = parseSpaceSeparatedArgs("build")
+
+    inputs.dir("docs")
+        .withPropertyName("docs")
+        .withPathSensitivity(RELATIVE)
+        .normalizeLineEndings()
+
+    // output directory `site_dir` is specified in mkdocs.yml
+    val mkdocsSiteDir = layout.buildDirectory.dir("mkdocs")
+    outputs.dir(mkdocsSiteDir)
+        .withPropertyName("site_dir")
+
+    outputs.cacheIf("always cache - task has defined output directory") { true }
+
+    // log a link to the generated docs using the build-in IJ server
+    val ideaActive = System.getProperty("idea.active").toBoolean()
+    val dokkaProjectDir = project.rootDir.parentFile
+
+    doLast {
+        if (ideaActive) {
+            // The built-in IntelliJ server requires a path to index.html
+            // that includes the root project name.
+            val indexPath = mkdocsSiteDir.get().asFile.relativeTo(dokkaProjectDir)
+
+            logger.quiet(
+                """
+                |
+                |***************************************************************************************************
+                |    
+                |    Dokka Developer Docs: http://localhost:63342/$indexPath/index.html
+                |
+                |***************************************************************************************************
+                |
+                """.trimMargin()
+            )
+        }
+    }
 }
