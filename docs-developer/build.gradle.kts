3768e085db28494f8ecc05641d46504ff660b973
Adam Semenenko <152864218+adam-enko@users.noreply.github.com>
2024-03-08 17:42:15 +0100

    update mkdocs site structure:
     - generate the site into `$dokkaVersion/` subdir
     - on non-SNAPSHOT versions generate a redirecting index.html

--- docs-developer/build.gradle.kts
+++ docs-developer/build.gradle.kts
@@ -2,6 +2,7 @@
  * Copyright 2014-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
 import com.pswidersk.gradle.python.VenvTask
+import org.apache.tools.ant.filters.ReplaceTokens
 import org.gradle.api.tasks.PathSensitivity.RELATIVE
 import org.jetbrains.kotlin.util.parseSpaceSeparatedArgs
 
@@ -9,6 +10,35 @@ plugins {
     id("com.pswidersk.python-plugin") version "2.4.0"
 }
 
+val dokkaVersion = provider { project.version.toString() }
+val isDokkaSnapshotVersion = dokkaVersion.map { it.endsWith("-SNAPSHOT") }
+
+/** Directory containing generated docs. */
+val currentMkDocsDir: Provider<Directory> = layout.buildDirectory.dir("mkdocs-current")
+
+/**
+ * Directory containing a fully built MkDocs site, ready for upload to GitHub Pages.
+ *
+ * The [currentMkDocsDir] must be placed in a [dokkaVersion] subdirectory.
+ *
+ * ```.
+ * └── build/
+ *     └── mkdocs/
+ *         └── 1.2.3-SNAPSHOT/
+ *             └── <content>
+ * ```
+ *
+ * If [dokkaVersion] is a release version (non-SNAPSHOT) then the dir must contain an index.html
+ * that redirects to the current version.
+ *
+ * ```
+ * └── build/
+ *     ├── mkdocs/
+ *     │   └── 1.2.3/
+ *     │       └── <content>
+ *     └── index.html (redirects to `1.2.3/index.html`)
+ * ```
+ */
 val mkDocsSiteDir: Provider<Directory> = layout.buildDirectory.dir("mkdocs")
 
 val mkDocsSetup: TaskProvider<VenvTask> by tasks.registering(VenvTask::class) {
@@ -50,37 +80,89 @@ val mkDocsBuild: TaskProvider<VenvTask> by tasks.registering(VenvTask::class) {
         .withPathSensitivity(RELATIVE)
         .normalizeLineEndings()
 
-    // output directory `site_dir` is specified in mkdocs.yml
-    outputs.dir(mkDocsSiteDir)
-        .withPropertyName("site_dir")
+    // output directory is also specified in mkdocs.yml as `site_dir`
+    outputs.dir(currentMkDocsDir)
+        .withPropertyName("mkDocsOutputDir")
 
     outputs.cacheIf("always cache - task has defined output directory") { true }
+}
+
+val generateMkDocsSiteIndexHtml by tasks.registering(Sync::class) {
+    description = "Generate the root index.html"
+    group = project.name
+
+    val dokkaVersion = dokkaVersion
+    val isDokkaSnapshotVersion = isDokkaSnapshotVersion
+    inputs.property("dokkaVersion", dokkaVersion)
+
+    val indexHtml = layout.projectDirectory.file("index.html")
+    inputs.file(indexHtml)
+        .withPropertyName("indexHtml")
+        .withPathSensitivity(RELATIVE)
+        .normalizeLineEndings()
+
+    // only generate a root index.html on non-snapshot versions
+    if (!isDokkaSnapshotVersion.get()) {
+        from(indexHtml)
+    }
+
+    filter<ReplaceTokens>("tokens" to mapOf("dokkaVersion" to dokkaVersion.get()))
+
+    into(temporaryDir)
+}
+
+
+val buildMkDocsSite by tasks.registering(Sync::class) {
+    description = "Generate a complete MkDocs site, with versioned subdirectories"
+    group = project.name
+
+    val dokkaVersion = dokkaVersion
+    inputs.property("dokkaVersion", dokkaVersion)
+
+    from(generateMkDocsSiteIndexHtml)
+    from(mkDocsBuild) {
+        eachFile {
+            relativePath = relativePath.prepend(dokkaVersion.get())
+        }
+    }
+
+    includeEmptyDirs = false
+
+    into(mkDocsSiteDir)
 }
 
 val logMkDocsLink: TaskProvider<Task> by tasks.registering {
     description = "Prints a link to the generated docs"
     group = project.name
 
-    dependsOn(mkDocsBuild)
+    dependsOn(buildMkDocsSite)
     doNotTrackState("this task performs no work and should always log the link")
 
-    // redefine mkdocsSiteDir for config-cache compliance
-    val mkDocsSiteDir = mkDocsSiteDir
+    // redefine currentMkDocsDir for config-cache compliance
+    val mkDocsDir = buildMkDocsSite.map { it.destinationDir }
 
     val ideaActive = System.getProperty("idea.active").toBoolean()
 
     val dokkaProjectDir = project.rootDir.parentFile
 
+    val dokkaVersion = dokkaVersion
+    val isDokkaSnapshotVersion = isDokkaSnapshotVersion
+
     doLast {
-        val indexHtml = mkDocsSiteDir.get().asFile.resolve("index.html")
-        val link =
+        val indexHtml = if (isDokkaSnapshotVersion.get()) {
+            mkDocsDir.get().resolve("$dokkaVersion/index.html")
+        } else {
+            mkDocsDir.get().resolve("index.html")
+        }
+
+        val link: String =
             if (ideaActive) {
                 // The built-in IntelliJ server requires a specific path to index.html:
                 // a _relative_ path starting with the project's directory (dokka)
                 val relativeOutputPath = indexHtml.relativeTo(dokkaProjectDir).invariantSeparatorsPath
                 "http://localhost:63342/$relativeOutputPath"
             } else {
-                indexHtml.invariantSeparatorsPath
+                indexHtml.toURI().toString()
             }
 
         logger.quiet(
