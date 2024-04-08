c737643517e07c249d980181258b0c7268d36122
IgnatBeresnev <ignat.beresnev@jetbrains.com>
2024-04-08 14:12:27 +0200

    Resolve a deprecation warning

--- dokka-runners/runner-gradle-plugin-classic/src/main/kotlin/org/jetbrains/dokka/gradle/kotlin/isMainSourceSet.kt
+++ dokka-runners/runner-gradle-plugin-classic/src/main/kotlin/org/jetbrains/dokka/gradle/kotlin/isMainSourceSet.kt
@@ -4,8 +4,6 @@
 
 package org.jetbrains.dokka.gradle.kotlin
 
-import com.android.build.gradle.api.ApplicationVariant
-import com.android.build.gradle.api.LibraryVariant
 import org.gradle.api.Project
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinJvmAndroidCompilation
@@ -22,7 +20,9 @@ private fun isMainCompilation(compilation: KotlinCompilation): Boolean {
     try {
         val androidVariant = compilation.run { this as? KotlinJvmAndroidCompilation }?.androidVariant
         if (androidVariant != null) {
-            return androidVariant is LibraryVariant || androidVariant is ApplicationVariant
+            @Suppress("DEPRECATION")
+            return androidVariant is com.android.build.gradle.api.LibraryVariant
+                    || androidVariant is com.android.build.gradle.api.ApplicationVariant
         }
     } catch (e: NoSuchMethodError) {
         // Kotlin Plugin version below 1.4
