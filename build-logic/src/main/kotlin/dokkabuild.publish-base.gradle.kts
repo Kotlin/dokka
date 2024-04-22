a03c01579f6607c44c37afe7fe243a52fe62e77d
Ignat Beresnev <ignat.beresnev@jetbrains.com>
2024-04-22 13:26:47 +0200

    Allow publishing to a pre-defined staging repository (#3562)

--- build-logic/src/main/kotlin/dokkabuild.publish-base.gradle.kts
+++ build-logic/src/main/kotlin/dokkabuild.publish-base.gradle.kts
@@ -1,3 +1,5 @@
+import java.net.URI
+
 /*
  * Copyright 2014-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
@@ -13,7 +15,7 @@ publishing {
     repositories {
         maven {
             name = "mavenCentral"
-            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            url = mavenCentralRepositoryUri()
             credentials {
                 username = System.getenv("DOKKA_SONATYPE_USER")
                 password = System.getenv("DOKKA_SONATYPE_PASSWORD")
@@ -65,6 +67,22 @@ publishing {
                 url.convention("https://github.com/Kotlin/dokka")
             }
         }
+    }
+}
+
+/**
+ * Due to Gradle running publishing tasks in parallel, multiple staging repositories
+ * can be created within a couple of seconds with all artifact files scattered throughout them.
+ *
+ * While Gradle's parallelism can be disabled, the simplest and most reliable option is
+ * to just publish to a pre-defined staging repository.
+ */
+fun mavenCentralRepositoryUri(): URI {
+    val repositoryId: String? = System.getenv("DOKKA_MVN_CENTRAL_REPOSITORY_ID")
+    return if (repositoryId.isNullOrBlank()) {
+        URI("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+    } else {
+        URI("https://oss.sonatype.org/service/local/staging/deployByRepositoryId/$repositoryId")
     }
 }
 
