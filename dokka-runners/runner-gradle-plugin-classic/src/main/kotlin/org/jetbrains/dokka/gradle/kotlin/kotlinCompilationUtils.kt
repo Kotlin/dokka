95369b7ab193457776a7e7b17103576958e0ce2d
Oleg Yukhnevich <whyoleg@gmail.com>
2024-05-13 16:18:15 +0300

    Update Kotlin to 2.0.0-RC3

--- dokka-runners/runner-gradle-plugin-classic/src/main/kotlin/org/jetbrains/dokka/gradle/kotlin/kotlinCompilationUtils.kt
+++ dokka-runners/runner-gradle-plugin-classic/src/main/kotlin/org/jetbrains/dokka/gradle/kotlin/kotlinCompilationUtils.kt
@@ -6,14 +6,12 @@ package org.jetbrains.dokka.gradle.kotlin
 
 import org.gradle.api.Project
 import org.jetbrains.dokka.gradle.kotlin
-import org.jetbrains.kotlin.gradle.dsl.KotlinCommonOptions
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinSingleTargetExtension
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinCommonCompilation
 
-internal typealias KotlinCompilation =
-        org.jetbrains.kotlin.gradle.plugin.KotlinCompilation<KotlinCommonOptions>
+internal typealias KotlinCompilation = org.jetbrains.kotlin.gradle.plugin.KotlinCompilation<*>
 
 internal fun Project.compilationsOf(sourceSet: KotlinSourceSet): List<KotlinCompilation> {
     //KT-45412 Make sure .kotlinSourceSets and .allKotlinSourceSets include the default source set
