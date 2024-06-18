a5ad6323bda888ddc043a33c1b2ad286ad9b4ded
berezinant <berezinant@gmail.com>
2024-06-18 15:32:14 +0200

    feat: reworked navbar component

--- dokka-subprojects/plugin-base-frontend/src/main/ui-kit/navbar/index.ts
+++ dokka-subprojects/plugin-base-frontend/src/main/ui-kit/navbar/index.ts
@@ -1,7 +1,5 @@
 /*
  * Copyright 2014-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
-import * as navbar from './navbar/index';
-import * as tabs from './tabs/index';
 
-export { navbar, tabs };
+import './styles.scss';
