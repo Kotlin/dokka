4ca3e99660c87f3b02ad314d0ff4d2eb050a24e3
Adam Semenenko <152864218+adam-enko@users.noreply.github.com>
2024-06-19 12:29:55 +0200

    fix check task

--- dokka-integration-tests/build.gradle.kts
+++ dokka-integration-tests/build.gradle.kts
@@ -32,3 +32,9 @@ fun subprojectTasks(taskName: String): List<String> =
     subprojects
         .filter { it.getTasksByName(taskName, false).isNotEmpty() }
         .map { ":${it.name}:$taskName" }
+
+tasks.check {
+    subprojects.forEach { project ->
+        project.tasks.findByName("check")?.let { dependsOn(it) }
+    }
+}
