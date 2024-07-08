1fb9f8f58a7282cc484a63bca2cfe7d981e5636e
berezinant <berezinant@gmail.com>
2024-07-08 20:30:29 +0200

    feat: implemented new UI for platform-tags

--- dokka-subprojects/plugin-base-frontend/src/main/ui-kit/index.ts
+++ dokka-subprojects/plugin-base-frontend/src/main/ui-kit/index.ts
@@ -1,8 +1,10 @@
 /*
  * Copyright 2014-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
+import * as filterSection from './filter-section/index';
 import * as navbar from './navbar/index';
 import * as platformTag from './platform-tag/index';
+import * as platformTags from './platform-tags/index';
 import * as tabs from './tabs/index';
 
-export { navbar, tabs, platformTag };
+export { filterSection, navbar, tabs, platformTag, platformTags };
