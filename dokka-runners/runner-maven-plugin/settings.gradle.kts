4249644eeb24bdc8019bfcb0adf37f0c1a9764c1
Oleg Yukhnevich <whyoleg@gmail.com>
2024-07-09 12:20:59 +0300

    test Kotlin 2.0.10

--- dokka-runners/runner-maven-plugin/settings.gradle.kts
+++ dokka-runners/runner-maven-plugin/settings.gradle.kts
@@ -9,6 +9,7 @@ pluginManagement {
     includeBuild("../../build-settings-logic")
 
     repositories {
+        maven("https://cache-redirector.jetbrains.com/maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
         mavenCentral()
         gradlePluginPortal()
     }
@@ -22,6 +23,7 @@ plugins {
 dependencyResolutionManagement {
     @Suppress("UnstableApiUsage")
     repositories {
+        maven("https://cache-redirector.jetbrains.com/maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
         mavenCentral()
     }
 
