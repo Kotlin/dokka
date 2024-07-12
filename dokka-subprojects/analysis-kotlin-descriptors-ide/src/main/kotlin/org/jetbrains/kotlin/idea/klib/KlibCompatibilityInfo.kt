12646168898987424d15d782a6bf03add7ba6292
Oleg Yukhnevich <whyoleg@gmail.com>
2024-07-12 12:37:54 +0300

    Update to Kotlin 2.0.20-Beta2 (#3690)

--- dokka-subprojects/analysis-kotlin-descriptors-ide/src/main/kotlin/org/jetbrains/kotlin/idea/klib/KlibCompatibilityInfo.kt
+++ dokka-subprojects/analysis-kotlin-descriptors-ide/src/main/kotlin/org/jetbrains/kotlin/idea/klib/KlibCompatibilityInfo.kt
@@ -11,36 +11,32 @@ import org.jetbrains.kotlin.library.metadata.KlibMetadataVersion
 import org.jetbrains.kotlin.library.metadata.metadataVersion
 import java.io.IOException
 
+// File content is based on https://github.com/JetBrains/intellij-community/blob/76b51b233ad2e831c3e1fe8f7dcb0f7c77ad918a/plugins/kotlin/base/project-structure/src/org/jetbrains/kotlin/idea/base/projectStructure/moduleInfo/KlibCompatibilityInfo.kt
+
 /**
  * Whether a certain KLIB is compatible for the purposes of IDE: indexation, resolve, etc.
  */
 internal sealed class KlibCompatibilityInfo(val isCompatible: Boolean) {
     object Compatible : KlibCompatibilityInfo(true)
-    object Pre14Layout : KlibCompatibilityInfo(false)
     class IncompatibleMetadata(val isOlder: Boolean) : KlibCompatibilityInfo(false)
 }
-
 
 internal fun <T> KotlinLibrary.safeRead(defaultValue: T, action: KotlinLibrary.() -> T) = try {
     action()
 } catch (_: IOException) {
     defaultValue
 }
+
 internal val KotlinLibrary.compatibilityInfo: KlibCompatibilityInfo
     get() {
-        val hasPre14Manifest = safeRead(false) { has_pre_1_4_manifest }
-        if (hasPre14Manifest)
-            return KlibCompatibilityInfo.Pre14Layout
-
         val metadataVersion = safeRead(null) { this.metadataVersion }
-        @Suppress("DEPRECATION")
         return when {
             metadataVersion == null -> {
                 // Too old KLIB format, even doesn't have metadata version
                 KlibCompatibilityInfo.IncompatibleMetadata(true)
             }
 
-            !metadataVersion.isCompatible() -> {
+            !metadataVersion.isCompatibleWithCurrentCompilerVersion() -> {
                 val isOlder = metadataVersion.isAtLeast(KlibMetadataVersion.INSTANCE)
                 KlibCompatibilityInfo.IncompatibleMetadata(!isOlder)
             }
