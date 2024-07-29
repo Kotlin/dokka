52df9adf06698e626ee2d6c0924510f4b871152f
Adam Semenenko <152864218+adam-enko@users.noreply.github.com>
2024-07-29 16:45:37 +0200

    re-add dokka-subprojects/build.gradle.kts

---
+++ dokka-subprojects/build.gradle.kts
@@ -0,0 +1,8 @@
+/*
+ * Copyright 2014-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+plugins {
+    id("dokkabuild.base")
+    alias(libs.plugins.kotlinx.binaryCompatibilityValidator)
+}
