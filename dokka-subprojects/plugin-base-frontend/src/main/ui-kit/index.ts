1dfe9f8e45558f713db2e9dfa037c4d161c88774
berezinant <berezinant@gmail.com>
2024-08-06 19:19:38 +0200

    feat: reworked navbar according to new design (#3668)

--- dokka-subprojects/plugin-base-frontend/src/main/ui-kit/index.ts
+++ dokka-subprojects/plugin-base-frontend/src/main/ui-kit/index.ts
@@ -1,6 +1,15 @@
 /*
  * Copyright 2014-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
+import * as filterSection from './filter-section/index';
+import * as libraryName from './library-name/index';
+import * as libraryVersion from './library-version/index';
+import * as navbar from './navbar/index';
+import * as navbarButton from './navbar-button/index';
+import * as platformTag from './platform-tag/index';
+import * as platformTags from './platform-tags/index';
 import * as tabs from './tabs/index';
+import './helpers.scss';
+import './global.scss';
 
-export { tabs };
+export { filterSection, libraryName, libraryVersion, navbar, navbarButton, platformTag, platformTags, tabs };
