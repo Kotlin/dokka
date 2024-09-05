dc3e2f87c0d288c4f90b721433eaf480c99e9bdf
Adam Semenenko <152864218+adam-enko@users.noreply.github.com>
2024-09-05 12:44:49 +0200

    update expected data...

--- dokka-integration-tests/gradle-examples/src/testCustomFormat/expectedData/html/scripts/sourceset_dependencies.js
+++ dokka-integration-tests/gradle-examples/src/testCustomFormat/expectedData/html/scripts/sourceset_dependencies.js
@@ -1,1 +1,1 @@
-sourceset_dependencies = '{":/main":[]}'
+sourceset_dependencies='{":/main":[]}'
--- dokka-integration-tests/gradle-examples/src/testKotlinMultiplatform/expectedData/html/scripts/sourceset_dependencies.js
+++ dokka-integration-tests/gradle-examples/src/testKotlinMultiplatform/expectedData/html/scripts/sourceset_dependencies.js
@@ -1,1 +1,1 @@
-sourceset_dependencies = '{":/main":[]}'
+sourceset_dependencies='{":/commonMain":[],":/customSourceSet":[],":/jsMain":[":/commonMain"],":/jvmMain":[":/commonMain"],":/linuxMain":[":/commonMain"],":/macosMain":[":/commonMain"]}'
