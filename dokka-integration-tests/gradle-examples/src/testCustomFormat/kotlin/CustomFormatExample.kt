935598a8a5bbe9bf967197ded978a00b8d0c5ebc
Adam Semenenko <152864218+adam-enko@users.noreply.github.com>
2024-09-05 12:37:04 +0200

    start testing the other examples...

---
+++ dokka-integration-tests/gradle-examples/src/testCustomFormat/kotlin/CustomFormatExample.kt
@@ -0,0 +1,175 @@
+/*
+ * Copyright 2014-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+package org.jetbrains.dokka.it.gradle.examples
+
+import io.kotest.assertions.asClue
+import io.kotest.assertions.withClue
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
+import io.kotest.matchers.string.shouldNotContain
+import org.gradle.testkit.runner.GradleRunner
+import org.jetbrains.dokka.gradle.utils.*
+import org.jetbrains.dokka.it.gradle.loadConfigurationCacheReportData
+import org.jetbrains.dokka.it.systemProperty
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import java.nio.file.Path
+import kotlin.io.path.Path
+import kotlin.io.path.copyToRecursively
+import kotlin.io.path.name
+import kotlin.io.path.walk
+
+class CustomFormatExample {
+
+    val exampleProject = initProject(projectTestTempDir)
+
+    @Test
+    fun `check name`() {
+        exampleProjectDir.name shouldBe "custom-format-example"
+    }
+
+    @Nested
+    @DisplayName("verify generated HTML")
+    inner class Html {
+        @Test
+        fun `expect DGP can generate HTML`() {
+            exampleProject.runner
+                .addArguments(
+                    ":dokkaGenerate",
+                    "--stacktrace",
+                )
+                .forwardOutput()
+                .build {
+                    output shouldContain "BUILD SUCCESSFUL"
+                }
+        }
+
+        @Nested
+        @DisplayName("expect Dokka HTML matches expected data")
+        inner class ExpectedData {
+            private val exampleDataDir = expectedDataDir.resolve("html")
+            private val dokkaHtmlDir = exampleProject.projectDir.resolve("build/dokka/html")
+
+            @Test
+            fun `expect file trees are the same`() {
+                val expectedFileTree = exampleDataDir.toTreeString()
+                val actualFileTree = dokkaHtmlDir.toTreeString()
+                println((actualFileTree to expectedFileTree).sideBySide())
+                expectedFileTree shouldBe actualFileTree
+            }
+
+            @Test
+            fun `expect directories are the same`() {
+                withClue(
+                    "dokkaHtmlDir[${dokkaHtmlDir.walk().toList()}], " +
+                            "exampleDataDir[${exampleDataDir.walk().toList()}]"
+                ) {
+                    dokkaHtmlDir.shouldHaveSameStructureAs(exampleDataDir, skipEmptyDirs = true)
+                    dokkaHtmlDir.shouldHaveSameStructureAndContentAs(exampleDataDir, skipEmptyDirs = true)
+                }
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("Gradle caching")
+    inner class GradleCaching {
+        @Test
+        fun `expect DGP is compatible with Gradle Build Cache`() {
+            exampleProject.runner
+                .addArguments(
+                    "clean",
+                    ":dokkaGenerate",
+                    "--stacktrace",
+                )
+                .forwardOutput()
+                .build {
+                    output shouldContain "BUILD SUCCESSFUL"
+                }
+
+            exampleProject.runner
+                .addArguments(
+                    ":dokkaGenerate",
+                    "--stacktrace",
+                    "--build-cache",
+                )
+                .forwardOutput()
+                .build {
+                    output shouldContainAll listOf(
+                        "> Task :dokkaGenerateModuleHtml UP-TO-DATE",
+                        "> Task :dokkaGenerate UP-TO-DATE",
+                        "BUILD SUCCESSFUL",
+                        // expect "1 executed" because :checkKotlinGradlePluginConfigurationErrors always runs (fixed KGP in 2.0?)
+                        "2 actionable tasks: 2 up-to-date",
+                    )
+                }
+        }
+
+        @Test
+        fun `expect DGP is compatible with Gradle Configuration Cache`() {
+
+            // delete old configuration cache results and reports
+            exampleProject.findFiles {
+                val isCCDir = it.invariantSeparatorsPath.endsWith(".gradle/configuration-cache")
+                val isCCReportDir = it.invariantSeparatorsPath.endsWith("build/reports/configuration-cache")
+                it.isDirectory && (isCCReportDir || isCCDir)
+            }.forEach { it.deleteRecursively() }
+
+            val configCacheRunner: GradleRunner =
+                exampleProject.runner
+                    .addArguments(
+                        ":dokkaGenerate",
+                        "--stacktrace",
+                        "--configuration-cache",
+                    )
+                    .forwardOutput()
+
+            //first build should store the configuration cache
+            configCacheRunner.build {
+                output shouldContain "BUILD SUCCESSFUL"
+                output shouldContain "Configuration cache entry stored"
+                output shouldNotContain "problems were found storing the configuration cache"
+            }
+
+            val ccReport = exampleProject.loadConfigurationCacheReportData().shouldNotBeNull()
+
+            ccReport.asClue {
+                it.totalProblemCount shouldBe 0
+            }
+
+            // second build should reuse the configuration cache
+            configCacheRunner.build {
+                output shouldContain "BUILD SUCCESSFUL"
+                output shouldContain "Configuration cache entry reused"
+            }
+        }
+    }
+
+    companion object {
+        private val projectTestTempDir by systemProperty(::Path)
+        private val exampleProjectDir by systemProperty(::Path)
+        private val expectedDataDir by systemProperty(::Path)
+
+        private fun initProject(
+            destinationDir: Path,
+        ): GradleProjectTest {
+
+            return GradleProjectTest(destinationDir).apply {
+                exampleProjectDir.copyToRecursively(projectDir, overwrite = true, followLinks = false)
+
+                gradleProperties {
+                    dokka {
+                        v2Plugin = true
+                        v2MigrationHelpers = false
+                        v2PluginNoWarn = true
+                    }
+                }
+
+                updateSettingsRepositories()
+            }
+        }
+    }
+}
---
+++ dokka-integration-tests/gradle-examples/src/testFixtures/kotlin/CustomFooTestExtension.kt
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2014-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+package org.jetbrains.dokka.it.gradle
+
+import io.kotest.property.Arb
+import io.kotest.property.Exhaustive
+import io.kotest.property.arbitrary.bind
+import io.kotest.property.exhaustive.of
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.extension.*
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import java.util.stream.Stream
+
+
+data class Foo(val version: String)
+
+sealed interface DokkaGradlePluginTestParameter {
+    data class AndroidGradlePlugin(val version: String) : DokkaGradlePluginTestParameter
+    data class Gradle(val version: String) : DokkaGradlePluginTestParameter
+    data class KotlinGradlePlugin(val version: String) : DokkaGradlePluginTestParameter
+}
+
+data class DokkaGradlePluginTestParameters(
+    val agpVersion: String,
+    val gradleVersion: String,
+    val kgpVersion: String,
+    val analysisMode: String,
+)
+
+fun a () {
+val agpVersions = Exhaustive.of("")
+val gradleVersions = Exhaustive.of("")
+val kgpVersions = Exhaustive.of("")
+
+    val params = Arb.bind(
+        agpVersions,
+        gradleVersions,
+        kgpVersions,
+    ) {  agp , gradle, kgp ->
+        DokkaGradlePluginTestParameters(
+            agpVersion = agp,
+            gradleVersion = "",
+            kgpVersion = "",
+            analysisMode = "",
+        )
+    }
+}
+
+class FooArgumentsProvider : ArgumentsProvider {
+    override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> {
+        return Stream.of(
+            Arguments.of(Foo("1.0")),
+            Arguments.of(Foo("2.0")),
+            Arguments.of(Foo("3.0"))
+        )
+    }
+}
+
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+@ExtendWith(CustomFooTestExtension::class)
+annotation class CustomFooTest(val parameter: FooParameter = FooParameter.DEFAULT)
+
+enum class FooParameter {
+    DEFAULT
+}
+
+class CustomFooTestExtension :  ParameterResolver, TestInstancePostProcessor, BeforeEachCallback {
+
+    override fun supportsParameter(parameterContext: ParameterContext, extensionContext: ExtensionContext): Boolean {
+        return parameterContext.parameter.type == Foo::class.java
+    }
+
+    override fun resolveParameter(parameterContext: ParameterContext, context: ExtensionContext): Any {
+        val foo = Foo("1.0") // Example; replace with actual dynamic logic as needed
+        context.getStore(ExtensionContext.Namespace.GLOBAL).put("foo", foo)
+        return foo
+    }
+
+//    override fun resolveParameter(parameterContext: ParameterContext, extensionContext: ExtensionContext): Any {
+//        val foo = Foo("1.0") // Example; replace with actual dynamic logic as needed
+//        extensionContext.testInstance.ifPresent { instance ->
+//            val tags = mutableSetOf<String>()
+//            if (foo.version == "1.0") {
+//                tags.add("version-1")
+//            }
+//            val testDescriptor = extensionContext
+//               .root
+//               .uniqueId
+//               .resolve(AnnotationSupport.findAnnotation(instance.javaClass, CustomFooTest::class.java)
+//               .get())
+//               .root
+//            testDescriptor.addTags(*(tags.toTypedArray()))
+//        }
+//        return foo
+//    }
+
+    override fun beforeEach(context: ExtensionContext) {
+        val foo = context.getStore(ExtensionContext.Namespace.GLOBAL).get("foo", Foo::class.java) ?: return
+        if (foo.version == "1.0") {
+            context.testInstance.ifPresent { instance ->
+                instance.javaClass.declaredMethods.forEach { method ->
+                    if (method.isAnnotationPresent(CustomFooTest::class.java)) {
+                        method.annotations.filterIsInstance<Tag>().forEach {
+                            context.tags.add(it.value)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun postProcessTestInstance(testInstance: Any, context: ExtensionContext) {
+        // Implement any additional instance post-processing logic if needed
+    }
+}
