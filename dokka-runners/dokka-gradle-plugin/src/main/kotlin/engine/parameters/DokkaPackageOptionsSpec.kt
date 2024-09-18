ead80292ce91d7f393adee6335ca20a3c3a6ceee
Adam Semenenko <152864218+adam-enko@users.noreply.github.com>
2024-09-18 10:36:54 +0200

    remove unused annotation

--- dokka-runners/dokka-gradle-plugin/src/main/kotlin/engine/parameters/DokkaPackageOptionsSpec.kt
+++ dokka-runners/dokka-gradle-plugin/src/main/kotlin/engine/parameters/DokkaPackageOptionsSpec.kt
@@ -1,9 +1,6 @@
 /*
  * Copyright 2014-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
-
-@file:Suppress("FunctionName")
-
 package org.jetbrains.dokka.gradle.engine.parameters
 
 import org.gradle.api.provider.Property
