87bdcaf34419347da90fc83b81d9746744711001
Anton Berezin <berezinant@gmail.com>
2024-10-01 14:48:25 +0200

    feat: implemented new button states (#3737)
    * feat: implemented new button states
    
    * fix: button stories

--- dokka-subprojects/plugin-base-frontend/src/main/ui-kit/index.ts
+++ dokka-subprojects/plugin-base-frontend/src/main/ui-kit/index.ts
@@ -1,7 +1,9 @@
 /*
  * Copyright 2014-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
+import * as button from './button/index';
 import * as filterSection from './filter-section/index';
+import * as icon from './icon/index';
 import * as libraryName from './library-name/index';
 import * as libraryVersion from './library-version/index';
 import * as navbar from './navbar/index';
@@ -12,4 +14,15 @@ import * as tabs from './tabs/index';
 import './helpers.scss';
 import './global.scss';
 
-export { filterSection, libraryName, libraryVersion, navbar, navbarButton, platformTag, platformTags, tabs };
+export {
+  button,
+  filterSection,
+  icon,
+  libraryName,
+  libraryVersion,
+  navbar,
+  navbarButton,
+  platformTag,
+  platformTags,
+  tabs,
+};
