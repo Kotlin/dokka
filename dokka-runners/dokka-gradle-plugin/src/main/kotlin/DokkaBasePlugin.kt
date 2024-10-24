558913a27b1df0a37803c13719c3e5cf73f6db29
Adam <152864218+adam-enko@users.noreply.github.com>
2024-10-24 14:57:36 +0200

    Rename `dokkaModuleDirectory`, and mark as internal (#3880)
    * Rename `dokkaModuleDirectory`, and mark as internal
    
    Follow up for #3876
    
    When Dokka produces 'partial' Modules it stores them in a project-local directory.
    
    How Dokka processes Modules is an implementation detail, so exposing the directory is not useful for users, and could be confusing.
    It is useful to have a single property for DGP to use though, so it should be kept.
    
    As such, mark it as `internal` to hide it from the DSL.

--- dokka-runners/dokka-gradle-plugin/src/main/kotlin/DokkaBasePlugin.kt
+++ dokka-runners/dokka-gradle-plugin/src/main/kotlin/DokkaBasePlugin.kt
@@ -101,7 +101,7 @@ constructor(
 
             sourceSetScopeDefault.convention(project.path)
             basePublicationsDirectory.convention(layout.buildDirectory.dir("dokka"))
-            dokkaModuleDirectory.convention(layout.buildDirectory.dir("dokka-module"))
+            baseModulesDirectory.convention(layout.buildDirectory.dir("dokka-module"))
 //            @Suppress("DEPRECATION")
 //            dokkaConfigurationsDirectory.convention(layout.buildDirectory.dir("dokka-config"))
 
@@ -158,7 +158,7 @@ constructor(
             moduleVersion.convention(dokkaExtension.moduleVersion)
             offlineMode.convention(false)
             outputDirectory.convention(dokkaExtension.basePublicationsDirectory.dir(formatName))
-            moduleOutputDirectory.convention(dokkaExtension.dokkaModuleDirectory.dir(formatName))
+            moduleOutputDirectory.convention(dokkaExtension.baseModulesDirectory.dir(formatName))
             suppressInheritedMembers.convention(
                 @Suppress("DEPRECATION")
                 dokkaExtension.suppressInheritedMembers
