d302e6b4b23cd6023e2072c628db892871bbaf58
berezinant <berezinant@gmail.com>
2024-11-18 08:24:43 +0100

    fix: made version selector compatible with old versions

--- dokka-subprojects/plugin-base-frontend/src/main/ui-kit/button/index.ts
+++ dokka-subprojects/plugin-base-frontend/src/main/ui-kit/button/index.ts
@@ -2,3 +2,19 @@
  * Copyright 2014-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
 import './styles.scss';
+
+function initButton() {
+  const buttons = document.querySelectorAll('div.button');
+  buttons.forEach((button: Element) => {
+    button.addEventListener('keydown', (event) => {
+      const key = (event as KeyboardEvent).key;
+      if (key === 'Enter' || key === ' ') {
+        button.dispatchEvent(new MouseEvent('click'));
+      }
+    });
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  initButton();
+});
