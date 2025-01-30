2122095205f20af9ccb11b5f28db9d2d90c4f122
Oleg Yukhnevich <whyoleg@gmail.com>
2025-01-30 15:20:49 +0200

    Revert "Use `getOrPut` extension instead of `computeIfAbsent`"
    This reverts commit bd735933c9bb1f00f302fe1788f3d1a53c059e4e.

--- dokka-subprojects/core/src/main/kotlin/org/jetbrains/dokka/model/DisplaySourceSet.kt
+++ dokka-subprojects/core/src/main/kotlin/org/jetbrains/dokka/model/DisplaySourceSet.kt
@@ -75,7 +75,7 @@ internal object DisplaySourceSetCaches {
      * In reality, there will be only several [DokkaSourceSet] instances during Dokka execution.
      */
     fun displaySourceSet(sourceSet: DokkaSourceSet): DisplaySourceSet {
-        return instanceCache.getOrPut(sourceSet) { DisplaySourceSet(sourceSet) }
+        return instanceCache.computeIfAbsent(sourceSet, ::DisplaySourceSet)
     }
 
     /**
@@ -83,7 +83,7 @@ internal object DisplaySourceSetCaches {
      * so it makes sense to cache them by the set itself.
      */
     fun displaySourceSets(sourceSets: Iterable<DokkaSourceSet>): Set<DisplaySourceSet> {
-        return setCache.getOrPut(sourceSets) { sourceSets.map(::displaySourceSet).toSet() }
+        return setCache.computeIfAbsent(sourceSets) { it.map(::displaySourceSet).toSet() }
     }
 
     fun clear() {
