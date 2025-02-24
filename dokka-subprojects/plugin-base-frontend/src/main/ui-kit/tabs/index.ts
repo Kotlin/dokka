a6300df5ab81631200b30a159fed9d88938e9dd0
Anton Berezin <berezinant@gmail.com>
2025-02-24 12:24:10 +0100

    feat: added sidebar resize (#4031)

--- dokka-subprojects/plugin-base-frontend/src/main/ui-kit/tabs/index.ts
+++ dokka-subprojects/plugin-base-frontend/src/main/ui-kit/tabs/index.ts
@@ -1,37 +1,8 @@
 /*
  * Copyright 2014-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
+import { safeLocalStorage } from '../safeLocalStorage';
 import './styles.scss';
-
-/** When Dokka is viewed via iframe, local storage could be inaccessible (see https://github.com/Kotlin/dokka/issues/3323)
- * This is a wrapper around local storage to prevent errors in such cases
- * */
-const safeLocalStorage = (() => {
-  let isLocalStorageAvailable = false;
-  try {
-    const testKey = '__testLocalStorageKey__';
-    localStorage.setItem(testKey, testKey);
-    localStorage.removeItem(testKey);
-    isLocalStorageAvailable = true;
-  } catch (e) {
-    console.error('Local storage is not available', e);
-  }
-
-  return {
-    getItem: (key: string) => {
-      if (!isLocalStorageAvailable) {
-        return null;
-      }
-      return localStorage.getItem(key);
-    },
-    setItem: (key: string, value: string) => {
-      if (!isLocalStorageAvailable) {
-        return;
-      }
-      localStorage.setItem(key, value);
-    },
-  };
-})();
 
 export function initTabs() {
   // we could have only a single type of data - classlike or package
