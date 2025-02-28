6037899d94d35645a5c900beac72b4be0fc200b2
Oleg Yukhnevich <whyoleg@gmail.com>
2025-02-28 16:18:24 +0200

    Migrate ui-showcase to DGPv2

--- dokka-integration-tests/gradle/projects/ui-showcase/build-logic/src/main/kotlin/uitest.dokka-kmp.gradle.kts
+++ dokka-integration-tests/gradle/projects/ui-showcase/build-logic/src/main/kotlin/uitest.dokka-kmp.gradle.kts
@@ -2,26 +2,17 @@
  * Copyright 2014-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
 
-import org.intellij.lang.annotations.Language
-import org.jetbrains.dokka.gradle.AbstractDokkaTask
-
 plugins {
     id("uitest.dokka")
 }
 
-tasks.withType<AbstractDokkaTask>().configureEach {
-    @Language("JSON")
-    val dokkaBaseConfiguration = """
-    {
-      "footerMessage": "<div class='kt-footer__license'>Kotlin™ is protected under the <a href='https://kotlinlang.org/foundation/kotlin-foundation.html' data-test='external-link ' target='_blank' rel='noreferrer noopener' class='kt-footer__link'>Kotlin&nbsp;Foundation</a> and licensed under the&nbsp;<a href='https://github.com/JetBrains/kotlin-web-site/blob/master/LICENSE' data-test='external-link ' target='_blank' rel='noreferrer noopener' class='kt-footer__link'>Apache&nbsp;2 license</a>. </div>",
-      "homepageLink": "https://github.com/Kotlin/dokka/tree/master/dokka-integration-tests/gradle/projects/ui-showcase"
-    }
-    """.trimIndent()
-
-    pluginsMapConfiguration.set(
-        mapOf(
-            // fully qualified plugin name to json configuration
-            "org.jetbrains.dokka.base.DokkaBase" to dokkaBaseConfiguration
+dokka {
+    pluginsConfiguration.html {
+        footerMessage.set(
+            "<div class='kt-footer__license'>Kotlin™ is protected under the <a href='https://kotlinlang.org/foundation/kotlin-foundation.html' data-test='external-link ' target='_blank' rel='noreferrer noopener' class='kt-footer__link'>Kotlin&nbsp;Foundation</a> and licensed under the&nbsp;<a href='https://github.com/JetBrains/kotlin-web-site/blob/master/LICENSE' data-test='external-link ' target='_blank' rel='noreferrer noopener' class='kt-footer__link'>Apache&nbsp;2 license</a>. </div>"
         )
-    )
+        homepageLink.set(
+            "https://github.com/Kotlin/dokka/tree/master/dokka-integration-tests/gradle/projects/ui-showcase"
+        )
+    }
 }
--- dokka-integration-tests/gradle/projects/ui-showcase/build-logic/src/main/kotlin/uitest.dokka.gradle.kts
+++ dokka-integration-tests/gradle/projects/ui-showcase/build-logic/src/main/kotlin/uitest.dokka.gradle.kts
@@ -2,30 +2,17 @@
  * Copyright 2014-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
 
-import org.intellij.lang.annotations.Language
-import org.jetbrains.dokka.gradle.AbstractDokkaTask
-
 plugins {
     id("org.jetbrains.dokka")
 }
 
-tasks.withType<AbstractDokkaTask>().configureEach {
-    @Language("JSON")
-    val versioningPluginConfiguration = """
-    {
-      "olderVersionsDir": "${project.rootProject.projectDir.resolve("previousDocVersions").invariantSeparatorsPath}"
+dokka {
+    pluginsConfiguration.versioning {
+        olderVersionsDir.set(rootDir.resolve("previousDocVersions"))
     }
-    """.trimIndent()
-
-    pluginsMapConfiguration.set(
-        mapOf(
-            // fully qualified plugin name to json configuration
-            "org.jetbrains.dokka.versioning.VersioningPlugin" to versioningPluginConfiguration
-        )
-    )
 }
 
-val dokka_it_dokka_version: String by project
 dependencies {
-    dokkaPlugin("org.jetbrains.dokka:versioning-plugin:$dokka_it_dokka_version")
+    // No version is necessary, Dokka will add it automatically
+    dokkaHtmlPlugin("org.jetbrains.dokka:versioning-plugin")
 }
