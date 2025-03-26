b05988b4df9617003e0e235cf808401f50b33d46
berezinant <berezinant@gmail.com>
2025-03-26 19:17:00 +0100

    test: updated html output tests

---
+++ dokka-integration-tests/gradle/src/testExampleProjects/expectedData/exampleProjects/kotlin-multiplatform-example/html/scripts/safe-local-storage_blocking.js
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2014-2025 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+/** When Dokka is viewed via iframe, local storage could be inaccessible (see https://github.com/Kotlin/dokka/issues/3323)
+ * This is a wrapper around local storage to prevent errors in such cases
+ * */
+const safeLocalStorage = (() => {
+  let isLocalStorageAvailable = false;
+  try {
+    const testKey = '__testLocalStorageKey__';
+    localStorage.setItem(testKey, testKey);
+    localStorage.removeItem(testKey);
+    isLocalStorageAvailable = true;
+  } catch (e) {
+    console.error('Local storage is not available', e);
+  }
+
+  return {
+    getItem: (key) => {
+      if (!isLocalStorageAvailable) {
+        return null;
+      }
+      return localStorage.getItem(key);
+    },
+    setItem: (key, value) => {
+      if (!isLocalStorageAvailable) {
+        return;
+      }
+      localStorage.setItem(key, value);
+    },
+    getKeys: () => {
+    if (!isLocalStorageAvailable) {
+      return [];
+    }
+    return Object.keys(localStorage);
+  },
+  };
+})();
