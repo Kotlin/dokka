571c536e8120585facd617d4926a55fa1c9c76b7
Anton Berezin <berezinant@gmail.com>
2025-04-17 17:41:13 +0200

    feat: preserve toc expanded/collapsed state (#4063)
    * feat: preserve toc scroll position
    
    * test: updated html output tests
    
    * UI fixes for table of contents (#4077)
    
    * fix: prevent unwanted dropdown list scroll
    
    * fix: include dropdown toggle into focus trap
    
    * fix: smooth keyboard navigation for toc and dropdowns
    
    * feat: added skip to main content links to toc (#4078)
    
    * fix: proper scroll into view in the toc modal
    
    * feat: implemented skip to main content link
    
    * fix: accessibility for toc and navbar
    
    * fix: footer button outline
    
    * fix: improved accessibility in header and toc
    
    * refactor: review fixes

--- dokka-subprojects/plugin-base-frontend/src/main/ui-kit/navbar-button/index.ts
+++ dokka-subprojects/plugin-base-frontend/src/main/ui-kit/navbar-button/index.ts
@@ -2,7 +2,7 @@
  * Copyright 2014-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
 import './styles.scss';
-import { onToggleDropdown } from '../dropdown';
+import { DROPDOWN_TOGGLED_EVENT, onToggleDropdown, TDropdownToggledDto } from '../dropdown';
 
 function initTocToggle() {
   const tocToggle = document.getElementById('toc-toggle');
@@ -17,7 +17,7 @@ function initTocToggle() {
   });
 }
 
-export function toggleTocDropdown(): void {
+function toggleTocDropdown(): void {
   const tocDropdown = document.getElementById('toc-dropdown');
   if (!tocDropdown) {
     console.warn('Dokka: toc dropdown is not found');
@@ -26,6 +26,42 @@ export function toggleTocDropdown(): void {
   onToggleDropdown(tocDropdown);
 }
 
+document.addEventListener(DROPDOWN_TOGGLED_EVENT, (event) => {
+  const { dropdownId, isExpanded } = (event as CustomEvent<TDropdownToggledDto>).detail;
+  if (dropdownId === 'toc-dropdown') {
+    if (isExpanded) {
+      focusTocCloseButton();
+      scrollActiveTocPartIntoView();
+    } else {
+      focusTocOpenButton();
+    }
+  }
+});
+
+function scrollActiveTocPartIntoView(): void {
+  const activePart = document.querySelector('.toc--part[data-active="true"]')?.querySelector('.toc--link');
+  if (activePart) {
+    activePart.scrollIntoView({ block: 'center' });
+  }
+}
+
+function focusTocCloseButton(): void {
+  const tocCloseButton = document
+    .getElementById('leftColumn')
+    ?.querySelector('[data-role="dropdown-toggle"]') as HTMLElement;
+  if (tocCloseButton) {
+    tocCloseButton.focus();
+  }
+}
+
+function focusTocOpenButton(): void {
+  const tocOpenButton = document.getElementById('toc-toggle') as HTMLElement;
+  if (tocOpenButton) {
+    tocOpenButton.focus();
+  }
+}
+
+// Needs to be exposed to the global scope to be called from the outside
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 (window as any).toggleTocDropdown = toggleTocDropdown;
 
