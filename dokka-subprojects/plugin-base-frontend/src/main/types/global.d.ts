0ef096a45424fe3246a87d291f07ad40930e260c
Anton Berezin <berezinant@gmail.com>
2025-05-05 17:26:01 +0200

    feat: clear toc state by click on the logo and by closing a tab (#4093)

--- dokka-subprojects/plugin-base-frontend/src/main/types/global.d.ts
+++ dokka-subprojects/plugin-base-frontend/src/main/types/global.d.ts
@@ -8,5 +8,16 @@
 declare const safeLocalStorage: {
   getItem: (key: string) => string | null;
   setItem: (key: string, value: string) => void;
+  removeItem: (key: string) => void;
+  getKeys: () => string[];
+};
+
+/** When Dokka is viewed via iframe, session storage could be inaccessible (see https://github.com/Kotlin/dokka/issues/3323)
+ * The wrapper around session storage to prevent errors in such cases is defined in the plugin-base scripts assets
+ * */
+declare const safeSessionStorage: {
+  getItem: (key: string) => string | null;
+  setItem: (key: string, value: string) => void;
+  removeItem: (key: string) => void;
   getKeys: () => string[];
 };
