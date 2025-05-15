bcba0db23b7a4305eacb4a1db1181d8627216038
berezinant <berezinant@gmail.com>
2025-05-15 18:40:43 +0200

    fix: restyled tables

--- dokka-subprojects/core/src/main/kotlin/org/jetbrains/dokka/pages/ContentNodes.kt
+++ dokka-subprojects/core/src/main/kotlin/org/jetbrains/dokka/pages/ContentNodes.kt
@@ -397,6 +397,7 @@ public enum class TextStyle : Style {
 }
 
 public enum class ContentStyle : Style {
+   TableHeader,
     RowTitle,
     /**
      * The style is used only for HTML. It is applied only for [ContentGroup].
