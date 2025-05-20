06cd67ea6d5b139f46e59929de4da8e17ac32259
Adam Semenenko <152864218+adam-enko@users.noreply.github.com>
2025-05-20 14:01:05 +0200

    bump kxs

--- examples/gradle-v2/custom-dokka-plugin-example/dokka-plugin-hide-internal-api/build.gradle.kts
+++ examples/gradle-v2/custom-dokka-plugin-example/dokka-plugin-hide-internal-api/build.gradle.kts
@@ -9,5 +9,5 @@ dependencies {
     compileOnly("org.jetbrains.dokka:dokka-core:$dokkaVersion")
     implementation("org.jetbrains.dokka:dokka-base:$dokkaVersion")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
 }
