17fe988bc255068e6d5c454a32e954c372ea5e23
Oleg Yukhnevich <whyoleg@gmail.com>
2025-05-23 18:19:42 +0300

    WIP (types)

--- kotlin-documentation/src/commonMain/kotlin/KdDocumentation.kt
+++ kotlin-documentation/src/commonMain/kotlin/KdDocumentation.kt
@@ -4,15 +4,54 @@
 
 package org.jetbrains.kotlin.documentation
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
 public sealed class KdDocumented {
     public abstract val documentation: KdDocumentation?
 }
 
-public interface KdDocumentation
+@Serializable
+public data class KdDocumentation(
+    public val elements: List<KdDocumentationElement>
+)
 
 // parsed markdown representation: text, links, etc
+@Serializable
 public sealed class KdDocumentationElement {
+    @SerialName("text")
+    @Serializable
     public data class Text(public val value: String) : KdDocumentationElement()
-    public data class Link(public val elementId: KdElementId) : KdDocumentationElement()
+
+    @SerialName("code")
+    @Serializable
+    public data class Code(
+        public val lines: List<String>,
+        public val language: String? = null
+    ) : KdDocumentationElement()
+
+    @SerialName("parameter-link")
+    @Serializable
+    public data class ParameterLink(public val parameterName: String /* this -> receiver */) : KdDocumentationElement()
+
+    @SerialName("type-parameter-link")
+    @Serializable
+    public data class TypeParameterLink(public val typeParameterName: String) : KdDocumentationElement()
+
+    @SerialName("reference-link")
+    @Serializable
+    public data class ReferenceLink(public val elementId: KdElementId) : KdDocumentationElement()
+
+    // TODO: multi-reference, when we have ambiguity
+    @SerialName("ambiguity-reference-link")
+    @Serializable
+    public data class AmbiguityReferenceLink(public val elementIds: List<KdElementId>) : KdDocumentationElement()
+
+    @SerialName("external-link")
+    @Serializable
+    public data class ExternalLink(public val url: String) : KdDocumentationElement()
+
+    @SerialName("paragraph")
+    @Serializable
     public data class Paragraph(public val elements: List<KdDocumentationElement>) : KdDocumentationElement()
 }
