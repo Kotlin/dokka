8d2afa812ee9b7c0435301b3bcebb68bf29ad52b
Adam Semenenko <152864218+adam-enko@users.noreply.github.com>
2025-06-11 09:59:15 +0200

    fix DatetimeGradleIntegrationTest

--- dokka-integration-tests/gradle/src/testExternalProjectKotlinxDatetime/kotlin/DatetimeGradleIntegrationTest.kt
+++ dokka-integration-tests/gradle/src/testExternalProjectKotlinxDatetime/kotlin/DatetimeGradleIntegrationTest.kt
@@ -50,7 +50,11 @@ class DatetimeGradleIntegrationTest : AbstractGradleIntegrationTest(), TestOutpu
     @ParameterizedTest(name = "{0}")
     @ArgumentsSource(DatetimeBuildVersionsArgumentsProvider::class)
     fun execute(buildVersions: BuildVersions) {
-        val result = createGradleRunner(buildVersions, ":kotlinx-datetime:dokkaGenerate").buildRelaxed()
+        val result = createGradleRunner(
+            buildVersions,
+            ":kotlinx-datetime:dokkaGenerate",
+            dgpPluginMode = null,
+        ).buildRelaxed()
 
         assertEquals(TaskOutcome.SUCCESS, assertNotNull(result.task(":kotlinx-datetime:dokkaGenerate")).outcome)
 
