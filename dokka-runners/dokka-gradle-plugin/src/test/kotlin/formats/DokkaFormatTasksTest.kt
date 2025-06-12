1ceff9ba8327322ef31ad5a805acd8e49df1ac60
Adam Semenenko <152864218+adam-enko@users.noreply.github.com>
2025-06-12 10:14:46 +0200

    DGPv2: Add lifecycle tasks for generating formats
    #3960

---
+++ dokka-runners/dokka-gradle-plugin/src/test/kotlin/formats/DokkaFormatTasksTest.kt
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2014-2025 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package org.jetbrains.dokka.gradle.formats
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.collections.shouldBeEmpty
+import io.kotest.matchers.collections.shouldHaveSingleElement
+import io.kotest.matchers.shouldBe
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.getByType
+import org.gradle.testfixtures.ProjectBuilder
+import org.jetbrains.dokka.gradle.DokkaBasePlugin
+import org.jetbrains.dokka.gradle.DokkaExtension
+import org.jetbrains.dokka.gradle.dependencies.FormatDependenciesManager
+import org.jetbrains.dokka.gradle.utils.enableV2Plugin
+
+class DokkaFormatTasksTest : FunSpec({
+
+    context("verify format tasks") {
+        val project = projectWithDgpV2()
+
+        val dokkaExtension = project.extensions.getByType<DokkaExtension>()
+
+        val formatName = "testFormatName"
+        val htmlPublication = dokkaExtension.dokkaPublications.create(formatName)
+
+        val htmlFormatDependencies = FormatDependenciesManager(
+            project = project,
+            baseDependencyManager = dokkaExtension.baseDependencyManager,
+            formatName = formatName,
+            objects = project.objects,
+        )
+
+        val dokkaTasks = DokkaFormatTasks(
+            project = project,
+            publication = htmlPublication,
+            formatDependencies = htmlFormatDependencies,
+            providers = project.providers,
+        )
+
+        test("expect lifecycle generate task created") {
+            dokkaTasks.lifecycleGenerate.name shouldBe "dokkaGenerateTestFormatName"
+            dokkaTasks.generateModule.get().description shouldBe "Generate Dokka testFormat publications"
+            dokkaTasks.lifecycleGenerate.get().group shouldBe "dokka"
+            dokkaTasks.lifecycleGenerate.get().dependsOn shouldHaveSingleElement dokkaTasks.generatePublication
+        }
+
+        test("expect generate publication task created") {
+            dokkaTasks.generatePublication.name shouldBe "dokkaGeneratePublicationTestFormatName"
+            dokkaTasks.generatePublication.get().description =
+                "Executes the Dokka Generator, generating a $formatName publication"
+            dokkaTasks.generatePublication.get().group shouldBe "dokka"
+            dokkaTasks.generatePublication.get().dependsOn.shouldBeEmpty()
+        }
+
+        test("expect generate module task created") {
+            dokkaTasks.generateModule.name shouldBe "dokkaGenerateModuleTestFormatName"
+            dokkaTasks.generateModule.get().description =
+                "Executes the Dokka Generator, generating a $formatName module"
+            dokkaTasks.generateModule.get().group shouldBe "dokka"
+            dokkaTasks.generateModule.get().dependsOn.shouldBeEmpty()
+        }
+    }
+})
+
+private fun projectWithDgpV2(): Project =
+    ProjectBuilder.builder().build()
+        .enableV2Plugin()
+        .also { it.plugins.apply(DokkaBasePlugin::class) }
