ddd0de036ea6adbccaac705946d6a6408a31696e
Anton Berezin <berezinant@gmail.com>
2025-06-20 16:41:53 +0200

    fix: search and tables fixes (#4153)
    * fix: restyled tables
    
    * fix: restyled table headers
    
    * fix: make types table table
    
    * fix: restyled platform tags
    
    * fix: aligned platform tags
    
    * test: updated HTML tests
    
    * fix: hide dividers after tables
    
    * fix: paddings for packages and modules
    
    * fix: proper headers
    
    * fix: alignment of platform tags
    
    * fix: headers in html
    
    * feat: search accessible from keyboard
    
    * feat: toc hover
    
    * feat: accessible search scroll
    
    * refactor: one observer for the list
    
    * fix: removed unnecessary timeout
    
    * fix: review

--- dokka-subprojects/plugin-base-frontend/src/main/components/search/search.tsx
+++ dokka-subprojects/plugin-base-frontend/src/main/components/search/search.tsx
@@ -7,6 +7,7 @@ import Select, { SelectItem } from '@jetbrains/ring-ui/components/select/select'
 import React, { useCallback, useEffect, useState } from 'react';
 import '@jetbrains/ring-ui/components/input-size/input-size.css';
 import './search.scss';
+import { FocusTrap } from '../../ui-kit/dropdown/focus-trap';
 import { relativizeUrlForRequest } from '../utils/requests';
 import { DokkaFuzzyFilterComponent } from './dokkaFuzzyFilter';
 import { DokkaSearchAnchor } from './dokkaSearchAnchor';
@@ -21,11 +22,6 @@ const WithFuzzySearchFilterComponent: React.FC<Props> = ({ data }: Props) => {
     const maybeOption: Option = selectItem as Option;
     window.location.replace(`${(window as IWindow).pathToRoot}${maybeOption.location}?query=${maybeOption.name}`);
     onSelected(maybeOption);
-  }, []);
-
-  useEffect(() => {
-    addCloseSearchPopupButtonEventListener();
-    return removeCloseSearchPopupButtonEventListener;
   }, []);
 
   return (
@@ -50,6 +46,7 @@ const WithFuzzySearchFilterComponent: React.FC<Props> = ({ data }: Props) => {
           )}
           onOpen={onSearchPopupOpen}
           onClose={onSearchPopupClose}
+          onFilter={resetSearchOptionsListScrollPosition}
         />
       </div>
     </div>
@@ -86,33 +83,146 @@ export const WithFuzzySearchFilter = () => {
   return <WithFuzzySearchFilterComponent data={navigationList} />;
 };
 
-function addCloseSearchPopupButtonEventListener() {
-  document.addEventListener('click', handleCloseSearchPopupButtonClick, true);
+let focusTrap: FocusTrap | null = null;
+
+function onSearchPopupOpen() {
+  const clearButton = document.querySelector('[data-test="ring-input-clear"]');
+  if (clearButton) {
+    const closeButton = createSearchPopupCloseButton();
+    clearButton.after(closeButton);
+    closeButton.addEventListener('click', handleCloseSearchPopupButtonClick);
+    clearButton.addEventListener('click', handleClearButtonClick);
+  }
+  const inputContainer = document.querySelector('[data-test="ring-input"]');
+  if (inputContainer) {
+    inputContainer.addEventListener('keydown', handleInputKeyDown);
+    focusTrap = new FocusTrap({
+      trapElement: inputContainer as HTMLElement,
+      interactiveElementsSelector: () =>
+        '[data-test-custom="ring-select-popup-filter-input"], [data-test="ring-input-clear"], #search-close-button',
+    });
+  }
+  document.body.style.overflow = 'hidden';
+  initActiveSearchOptionObserver();
 }
 
-function removeCloseSearchPopupButtonEventListener() {
-  document.removeEventListener('click', handleCloseSearchPopupButtonClick, true);
+function createSearchPopupCloseButton() {
+  const closeButton = document.createElement('button');
+  closeButton.id = 'search-close-button';
+  closeButton.className = 'button button_dropdown button_dropdown_active search--close-button';
+  closeButton.setAttribute('aria-label', 'Close search popup');
+  return closeButton;
+}
+
+function onSearchPopupClose() {
+  document.body.style.overflow = '';
+  destroyActiveSearchOptionsObserver();
+}
+
+function handleInputKeyDown(event: Event) {
+  const { key } = event as KeyboardEvent;
+  if (key === 'Tab') {
+    // Prevent the ring ui component from closing the popup
+    event.preventDefault();
+    event.stopPropagation();
+  }
+  if (key === 'Enter' && (event.target as HTMLElement).tagName.toLowerCase() !== 'input') {
+    event.stopPropagation();
+  }
+}
+
+function handleClearButtonClick() {
+  const inputElement = document.querySelector('[data-test-custom="ring-select-popup-filter-input"]');
+  if (inputElement) {
+    (inputElement as HTMLInputElement).focus();
+  }
 }
 
 function handleCloseSearchPopupButtonClick(event: Event) {
   const target = event.target as HTMLElement;
   if (target?.id === 'search-close-button') {
-    document.getElementById('pages-search')?.click();
+    const inputContainer = document.querySelector('[data-test="ring-input"]');
+    if (inputContainer) {
+      inputContainer.removeEventListener('keydown', handleInputKeyDown);
+    }
+    const clearButton = document.querySelector('[data-test="ring-input-clear"]') as HTMLButtonElement;
+    if (clearButton) {
+      clearButton.removeEventListener('click', handleClearButtonClick);
+    }
+    const closeButton = document.getElementById('search-close-button') as HTMLButtonElement;
+    if (closeButton) {
+      closeButton.removeEventListener('click', handleCloseSearchPopupButtonClick);
+    }
+    if (focusTrap) {
+      focusTrap.destroy();
+      focusTrap = null;
+    }
+    const searchAnchor = document.getElementById('pages-search');
+    if (searchAnchor) {
+      searchAnchor.focus();
+      // Closes the search popup
+      searchAnchor.click();
+    }
   }
 }
 
-function onSearchPopupOpen() {
-  const cleatButton = document.querySelector('[data-test="ring-input-clear"]');
-  if (cleatButton) {
-    const closeButton = document.createElement('button');
-    closeButton.id = 'search-close-button';
-    closeButton.className = 'button button_dropdown button_dropdown_active search--close-button';
-    closeButton.setAttribute('aria-label', 'Close search popup');
-    cleatButton.after(closeButton);
+let activeSearchOptionsObserver: MutationObserver | null = null;
+
+function initActiveSearchOptionObserver() {
+  destroyActiveSearchOptionsObserver();
+
+  activeSearchOptionsObserver = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      const searchOptionElement = mutation.target as HTMLElement;
+
+      if (isSearchOptionActive(searchOptionElement) && !isSearchOptionVisible(searchOptionElement)) {
+        searchOptionElement.scrollIntoView({
+          behavior: 'smooth',
+          block: 'center',
+          inline: 'center',
+        });
+      }
+    });
+  });
+
+  const listWrapper = document.querySelector('.ReactVirtualized__Grid');
+
+  if (!listWrapper || !activeSearchOptionsObserver) {
+    return;
   }
-  document.body.style.overflow = 'hidden';
+  activeSearchOptionsObserver.observe(listWrapper, {
+    childList: true,
+    subtree: true,
+    attributes: true,
+    attributeFilter: ['class'],
+  });
 }
 
-function onSearchPopupClose() {
-  document.body.style.overflow = '';
+function destroyActiveSearchOptionsObserver() {
+  if (activeSearchOptionsObserver !== null) {
+    activeSearchOptionsObserver.disconnect();
+    activeSearchOptionsObserver = null;
+  }
+}
+
+function resetSearchOptionsListScrollPosition() {
+  const listWrapper = document.querySelector('.ReactVirtualized__Grid');
+  if (listWrapper) {
+    listWrapper.scrollTop = 0;
+  }
+}
+
+function isSearchOptionActive(element: HTMLElement): boolean {
+  return [...element.classList].some((className) => className.includes('hover'));
+}
+
+function isSearchOptionVisible(element: HTMLElement): boolean {
+  const listWrapper = element.closest('.ReactVirtualized__Grid');
+  if (!listWrapper) {
+    return false;
+  }
+
+  const listRect = listWrapper.getBoundingClientRect();
+  const elementRect = element.getBoundingClientRect();
+  return elementRect.top >= listRect.top && elementRect.bottom <= listRect.bottom;
 }
