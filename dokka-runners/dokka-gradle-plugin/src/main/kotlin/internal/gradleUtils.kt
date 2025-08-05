cce66ad7ab4b84b57be2a9b5399f8a6d77909544
Adam Semenenko <152864218+adam-enko@users.noreply.github.com>
2025-08-05 14:44:11 +0200

    add kdoc for `Project.pathAsFilePath()`

--- dokka-runners/dokka-gradle-plugin/src/main/kotlin/internal/gradleUtils.kt
+++ dokka-runners/dokka-gradle-plugin/src/main/kotlin/internal/gradleUtils.kt
@@ -112,6 +112,14 @@ internal operator fun GradleVersion.compareTo(version: String): Int =
 internal operator fun <T> Spec<T>.not(): Spec<T> = Spec<T> { !this@not.isSatisfiedBy(it) }
 
 
+/**
+ * Convert a project path to a relative path.
+ *
+ * E.g. `:x:y:z:my-cool-subproject` → `x/y/z/my-cool-subproject`.
+ *
+ * Used for [org.jetbrains.dokka.gradle.DokkaExtension.modulePath].
+ * The path has to be unique per module - using the project path is a useful way to achieve this.
+ */
 internal fun Project.pathAsFilePath(): String = path
     .removePrefix(GradleProjectPath.SEPARATOR)
     .replace(GradleProjectPath.SEPARATOR, "/")
