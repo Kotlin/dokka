9d6b0c3691247175ef3d0b3adb1b9363b2bddd47
Adam <152864218+adam-enko@users.noreply.github.com>
2025-08-12 20:55:28 +0200

    Throw error when running V1 migration helper tasks (#4187)
    Also, suppress AbstractDokkaTask deprecation warning
    
    ---------
    
    Co-authored-by: Oleg Yukhnevich <whyoleg@gmail.com>

--- dokka-runners/dokka-gradle-plugin/src/testFunctional/kotlin/DokkaV1TaskDisabledTest.kt
+++ dokka-runners/dokka-gradle-plugin/src/testFunctional/kotlin/DokkaV1TaskDisabledTest.kt
@@ -4,9 +4,10 @@
 package org.jetbrains.dokka.gradle
 
 import io.kotest.core.spec.style.FunSpec
-import org.gradle.testkit.runner.TaskOutcome.SKIPPED
+import io.kotest.matchers.string.shouldContain
+import org.gradle.testkit.runner.TaskOutcome.FAILED
 import org.jetbrains.dokka.gradle.utils.addArguments
-import org.jetbrains.dokka.gradle.utils.build
+import org.jetbrains.dokka.gradle.utils.buildAndFail
 import org.jetbrains.dokka.gradle.utils.projects.initNoConfigMultiModuleProject
 import org.jetbrains.dokka.gradle.utils.shouldHaveRunTask
 
@@ -21,13 +22,57 @@ class DokkaV1TaskDisabledTest : FunSpec({
             }
         }
 
-        test("v1 tasks should be skipped when v2 is enabled") {
-            project.runner
-                .addArguments("dokkaHtml")
-                .build {
-                    shouldHaveRunTask(":subproject-one:dokkaHtml", SKIPPED)
-                    shouldHaveRunTask(":subproject-two:dokkaHtml", SKIPPED)
-                }
+        context("v1 tasks should fail (because v2 is enabled by default)") {
+            test("dokkaHtml") {
+                project.runner
+                    .addArguments("dokkaHtml")
+                    .buildAndFail {
+                        shouldHaveRunTask(":subproject-one:dokkaHtml", FAILED)
+                        output shouldContain /* language=text */ """
+                            |> Cannot run Dokka V1 tasks when V2 mode is enabled.
+                            |  Dokka Gradle plugin V1 mode is deprecated, and scheduled to be removed in Dokka v2.2.0.
+                            |  To finish migrating to V2 mode, please check the migration guide https://kotl.in/dokka-gradle-migration
+                            |  Suggestion: Use `dokkaGenerate` or `dokkaGenerateHtml` tasks instead.
+                            """.trimMargin()
+                    }
+            }
+            test("dokkaJavadoc") {
+                project.runner
+                    .addArguments("dokkaJavadoc")
+                    .buildAndFail {
+                        shouldHaveRunTask(":subproject-one:dokkaJavadoc", FAILED)
+                        output shouldContain /* language=text */ """
+                            |> Cannot run Dokka V1 tasks when V2 mode is enabled.
+                            |  Dokka Gradle plugin V1 mode is deprecated, and scheduled to be removed in Dokka v2.2.0.
+                            |  To finish migrating to V2 mode, please check the migration guide https://kotl.in/dokka-gradle-migration
+                            |  Suggestion: Use `dokkaGenerate` or `dokkaGenerateJavadoc` tasks instead.
+                            """.trimMargin()
+                    }
+            }
+            test("dokkaGfm") {
+                project.runner
+                    .addArguments("dokkaGfm")
+                    .buildAndFail {
+                        shouldHaveRunTask(":subproject-one:dokkaGfm", FAILED)
+                        output shouldContain /* language=text */ """
+                            |> Cannot run Dokka V1 tasks when V2 mode is enabled.
+                            |  Dokka Gradle plugin V1 mode is deprecated, and scheduled to be removed in Dokka v2.2.0.
+                            |  To finish migrating to V2 mode, please check the migration guide https://kotl.in/dokka-gradle-migration
+                            """.trimMargin()
+                    }
+            }
+            test("dokkaJekyll") {
+                project.runner
+                    .addArguments("dokkaJekyll")
+                    .buildAndFail {
+                        shouldHaveRunTask(":subproject-one:dokkaJekyll", FAILED)
+                        output shouldContain /* language=text */ """
+                            |> Cannot run Dokka V1 tasks when V2 mode is enabled.
+                            |  Dokka Gradle plugin V1 mode is deprecated, and scheduled to be removed in Dokka v2.2.0.
+                            |  To finish migrating to V2 mode, please check the migration guide https://kotl.in/dokka-gradle-migration
+                            """.trimMargin()
+                    }
+            }
         }
     }
 })
