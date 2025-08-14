adfb3c14ae8e9b778a7280211bbb06d6442bc40a
vmishenev <vad-mishenev@yandex.ru>
2025-08-14 12:53:24 +0300

    Increase metaspace a little

--- dokka-integration-tests/gradle/src/testTemplateProjectMultimodule0/kotlin/MultiModule0IntegrationTest.kt
+++ dokka-integration-tests/gradle/src/testTemplateProjectMultimodule0/kotlin/MultiModule0IntegrationTest.kt
@@ -23,7 +23,7 @@ class MultiModule0IntegrationTest : AbstractGradleIntegrationTest() {
             ":moduleA:dokkaHtmlMultiModule",
             ":moduleA:dokkaGfmMultiModule",
             ":moduleA:dokkaJekyllMultiModule",
-            jvmArgs = listOf("-Xmx2G", "-XX:MaxMetaspaceSize=1400m")
+            jvmArgs = listOf("-Xmx2G", "-XX:MaxMetaspaceSize=1600m")
         ).buildRelaxed()
 
         result.shouldHaveTask(":moduleA:dokkaHtmlMultiModule").shouldHaveOutcome(SUCCESS, FROM_CACHE)
