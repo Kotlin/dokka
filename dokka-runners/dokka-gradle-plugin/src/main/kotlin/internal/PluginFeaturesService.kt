01bf9821b60e1e8caedc219035c780159edc33fe
Adam Semenenko <152864218+adam-enko@users.noreply.github.com>
2025-08-19 17:39:47 +0200

    Update K1 deprecation message

--- dokka-runners/dokka-gradle-plugin/src/main/kotlin/internal/PluginFeaturesService.kt
+++ dokka-runners/dokka-gradle-plugin/src/main/kotlin/internal/PluginFeaturesService.kt
@@ -202,7 +202,7 @@ constructor(
             logger.lifecycle(
                 """
                 |Dokka K1 Analysis is deprecated. It can cause build failures or generate incorrect documentation.
-                |We recommend using Dokka K2 Analysis, which supports new language features like context parameters.
+                |Please use Dokka K2 Analysis, which is enabled by default, and supports new language features like context parameters.
                 |
                 |To start using Dokka K2 Analysis remove
                 |    ${K2_ANALYSIS_ENABLED_FLAG}=false
