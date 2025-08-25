fa41f6d3049be476cc671e4d8cb71bff9c679820
Oleg Yukhnevich <whyoleg@gmail.com>
2025-08-25 15:34:35 +0300

    Set the 2.1.0-SNAPSHOT version for consistency (#4225)

--- examples/gradle-v2/basic-gradle-example/build.gradle.kts
+++ examples/gradle-v2/basic-gradle-example/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "2.2.0"
-    id("org.jetbrains.dokka") version "2.0.20-SNAPSHOT"
+    id("org.jetbrains.dokka") version "2.1.0-SNAPSHOT"
 }
 
 dokka {
--- examples/gradle-v2/versioning-multimodule-example/buildSrc/build.gradle.kts
+++ examples/gradle-v2/versioning-multimodule-example/buildSrc/build.gradle.kts
@@ -4,6 +4,6 @@ plugins {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
-    val dokkaVersion = providers.gradleProperty("dokkaVersion").getOrElse("2.0.20-SNAPSHOT")
+    val dokkaVersion = providers.gradleProperty("dokkaVersion").getOrElse("2.1.0-SNAPSHOT")
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:$dokkaVersion")
 }
