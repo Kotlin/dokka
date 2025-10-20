4526bfe6d6f41988a9dd460b8f7036e609d97854
Yurii Oliinyk <oliinykyurii123@gmail.com>
2025-10-20 12:35:52 +0200

    feat: add RunnableSamplesPlugin

---
+++ dokka-subprojects/plugin-runnable-samples/src/main/kotlin/org.jetbrains.dokka.runnablesamples/RunnableSamplesConfiguration.kt
@@ -0,0 +1,12 @@
+package org.jetbrains.dokka.runnablesamples
+
+import org.jetbrains.dokka.plugability.ConfigurableBlock
+
+public data class RunnableSamplesConfiguration(
+    var kotlinPlaygroundScript: String = defaultKotlinPlaygroundScript
+) : ConfigurableBlock {
+    public companion object {
+        public const val defaultKotlinPlaygroundScript: String =
+            "https://unpkg.com/kotlin-playground@1/dist/playground.min.js"
+    }
+}
---
+++ dokka-subprojects/plugin-runnable-samples/src/test/kotlin/org.jetbrains.dokka.runnablesamples/RunnableSamplesPluginTest.kt
@@ -0,0 +1,41 @@
+package org.jetbrains.dokka.runnablesamples
+
+import org.jetbrains.dokka.base.testApi.testRunner.BaseAbstractTest
+import org.jetbrains.dokka.plugability.plugin
+import kotlin.test.Test
+import kotlin.test.assertNotNull
+
+class RunnableSamplesPluginTest : BaseAbstractTest() {
+    private val configuration = dokkaConfiguration {
+        sourceSets {
+            sourceSet {
+                sourceRoots = listOf("src/main/kotlin")
+            }
+        }
+    }
+
+    @Test
+    fun `RunnableSamplesPlugin is loaded into DokkaContext`() {
+        testInline(
+            """
+            |/src/main/kotlin/Sample.kt
+            |package com.example
+            |
+            |fun sampleFunction() {
+            |    println("This is a sample")
+            |}
+            |
+            | /**
+            | * @sample [com.example.sampleFunction]
+            | */
+            |class Foo
+            """.trimMargin(),
+            configuration = configuration
+        ) {
+            pluginsSetupStage = { context ->
+                val runnableSamplesPlugin = context.plugin<RunnableSamplesPlugin>()
+                assertNotNull(runnableSamplesPlugin)
+            }
+        }
+    }
+}
