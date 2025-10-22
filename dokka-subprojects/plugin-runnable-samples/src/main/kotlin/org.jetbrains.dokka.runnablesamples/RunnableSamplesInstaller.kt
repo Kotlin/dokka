9fa498ea00c92ca667766824f5d5fd96295740a8
Yurii Oliinyk <oliinykyurii123@gmail.com>
2025-10-22 18:55:26 +0200

    feat: add playground server configuration to runnable samples

--- dokka-subprojects/plugin-runnable-samples/src/main/kotlin/org.jetbrains.dokka.runnablesamples/RunnableSamplesInstaller.kt
+++ dokka-subprojects/plugin-runnable-samples/src/main/kotlin/org.jetbrains.dokka.runnablesamples/RunnableSamplesInstaller.kt
@@ -4,9 +4,13 @@ import org.jetbrains.dokka.pages.RendererSpecificResourcePage
 import org.jetbrains.dokka.pages.RenderingStrategy
 import org.jetbrains.dokka.pages.RootPageNode
 import org.jetbrains.dokka.plugability.DokkaContext
+import org.jetbrains.dokka.plugability.configuration
 import org.jetbrains.dokka.transformers.pages.PageTransformer
 
 public class RunnableSamplesScriptsInstaller(private val dokkaContext: DokkaContext) : PageTransformer {
+
+    private val playgroundServer =
+        configuration<RunnableSamplesPlugin, RunnableSamplesConfiguration>(dokkaContext)?.playgroundServer
 
     private val scriptsPages = listOf(
         "scripts/runnable-samples.js"
@@ -15,12 +19,37 @@ public class RunnableSamplesScriptsInstaller(private val dokkaContext: DokkaCont
     override fun invoke(input: RootPageNode): RootPageNode =
         input.let { root ->
             if (dokkaContext.configuration.delayTemplateSubstitution) root
-            else root.modified(children = input.children + scriptsPages.toRenderSpecificResourcePage())
+            else {
+                if (playgroundServer == null) {
+                    root.modified(children = input.children + scriptsPages.toRenderSpecificResourcePage())
+                } else {
+                    val modifiedScript = modifyScript()
+
+                    root.modified(children = input.children + scriptsPages.map {
+                        RendererSpecificResourcePage(
+                            it,
+                            emptyList(),
+                            RenderingStrategy.Write(modifiedScript)
+                        )
+                    })
+                }
+            }
         }.transformContentPagesTree {
             it.modified(
                 embeddedResources = it.embeddedResources + scriptsPages
             )
         }
+
+    private fun modifyScript(): String {
+        val scriptContent = javaClass.getResource("/dokka/scripts/runnable-samples.js")
+            ?.readText()
+            ?: throw IllegalStateException("Script /dokka/scripts/runnable-samples.js not found in resources")
+
+        return scriptContent.replace(
+            "const playgroundServer = null",
+            "const playgroundServer = \"$playgroundServer\""
+        )
+    }
 }
 
 public class RunnableSamplesStylesInstaller(private val dokkaContext: DokkaContext) : PageTransformer {
