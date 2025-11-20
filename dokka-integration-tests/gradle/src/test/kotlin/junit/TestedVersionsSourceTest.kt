905814389c58863a7c13eb73254350c6077e9dca
Adam Semenenko <152864218+adam-enko@users.noreply.github.com>
2025-11-20 15:42:12 +0100

    update TestVersionsSourceTest

--- dokka-integration-tests/gradle/src/test/kotlin/junit/TestedVersionsSourceTest.kt
+++ dokka-integration-tests/gradle/src/test/kotlin/junit/TestedVersionsSourceTest.kt
@@ -5,9 +5,11 @@
 package org.jetbrains.dokka.it.gradle.junit
 
 import io.kotest.matchers.shouldBe
-import org.jetbrains.dokka.it.gradle.junit.KotlinBuiltInCompatibility.Incompatible
-import org.jetbrains.dokka.it.gradle.junit.KotlinBuiltInCompatibility.Required
+import org.jetbrains.dokka.it.gradle.junit.KotlinBuiltInCompatibility.*
+import org.jetbrains.dokka.it.gradle.junit.TestedVersions.Companion.toMap
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EnumSource
 
 class TestedVersionsSourceTest {
 
@@ -15,109 +17,122 @@ class TestedVersionsSourceTest {
     fun `test default versions`() {
         val default = TestedVersionsSource.Default
 
-        val versions = default.get()
-            .map {
-                it.run {
-                    "gradle:$gradle - kgp:$kgp"
-                }
-            }
-            .sorted()
-            .joinToString("\n")
+        val versions = default.get().joinAllToString()
 
         versions.shouldBe(
             """
-            gradle:7.6.4 - kgp:1.9.25
-            gradle:7.6.4 - kgp:2.0.21
-            gradle:8.14.3 - kgp:1.9.25
-            gradle:8.14.3 - kgp:2.0.21
-            gradle:9.1.0 - kgp:2.1.21
-            gradle:9.1.0 - kgp:2.2.21
-            gradle:9.1.0 - kgp:2.3.0-RC
+            {gradle=7.6.4, kgp=1.9.25}
+            {gradle=8.14.3, kgp=1.9.25}
+            {gradle=7.6.4, kgp=2.0.21}
+            {gradle=8.14.3, kgp=2.0.21}
+            {gradle=9.1.0, kgp=2.1.21}
+            {gradle=9.1.0, kgp=2.2.21}
+            {gradle=9.1.0, kgp=2.3.0-RC}
             """.trimIndent()
         )
     }
 
-    @Test
-    fun `Android versions`() {
-        val default = TestedVersionsSource.Android(
-            kotlinBuiltIn = Required,
-        )
+    @ParameterizedTest
+    @EnumSource(KotlinBuiltInCompatibility::class)
+    fun `Android versions`(
+        kotlinBuiltIn: KotlinBuiltInCompatibility
+    ) {
+        val versions = TestedVersionsSource.Android(kotlinBuiltIn = kotlinBuiltIn)
+            .get().joinAllToString()
 
-        val versions = default.get()
-            .map {
-                it.run {
-                    "gradle:$gradle - kgp:$kgp - agp:$agp"
-                }
-            }
-            .sorted()
-            .joinToString("\n")
+        val expected = when (kotlinBuiltIn) {
+            Required ->
+                """
+                {gradle=9.1.0, kgp=2.1.21, agp=9.0.0-beta01}
+                {gradle=9.1.0, kgp=2.2.21, agp=9.0.0-beta01}
+                {gradle=9.1.0, kgp=2.3.0-RC, agp=9.0.0-beta01}
+                """.trimIndent()
 
-        versions.shouldBe(
-            """
-            gradle:7.6.4 - kgp:1.9.25 - agp:7.4.2
-            gradle:7.6.4 - kgp:2.0.21 - agp:7.4.2
-            gradle:8.14.3 - kgp:1.9.25 - agp:8.11.2
-            gradle:8.14.3 - kgp:1.9.25 - agp:8.12.3
-            gradle:8.14.3 - kgp:1.9.25 - agp:8.13.0
-            gradle:8.14.3 - kgp:2.0.21 - agp:8.11.2
-            gradle:8.14.3 - kgp:2.0.21 - agp:8.12.3
-            gradle:8.14.3 - kgp:2.0.21 - agp:8.13.0
-            """.trimIndent()
-        )
+            Supported ->
+                """
+                {gradle=7.6.4, kgp=1.9.25, agp=7.4.2}
+                {gradle=8.14.3, kgp=1.9.25, agp=8.11.2}
+                {gradle=8.14.3, kgp=1.9.25, agp=8.12.3}
+                {gradle=8.14.3, kgp=1.9.25, agp=8.13.0}
+                {gradle=7.6.4, kgp=2.0.21, agp=7.4.2}
+                {gradle=8.14.3, kgp=2.0.21, agp=8.11.2}
+                {gradle=8.14.3, kgp=2.0.21, agp=8.12.3}
+                {gradle=8.14.3, kgp=2.0.21, agp=8.13.0}
+                {gradle=9.1.0, kgp=2.1.21, agp=9.0.0-beta01}
+                {gradle=9.1.0, kgp=2.2.21, agp=9.0.0-beta01}
+                {gradle=9.1.0, kgp=2.3.0-RC, agp=9.0.0-beta01}
+                """.trimIndent()
+
+            Incompatible ->
+                """
+                {gradle=7.6.4, kgp=1.9.25, agp=7.4.2}
+                {gradle=8.14.3, kgp=1.9.25, agp=8.11.2}
+                {gradle=8.14.3, kgp=1.9.25, agp=8.12.3}
+                {gradle=8.14.3, kgp=1.9.25, agp=8.13.0}
+                {gradle=7.6.4, kgp=2.0.21, agp=7.4.2}
+                {gradle=8.14.3, kgp=2.0.21, agp=8.11.2}
+                {gradle=8.14.3, kgp=2.0.21, agp=8.12.3}
+                {gradle=8.14.3, kgp=2.0.21, agp=8.13.0}
+                """.trimIndent()
+        }
+
+        versions shouldBe expected
     }
 
-    @Test
-    fun `Android versions - with minimum`() {
-        val default = TestedVersionsSource.Android(
-            kotlinBuiltIn = Incompatible,
-        )
+    @ParameterizedTest
+    @EnumSource(KotlinBuiltInCompatibility::class)
+    fun `AndroidCompose versions`(
+        kotlinBuiltIn: KotlinBuiltInCompatibility
+    ) {
+        val versions = TestedVersionsSource.AndroidCompose(kotlinBuiltIn = kotlinBuiltIn)
+            .get().joinAllToString()
 
-        val versions = default.get()
-            .map {
-                it.run {
-                    "gradle:$gradle - kgp:$kgp - agp:$agp"
-                }
-            }
-            .sorted()
-            .joinToString("\n")
+        val expected = when (kotlinBuiltIn) {
+            Required ->
+                """
+                {gradle=9.1.0, kgp=2.1.21, agp=9.0.0-beta01, composeGradlePlugin=1.7.0}
+                {gradle=9.1.0, kgp=2.2.21, agp=9.0.0-beta01, composeGradlePlugin=1.7.0}
+                {gradle=9.1.0, kgp=2.3.0-RC, agp=9.0.0-beta01, composeGradlePlugin=1.7.0}
+                """.trimIndent()
 
-        versions.shouldBe(
-            """
-            gradle:9.1.0 - kgp:2.1.21 - agp:9.0.0-beta01
-            gradle:9.1.0 - kgp:2.2.21 - agp:9.0.0-beta01
-            gradle:9.1.0 - kgp:2.3.0-RC - agp:9.0.0-beta01
-            """.trimIndent()
-        )
+            Supported ->
+                """
+                {gradle=7.6.4, kgp=1.9.25, agp=7.4.2, composeGradlePlugin=1.7.0}
+                {gradle=8.14.3, kgp=1.9.25, agp=8.11.2, composeGradlePlugin=1.7.0}
+                {gradle=8.14.3, kgp=1.9.25, agp=8.12.3, composeGradlePlugin=1.7.0}
+                {gradle=8.14.3, kgp=1.9.25, agp=8.13.0, composeGradlePlugin=1.7.0}
+                {gradle=7.6.4, kgp=2.0.21, agp=7.4.2, composeGradlePlugin=1.7.0}
+                {gradle=8.14.3, kgp=2.0.21, agp=8.11.2, composeGradlePlugin=1.7.0}
+                {gradle=8.14.3, kgp=2.0.21, agp=8.12.3, composeGradlePlugin=1.7.0}
+                {gradle=8.14.3, kgp=2.0.21, agp=8.13.0, composeGradlePlugin=1.7.0}
+                {gradle=9.1.0, kgp=2.1.21, agp=9.0.0-beta01, composeGradlePlugin=1.7.0}
+                {gradle=9.1.0, kgp=2.2.21, agp=9.0.0-beta01, composeGradlePlugin=1.7.0}
+                {gradle=9.1.0, kgp=2.3.0-RC, agp=9.0.0-beta01, composeGradlePlugin=1.7.0}
+                """.trimIndent()
+
+            Incompatible ->
+                """
+                {gradle=7.6.4, kgp=1.9.25, agp=7.4.2, composeGradlePlugin=1.7.0}
+                {gradle=8.14.3, kgp=1.9.25, agp=8.11.2, composeGradlePlugin=1.7.0}
+                {gradle=8.14.3, kgp=1.9.25, agp=8.12.3, composeGradlePlugin=1.7.0}
+                {gradle=8.14.3, kgp=1.9.25, agp=8.13.0, composeGradlePlugin=1.7.0}
+                {gradle=7.6.4, kgp=2.0.21, agp=7.4.2, composeGradlePlugin=1.7.0}
+                {gradle=8.14.3, kgp=2.0.21, agp=8.11.2, composeGradlePlugin=1.7.0}
+                {gradle=8.14.3, kgp=2.0.21, agp=8.12.3, composeGradlePlugin=1.7.0}
+                {gradle=8.14.3, kgp=2.0.21, agp=8.13.0, composeGradlePlugin=1.7.0}
+                """.trimIndent()
+        }
+
+        versions shouldBe expected
     }
 
-    @Test
-    fun `AndroidCompose versions`() {
-        val default = TestedVersionsSource.AndroidCompose()
-
-        val versions = default.get()
-            .map {
-                it.run {
-                    "gradle:$gradle - kgp:$kgp - agp:$agp - compose:$composeGradlePlugin"
-                }
+    companion object {
+        private fun Sequence<TestedVersions>.joinAllToString(): String =
+            joinToString("\n") { versions ->
+                versions.toMap()
+                    .filterKeys { it != "dgp" }
+                    .toString()
             }
-            .sorted()
-            .joinToString("\n")
-
-        versions.shouldBe(
-            """
-            gradle:7.6.4 - kgp:1.9.25 - agp:7.4.2 - compose:1.7.0
-            gradle:7.6.4 - kgp:2.0.21 - agp:7.4.2 - compose:1.7.0
-            gradle:8.14.3 - kgp:1.9.25 - agp:8.11.2 - compose:1.7.0
-            gradle:8.14.3 - kgp:1.9.25 - agp:8.12.3 - compose:1.7.0
-            gradle:8.14.3 - kgp:1.9.25 - agp:8.13.0 - compose:1.7.0
-            gradle:8.14.3 - kgp:2.0.21 - agp:8.11.2 - compose:1.7.0
-            gradle:8.14.3 - kgp:2.0.21 - agp:8.12.3 - compose:1.7.0
-            gradle:8.14.3 - kgp:2.0.21 - agp:8.13.0 - compose:1.7.0
-            gradle:9.1.0 - kgp:2.1.21 - agp:9.0.0-beta01 - compose:1.7.0
-            gradle:9.1.0 - kgp:2.2.21 - agp:9.0.0-beta01 - compose:1.7.0
-            gradle:9.1.0 - kgp:2.3.0-RC - agp:9.0.0-beta01 - compose:1.7.0
-            """.trimIndent()
-        )
     }
 
 }
