8ae9ba8fa7a57c25817351b659a9b2e468341a8f
Yurii Oliinyk <oliinykyurii123@gmail.com>
2025-11-23 15:06:25 +0100

    fix: don't generate documentation for files in `/snippet-files`

--- dokka-subprojects/analysis-java-psi/src/main/kotlin/org/jetbrains/dokka/analysis/java/DefaultPsiToDocumentableTranslator.kt
+++ dokka-subprojects/analysis-java-psi/src/main/kotlin/org/jetbrains/dokka/analysis/java/DefaultPsiToDocumentableTranslator.kt
@@ -37,7 +37,7 @@ internal class DefaultPsiToDocumentableTranslator : AsyncSourceToDocumentableTra
             val localFileSystem = VirtualFileManager.getInstance().getFileSystem("file")
 
             val psiFiles = sourceRoots.parallelMap { sourceRoot ->
-                sourceRoot.absoluteFile.walkTopDown().mapNotNull {
+                sourceRoot.absoluteFile.walkTopDown().filter { !it.path.contains("/snippet-files") }.mapNotNull {
                     localFileSystem.findFileByPath(it.path)?.let { vFile ->
                         PsiManager.getInstance(project).findFile(vFile) as? PsiJavaFile
                     }
