b86986b412dfafa1a64979002fde36645a7b7325
Oleg Yukhnevich <whyoleg@gmail.com>
2025-12-19 18:51:39 +0200

    Kotlin 2.3.0

--- dokka-integration-tests/gradle/src/test/kotlin/junit/TestedVersionsSourceTest.kt
+++ dokka-integration-tests/gradle/src/test/kotlin/junit/TestedVersionsSourceTest.kt
@@ -19,7 +19,7 @@ class TestedVersionsSourceTest {
 
         actual shouldBe """
             gradle: 7.6.4, 8.14.3, 9.1.0
-            kgp: 1.9.25, 2.0.21, 2.1.21, 2.2.21, 2.3.0-Beta2
+            kgp: 1.9.25, 2.0.21, 2.1.21, 2.2.21, 2.3.0
         """.trimIndent()
     }
 
@@ -36,14 +36,14 @@ class TestedVersionsSourceTest {
                 """
                 agp: 9.0.0-beta01
                 gradle: 9.1.0
-                kgp: 2.1.21, 2.2.21, 2.3.0-Beta2
+                kgp: 2.1.21, 2.2.21, 2.3.0
                 """.trimIndent()
 
             Supported ->
                 """
                 agp: 7.4.2, 8.11.2, 8.12.3, 8.13.0, 9.0.0-beta01
                 gradle: 7.6.4, 8.14.3, 9.1.0
-                kgp: 1.9.25, 2.0.21, 2.1.21, 2.2.21, 2.3.0-Beta2
+                kgp: 1.9.25, 2.0.21, 2.1.21, 2.2.21, 2.3.0
                 """.trimIndent()
 
             Incompatible ->
@@ -71,7 +71,7 @@ class TestedVersionsSourceTest {
                 agp: 9.0.0-beta01
                 composeGradlePlugin: 1.7.0
                 gradle: 9.1.0
-                kgp: 2.1.21, 2.2.21, 2.3.0-Beta2
+                kgp: 2.1.21, 2.2.21, 2.3.0
                 """.trimIndent()
 
             Supported ->
@@ -79,7 +79,7 @@ class TestedVersionsSourceTest {
                 agp: 7.4.2, 8.11.2, 8.12.3, 8.13.0, 9.0.0-beta01
                 composeGradlePlugin: 1.7.0
                 gradle: 7.6.4, 8.14.3, 9.1.0
-                kgp: 1.9.25, 2.0.21, 2.1.21, 2.2.21, 2.3.0-Beta2
+                kgp: 1.9.25, 2.0.21, 2.1.21, 2.2.21, 2.3.0
                 """.trimIndent()
 
             Incompatible ->
