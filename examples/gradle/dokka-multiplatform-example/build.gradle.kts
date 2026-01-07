c773e089f824abf4526ea059f57ec78742674db2
Oleg Yukhnevich <whyoleg@gmail.com>
2026-01-07 17:57:58 +0200

    Update Kotlin to 2.3.0 and fixes IT (#4402)
    * Drop DGPv1 test for AGP: it doesn't work with Kotlin 2.3.0 because we compile with older AGP version
    * Regenerate expectedData to include `webMain` after update to KGP 2.3.0 which now creates this sourceSet
    * Add instruction on how to update expected data for `TestedVersionsSourceTest`

--- examples/gradle/dokka-multiplatform-example/build.gradle.kts
+++ examples/gradle/dokka-multiplatform-example/build.gradle.kts
@@ -8,7 +8,7 @@ import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.dokka.Platform
 
 plugins {
-    kotlin("multiplatform") version "2.2.0"
+    kotlin("multiplatform") version "2.3.0"
     id("org.jetbrains.dokka") version "2.1.0"
 }
 
