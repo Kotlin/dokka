07c5506f4092f9064e1e5b3684cde39751756e61
Adam Semenenko <152864218+adam-enko@users.noreply.github.com>
2026-03-05 17:32:31 +0100

    De-duplicate CC delete code. Split out CC test to separate test (so it's easier to see if the test is skipped due to a low Gradle version).

--- dokka-integration-tests/gradle/src/main/kotlin/org/jetbrains/dokka/it/gradle/junit/DokkaGradleProjectRunner.kt
+++ dokka-integration-tests/gradle/src/main/kotlin/org/jetbrains/dokka/it/gradle/junit/DokkaGradleProjectRunner.kt
@@ -6,7 +6,9 @@ package org.jetbrains.dokka.it.gradle.junit
 
 import org.gradle.testkit.runner.GradleRunner
 import org.jetbrains.dokka.gradle.utils.ProjectDirectoryScope
+import org.jetbrains.dokka.gradle.utils.file
 import java.nio.file.Path
+import kotlin.io.path.deleteRecursively
 
 /**
  * Details of a Gradle project, with a [GradleRunner], for running tests.
@@ -17,4 +19,13 @@ import java.nio.file.Path
 class DokkaGradleProjectRunner(
     override val projectDir: Path,
     val runner: GradleRunner,
-) : ProjectDirectoryScope
+) : ProjectDirectoryScope {
+
+    /**
+     * Delete all configuration cache caches and reports.
+     */
+    fun deleteConfigurationCacheData() {
+        file(".gradle/configuration-cache").deleteRecursively()
+        file("build/reports/configuration-cache").deleteRecursively()
+    }
+}
