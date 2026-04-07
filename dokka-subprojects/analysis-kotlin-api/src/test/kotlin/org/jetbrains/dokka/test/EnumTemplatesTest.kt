c2bf556329d32c70b0e66281b9fad1860622fc2e
vmishenev <vad-mishenev@yandex.ru>
2026-04-07 23:07:51 +0300

    Add a package declaration

--- dokka-subprojects/analysis-kotlin-api/src/test/kotlin/org/jetbrains/dokka/test/EnumTemplatesTest.kt
+++ dokka-subprojects/analysis-kotlin-api/src/test/kotlin/org/jetbrains/dokka/test/EnumTemplatesTest.kt
@@ -2,6 +2,8 @@
  * Copyright 2014-2026 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
 
+package org.jetbrains.dokka.test
+
 import org.intellij.lang.annotations.Language
 import org.junit.jupiter.api.assertAll
 import java.nio.file.Files
