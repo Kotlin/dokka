cb79bef3d335aa0d7d7e569c0a88580e6442bde3
Ryan Nett <JNett96@gmail.com>
2026-04-16 12:32:51 -0700

    Add option to suppress annotated declarations (#4464)

--- dokka-runners/runner-cli/src/main/kotlin/org/jetbrains/dokka/SourceSetArgumentsParser.kt
+++ dokka-runners/runner-cli/src/main/kotlin/org/jetbrains/dokka/SourceSetArgumentsParser.kt
@@ -107,6 +107,11 @@ internal fun parseSourceSet(moduleName: String, args: Array<String>): DokkaConfi
         description = "Paths to files to be suppressed. Accepts multiple paths separated by semicolons."
     ).delimiter(";")
 
+    val suppressAnnotatedWith by parser.option(
+        ArgType.String,
+        description = "List of annotation FQNs to suppress declarations annotated with. Accepts multiple values separated by semicolons."
+    ).delimiter(";")
+
     val analysisPlatform: Platform by parser.option(
         ArgTypePlatform,
         description = "Platform used for setting up analysis"
@@ -159,6 +164,8 @@ internal fun parseSourceSet(moduleName: String, args: Array<String>): DokkaConfi
         override val noStdlibLink = noStdlibLink
         override val noJdkLink = noJdkLink
         override val suppressedFiles = suppressedFiles.toMutableSet()
+        override val suppressAnnotatedWith: Set<String> = suppressAnnotatedWith.toSet()
+            .ifEmpty { DokkaDefaults.suppressAnnotatedWith }.toMutableSet()
         override val documentedVisibilities: Set<DokkaConfiguration.Visibility> = documentedVisibilities.toSet()
             .ifEmpty { DokkaDefaults.documentedVisibilities }
 
