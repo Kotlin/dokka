cb803378fed03f3386603a688702aedf1c87ff2e
Oleg Yukhnevich <whyoleg@gmail.com>
2026-05-12 13:34:51 +0300

    Kotlin 2.4.0-RC

--- examples/gradle/dokka-customFormat-example/build.gradle.kts
+++ examples/gradle/dokka-customFormat-example/build.gradle.kts
@@ -7,7 +7,7 @@ import org.jetbrains.dokka.base.DokkaBase
 import org.jetbrains.dokka.base.DokkaBaseConfiguration
 
 plugins {
-    kotlin("jvm") version "2.3.0"
+    kotlin("jvm") version "2.4.0-RC"
     id("org.jetbrains.dokka") version "2.2.0"
 }
 
